110ce8c66800729c8bb4f734294c962e71926cef
Andrew Scobie <andrew.scobie@gmail.com>
2016-12-06 13:10:40 +1300

    base test file

---
+++ test/app/components/job/action/disable/disable-job-dialog.component.spec.ts
@@ -0,0 +1,14 @@
+
+import { DisableJobDialogComponent } from "app/components/job/action";
+
+describe("DisableJobDialogComponent ", () => {
+    let component: DisableJobDialogComponent;
+
+    beforeEach(() => {
+        component = new DisableJobDialogComponent(null, null);
+    });
+
+    it("Should do something ...", () => {
+        // test me
+    });
+});
