e02874cd01066f81616698af02a3d92e38d9d2ff
Timothee Guerin <tiguerin@microsoft.com>
2016-12-08 11:01:14 -0800

    Create form error show reqeuest id and time

--- app/components/base/create-form/create-form.component.ts
+++ app/components/base/create-form/create-form.component.ts
@@ -1,10 +1,17 @@
 import { Component, Input } from "@angular/core";
+import { animate, state, style, transition, trigger } from "@angular/core";
 import { FormGroup } from "@angular/forms";
 import { autobind } from "core-decorators";
 import { Observable } from "rxjs";
 
 import { BatchError } from "app/models";
 import { SidebarRef } from "../sidebar";
+
+interface ErrorData {
+    message: string;
+    requestId: string;
+    time: string;
+}
 
 @Component({
     selector: "bex-create-form",
@@ -29,7 +36,17 @@ export class CreateFormComponent {
 
     public loading = false;
 
-    public error: BatchError = null;
+    public showErrorTroubleshoot = false;
+
+    public set error(error: BatchError) {
+        this._error = error;
+        this.errorData = this.parseErrorData();
+    }
+    public get error() { return this._error; };
+
+    public errorData: ErrorData;
+
+    private _error: BatchError = null;
     @autobind()
     public add(): Observable<any> {
         this.loading = true;
@@ -64,13 +81,30 @@ export class CreateFormComponent {
         }
     }
 
-    public get errorMessage() {
+    public parseErrorData(): ErrorData {
         if (!this.error || !this.error.message) {
             return null;
         }
-        const message = this.error.message.value;
+        const value = this.error.message.value;
         // Remove the request id from the the message
-        const val = message.split("\n");
-        return val.length > 0 ? val[0] : null;
+        const lines = value.split("\n");
+        const message = lines.length > 0 ? lines[0] : null;
+        const requestId = lines.length > 1 ? this._parseRequestDetails(lines[1]) : null;
+        const time = lines.length > 2 ? this._parseRequestDetails(lines[2]) : null;
+
+        return {
+            message,
+            requestId,
+            time,
+        };
+    }
+
+    private _parseRequestDetails(value: string): string {
+        const data = value.split(":", 2);
+        if (data.length > 1) {
+            return data[1];
+        } else {
+            return data[0];
+        }
     }
 }
