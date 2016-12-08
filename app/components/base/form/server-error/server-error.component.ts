e0192bf58a62a08bdcd4c82c14b320014272d2be
Timothee Guerin <tiguerin@microsoft.com>
2016-12-08 13:46:59 -0800

    fix test

--- app/components/base/form/server-error/server-error.component.ts
+++ app/components/base/form/server-error/server-error.component.ts
@@ -25,7 +25,6 @@ export class ServerErrorComponent {
 
     private _error: BatchError = null;
 
-
     public parseErrorData(): ErrorData {
         if (!this.error || !this.error.message) {
             return null;
