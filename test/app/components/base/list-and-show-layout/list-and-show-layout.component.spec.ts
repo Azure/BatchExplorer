70238b652d7b283d1c76ae30623a29b606f6d916
Timothee Guerin <timothee.guerin@outlook.com>
2016-12-13 07:43:36 -0800

    Spec v1

---
+++ test/app/components/base/list-and-show-layout/list-and-show-layout.component.spec.ts
@@ -0,0 +1,79 @@
+import { Component, DebugElement } from "@angular/core";
+import { ComponentFixture, TestBed, fakeAsync, tick } from "@angular/core/testing";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MaterialModule, MdDialog } from "@angular/material";
+import { BrowserModule, By } from "@angular/platform-browser";
+import { ActivatedRoute } from "@angular/router";
+import { BehaviorSubject } from "rxjs";
+
+import { ListAndShowLayoutComponent } from "app/components/base/list-and-show-layout";
+import { RefreshButtonComponent } from "app/components/base/refresh-btn";
+import { ScrollableComponent } from "app/components/base/scrollable";
+import { Filter, FilterBuilder } from "app/utils/filter-builder";
+
+@Component({
+    template: `
+        <bex-list-and-show-layout quickSearchField="url">
+
+        </bex-list-and-show-layout>
+    `,
+})
+export class TestLayoutComponent {
+
+}
+
+fdescribe("ListAndShowLayout", () => {
+    let fixture: ComponentFixture<TestLayoutComponent>;
+    let de: DebugElement;
+    let layoutComponent: ListAndShowLayoutComponent;
+    let quickSearchInput: DebugElement;
+    let activatedRouteSpy: any;
+
+    beforeEach(() => {
+        activatedRouteSpy = {
+            queryParams: new BehaviorSubject({}),
+        };
+        TestBed.configureTestingModule({
+            imports: [BrowserModule, FormsModule, ReactiveFormsModule, MaterialModule],
+            declarations: [
+                TestLayoutComponent,
+                ListAndShowLayoutComponent,
+                ScrollableComponent,
+                RefreshButtonComponent,
+            ],
+            providers: [
+                { provide: ActivatedRoute, useValue: activatedRouteSpy },
+                { provide: MdDialog, useValue: {} },
+            ],
+        });
+        fixture = TestBed.createComponent(TestLayoutComponent);
+        fixture.detectChanges();
+
+        de = fixture.debugElement;
+        layoutComponent = de.query(By.css("bex-list-and-show-layout")).componentInstance;
+        quickSearchInput = de.query(By.css(".quicksearch > input"));
+
+    });
+
+    describe("Filter", () => {
+        it("Quick search should use the custom field specified", fakeAsync(() => {
+            quickSearchInput.nativeElement.value = "abc";
+            quickSearchInput.nativeElement.dispatchEvent(new Event("input"));
+            expect(layoutComponent.filter.isEmpty()).toBe(true);
+
+            // Should wait the debounce time
+            tick(400);
+            expect(layoutComponent.filter.toOData()).toBe("(startswith(url, 'abc'))");
+        }));
+
+        it("Triggering the advanced filter callback should update the filter", fakeAsync(() => {
+            layoutComponent.advancedFilterChanged(FilterBuilder.prop("state").eq("running"));
+            expect(layoutComponent.filter.toOData()).toBe("(state eq 'running')");
+
+            quickSearchInput.nativeElement.value = "abc";
+            quickSearchInput.nativeElement.dispatchEvent(new Event("input"));
+            tick(400);
+            expect(layoutComponent.filter.toOData()).toBe("(startswith(url, 'abc') and state eq 'running')");
+        }));
+    });
+});
