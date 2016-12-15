5b186617355c16868522130adaf57eaa1498104c
Timothee Guerin <tiguerin@microsoft.com>
2016-12-15 10:05:23 -0800

    Added tooltip for task preview

--- app/components/task/browse/preview/task-preview.component.ts
+++ app/components/task/browse/preview/task-preview.component.ts
@@ -24,6 +24,16 @@ export class TaskPreviewComponent {
         return code === undefined ? "?" : code;
     }
 
+    public get exitCodeMessage() {
+        const code = this.task.executionInfo ? this.task.executionInfo.exitCode : this.task.exitCode;
+        if (code === undefined) {
+            return `Task completed with no exit code`;
+        } else {
+            return `Task completed with exit code ${code}`;
+        }
+
+    }
+
     public get startTime() {
         return this.task.startTime || this.task.executionInfo.startTime;
     }
