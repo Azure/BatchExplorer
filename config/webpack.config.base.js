f369b6007adb78778106e7a8d53b4c4217e32d57
Timothee Guerin <timothee.guerin@outlook.com>
2017-01-06 16:53:40 -0800

    Upgrade to webpack 2.2 fix the webpack error in the console (#93)

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -1,3 +1,5 @@
+const webpack = require("webpack");
+
 const baseConfig = {
     entry: {
         "polyfills": "./app/polyfills.browser",
@@ -5,38 +7,41 @@ const baseConfig = {
     },
 
     resolve: {
-        extensions: ["", ".ts", ".js", ".json", ".scss", ".css", ".html"],
-        modulesDirectories: [".", "node_modules"],
+        extensions: [".ts", ".js", ".json", ".scss", ".css", ".html"],
+        modules: [".", "node_modules"],
     },
 
     module: {
-        loaders: [{
+        rules: [{
             test: /\.ts$/,
-            loaders: ["ts", "angular2-template-loader"],
+            loaders: ["ts-loader", "angular2-template-loader"],
             exclude: [/node_modules/],
         },
         {
             test: /\.scss$/,
-            loader: "style!css!sass",
+            loader: "style-loader!css-loader!sass-loader",
             exclude: [/node_modules/],
         },
         {
             test: /\.html$/,
-            loader: "raw",
+            loader: "raw-loader",
             exclude: [/node_modules/],
         },
         {
             test: /\.json$/,
-            loader: "raw",
+            loader: "raw-loader",
             exclude: [],
         },
         {
             test: /node_modules.*\.css$/,
-            loader: "raw",
+            loader: "raw-loader",
         }],
     },
+    plugins: [
+        // Workaround for WARNING Critical dependency: the request of a dependency is an expression
+        new webpack.ContextReplacementPlugin(/angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/, __dirname),
+    ],
     target: "electron-renderer",
 };
-
 
 module.exports = baseConfig;
