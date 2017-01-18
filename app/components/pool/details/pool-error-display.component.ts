8ee7d0e1fdafa592e67b2cd5aedb74a570c4425a
Timothee Guerin <tiguerin@microsoft.com>
2017-01-18 11:47:16 -0800

    Pool resize error quota reached help

---
+++ app/components/pool/details/pool-error-display.component.ts
@@ -0,0 +1,47 @@
+import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { autobind } from "core-decorators";
+import { shell } from "electron";
+
+import { Pool, ResizeErrorCode } from "app/models";
+import { PoolService } from "app/services";
+import { ExternalLinks } from "app/utils/constants";
+
+@Component({
+    selector: "bex-pool-error-display",
+    templateUrl: "pool-error-display.html",
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PoolErrorDisplayComponent {
+    @Input()
+    public pool: Pool;
+
+    constructor(private poolService: PoolService) {
+
+    }
+
+    public get hasResizeError(): boolean {
+        return Boolean(this.pool && this.pool.resizeError);
+    }
+
+    public get hasQuotaReachedError(): boolean {
+        return this.hasResizeError && Boolean(this.pool.resizeError.code === ResizeErrorCode.accountCoreQuotaReached);
+    }
+
+    @autobind()
+    public fixStopResizeError() {
+        const obs = this.poolService.resize(this.pool.id, this.pool.targetDedicated);
+        obs.subscribe(() => {
+            this.refreshPool();
+        });
+
+        return obs;
+    }
+
+    public increaseQuota() {
+        shell.openExternal(ExternalLinks.supportRequest);
+    }
+
+    public refreshPool() {
+        return this.poolService.getOnce(this.pool.id);
+    }
+}
