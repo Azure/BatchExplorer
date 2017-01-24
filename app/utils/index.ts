3e29471b881d6038a854c337b85f6b6f50dc52df
Pablo <paselem@microsoft.com>
2017-01-24 14:12:43 -0800

    rename and refactor fileUrilUtils

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,13 +1,11 @@
 export * from "./array";
 export * from "./color";
+export * from "./file-url-utils";
 export * from "./logger";
 export * from "./observable";
 export * from "./object";
 export * from "./os";
 export * from "./secure";
-
-import * as fileUrlUtils from "./fileUrlUtils";
-export const FileUrlUtils = fileUrlUtils;
 
 import * as constants from "./constants";
 export const Constants = constants;
