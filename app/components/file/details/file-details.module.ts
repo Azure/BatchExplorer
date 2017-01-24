c2aa81caada2ababf3ff1f9d65b5ec4c0524e362
Timothee Guerin <tiguerin@microsoft.com>
2017-01-23 17:40:52 -0800

    Message

--- app/components/file/details/file-details.module.ts
+++ app/components/file/details/file-details.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from "@angular/core";
 
 import { commonModules } from "app/common";
-import { NodeBrowseModule } from "app/components/node/browse";
-import { PoolGraphsModule } from "app/components/pool/graphs";
-import { StartTaskModule } from "app/components/pool/start-task";
 import { FileContentComponent } from "./file-content.component";
 
 const components = [FileContentComponent];
