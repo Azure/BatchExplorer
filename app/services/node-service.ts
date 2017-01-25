620ab4b255740150f528e12f5ea51d653f6ebf3a
Timothee Guerin <tiguerin@microsoft.com>
2017-01-25 14:37:42 -0800

    Fix

--- app/services/node-service.ts
+++ app/services/node-service.ts
@@ -48,8 +48,7 @@ export class NodeService extends ServiceBase {
             },
             initialParams: { poolId: initialPoolId },
             initialOptions,
-        })​;
-
+        });
     }
 
     public listAll(poolId: string, options: PoolListOptions = {}): Observable<List<Node>> {
@@ -72,8 +71,7 @@ export class NodeService extends ServiceBase {
                 return BatchClient.node.get(params.poolId, params.id, options);
             },
             initialParams: { poolId: initialPoolId },
-        })​;
-
+        });
     }
 
     public reboot(poolId: string, nodeId: string): Observable<any> {
