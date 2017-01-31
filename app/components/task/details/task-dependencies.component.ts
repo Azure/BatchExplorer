91a31a3b661a16381f28e470f5e6bb3d4b9dcea6
Andrew Scobie <andrew.scobie@gmail.com>
2017-01-31 15:45:25 +1300

    fixed issue when no tasks found for multiple task query

--- app/components/task/details/task-dependencies.component.ts
+++ app/components/task/details/task-dependencies.component.ts
@@ -83,13 +83,13 @@ export class TaskDependenciesComponent implements OnDestroy {
      * @param pageData: data for the current page
      */
     private _processMultipleTaskResponse(tasks: List<Task>, pageData: TaskDependency[]): void {
-        if (!(tasks && tasks.size > 0)) {
-            return;
-        }
         for (let td of pageData) {
             td.loading = false;
 
-            const found = tasks.filter(item => item.id === td.id).first();
+            const found = tasks && tasks.size > 0
+                ? tasks.filter(item => item.id === td.id).first()
+                : null;
+
             if (!found) {
                 continue;
             }
