15a273a7ea766aa815140bbe353b32f9bb2046e7
Andrew Scobie <andrew.scobie@gmail.com>
2017-02-08 17:08:37 +1300

    uploading file basic layout

--- app/components/application/action/add/application-create-dialog.component.ts
+++ app/components/application/action/add/application-create-dialog.component.ts
@@ -17,6 +17,15 @@ import { Constants } from "app/utils";
 export class ApplicationCreateDialogComponent implements OnInit {
     public file: File;
     public applicationForm: FormGroup;
+    public blockCount: number = 0;
+
+    // upload block size of 1mb
+    private _blockSize: number;
+    private _fileReader: FileReader;
+    private _blockIds: string[];
+    private _currentFilePointer: number;
+    private _totalBytesRemaining: number;
+    private _bytesUploaded: number;
 
     constructor(
         private formBuilder: FormBuilder,
@@ -55,17 +64,48 @@ export class ApplicationCreateDialogComponent implements OnInit {
         const element = <any>changeEvent.srcElement;
         if (element.files.length > 0) {
             this.file = element.files[0];
-            // this.applicationForm.patchValue({ package: this.file.name });
             this.applicationForm.controls["package"].setValue(this.file.name);
         } else {
             this.file = null;
             this.applicationForm.controls["package"].setValue(null);
-            // this.applicationForm.patchValue({ package: null });
         }
 
-        // this.applicationForm.controls["package"].touched = true;
-        console.log("this.applicationForm.controls[\"package\"] :: ", this.applicationForm.controls["package"]);
-        console.log("fileSelected :: ", this.file);
+        /**
+         * TODO :: The following code wont be run from here. It will be kicked off
+         * from the submit() method. Just here so i can test it on file selection.
+         *
+         * NOTE: mostly taken from here:
+         * http://gauravmantri.com/2013/02/16/uploading-large-files-in-windows-azure-blob-storage-using-shared-access-signature-html-and-javascript/
+         */
+        if (this.file) {
+            this._blockIds = [];
+            this._currentFilePointer = 0;
+            this._totalBytesRemaining = 0;
+            this._blockSize = 1024 * 1024;
+            this._bytesUploaded = 0;
+
+            this._fileReader = new FileReader();
+            this._fileReader.onloadend = (evt => this._fileReaderLoadEnded(evt));
+
+            console.log("fileSelected :: ", this.file.path);
+
+            let fileSize = this.file.size;
+            this._blockSize = fileSize < this._blockSize
+                ? fileSize
+                : this._blockSize;
+
+            console.log("max block size :: " + this._blockSize);
+
+            this._totalBytesRemaining = fileSize;
+            if (fileSize % this._blockSize === 0) {
+                this.blockCount = fileSize / this._blockSize;
+            } else {
+                this.blockCount = Math.floor(fileSize / this._blockSize) + 1;
+            }
+
+            console.log("total blocks to upload :: " + this.blockCount);
+            this._readAndUploadFileBlocks();
+        }
     }
 
     @autobind()
@@ -75,21 +115,75 @@ export class ApplicationCreateDialogComponent implements OnInit {
         observable.subscribe({
             next: (packageVersion) => {
                 const id = formData.id;
-
                 /**
                  * get "packageVersion.storageUrl" and use that SAS to upload the package into storage
                  * then call /activate to complete the upload.
+                 *
+                 * todo:
+                 *      create upload file handler action
+                 *      create activate package action
+                 *      pass both of these actions to the long running task mananger
+                 *      on completion call the onApplicationAdded code below
                  */
+
+                console.log("packageVersion.storageUrl :: ", packageVersion.storageUrl);
 
                 this.applicationService.onApplicationAdded.next(id);
                 this.notificationManager.success(
                     "Application added!",
-                    `Version ${packageVersion.version} for application '${id}' was successfully created!`
+                    `Version ${packageVersion.version} for application '${id}' was successfully created!`,
                 );
             },
             error: () => null,
         });
 
         return observable;
+    }
+
+    private _readAndUploadFileBlocks() {
+        if (this._totalBytesRemaining > 0) {
+                const blockId = "block-" + (this._blockIds.length + "").padStart(6, "0");
+                const fileContent = this.file.slice(this._currentFilePointer,
+                    this._currentFilePointer + this._blockSize);
+
+                console.log(`block id: ${blockId}, current file pointer: ${this._currentFilePointer}, bytes read: ${this._blockSize}`);
+
+                /**
+                 * TODO: a couple of sites say not to use "btoa" as apparently it has an issue
+                 * handling Unicode.
+                 */
+                this._blockIds.push(btoa(blockId));
+                this._fileReader.readAsArrayBuffer(fileContent);
+
+                this._currentFilePointer += this._blockSize;
+                this._totalBytesRemaining -= this._blockSize;
+
+                if (this._totalBytesRemaining < this._blockSize) {
+                    this._blockSize = this._totalBytesRemaining;
+                }
+            } else {
+                this._commitBlockList();
+            }
+    }
+
+    private _fileReaderLoadEnded(evt: any) {
+        // console.log("_afterLoadEnded :: ", evt);
+        if (evt.target.readyState === 2) {
+            const requestData = new Uint8Array(evt.target.result);
+
+            // upload file here to SAS URL then call to upload the next one
+            // this.uploadService.uploadBlock(sasUrl, requestData)
+
+            this._bytesUploaded += requestData.length;
+            const percentComplete = Math.floor(this._bytesUploaded / this.file.size * 100);
+            console.log(`requestData.length: ${requestData.length}, progress: ${percentComplete}%`);
+
+            this._readAndUploadFileBlocks();
+        }
+    }
+
+    private _commitBlockList() {
+        console.log("*** _commitBlockList ***");
+        // this.uploadService.commitBlockList(sasUrl, this._blockIds)
     }
 }
--- test/app/utils/extensions/string.spec.ts
+++ test/app/utils/extensions/string.spec.ts
@@ -1,11 +1,20 @@
 import "app/utils/extensions/string";
 
-describe("String extensions", () => {
+fdescribe("String extensions", () => {
     it("Should format string correctly with 1 argument", () => {
         expect("Some value is {0}".format("awesome")).toEqual("Some value is awesome");
     });
 
     it("Should format string correctly with multiple arguments", () => {
         expect("{0}:{1}".format("key", "val")).toEqual("key:val");
+    });
+
+    it("Should pad the start of a string correctly", () => {
+        expect("1".padStart(5, "0")).toEqual("00001");
+        expect("1".padStart(5, "bob")).toEqual("bobb1");
+        expect("10000".padStart(5, "0")).toEqual("10000");
+
+        // should leave any over length strings as they are
+        expect("50000000".padStart(5, "0")).toEqual("50000000");
     });
 });
