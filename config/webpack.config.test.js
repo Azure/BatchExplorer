d6349e6b0f8714b0125e9dd2d4ab152544a920db
Timothee Guerin <timothee.guerin@outlook.com>
2017-02-14 08:58:23 -0800

    Make active item to always be in the selected list (#174)
    * More
    
    * tslint fixes
    
    * Task file list cleanup
    
    * Quick list specs

--- config/webpack.config.test.js
+++ config/webpack.config.test.js
@@ -1,4 +1,6 @@
 const config = require("./webpack.config.base");
+const webpack = require("webpack");
+const CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
 
 // We need to remove the app entry from the default config as this is defined in karma
 delete config.entry;
@@ -6,5 +8,5 @@ delete config.entry;
 config.devtool = "inline-source-map";
 
 // Karma webpack doesn't support CommonChunkPlugin yet https://github.com/webpack-contrib/karma-webpack/issues/24
-config.plugins = [];
+config.plugins = config.plugins.filter(x => !(x instanceof CommonsChunkPlugin));
 module.exports = config;
