e91b43222b96743a9fe1c52833e6098db5387a5a
Timothee Guerin <tiguerin@microsoft.com>
2017-02-14 09:19:52 -0800

    tests & cleanup

--- app/components/base/scrollable/scrollable.module.ts
+++ app/components/base/scrollable/scrollable.module.ts
@@ -6,7 +6,6 @@ import { BrowserModule } from "@angular/platform-browser";
 import { ScrollableComponent } from "./scrollable.component";
 import { ScrollableService } from "./scrollable.service";
 
-
 @NgModule({
     declarations: [
         ScrollableComponent,
@@ -21,7 +20,7 @@ import { ScrollableService } from "./scrollable.service";
     ],
     providers: [
         ScrollableService,
-    ]
+    ],
 })
 export class ScrollableModule {
 }
