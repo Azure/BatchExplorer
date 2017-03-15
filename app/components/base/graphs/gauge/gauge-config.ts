5ad2e219c0d9ba24bb6850985cbe54b9eeeba9fd
Timothee Guerin <tiguerin@microsoft.com>
2017-03-15 12:13:44 -0700

    Gauge config

--- app/components/base/graphs/gauge/gauge-config.ts
+++ app/components/base/graphs/gauge/gauge-config.ts
@@ -1,12 +1,33 @@
 
 export interface GaugeLabel {
+    /**
+     * Specify this to override the default displayed value of the label.
+     */
     value?: string;
+
+    /**
+     * Specify this to add a tooltip when hovering the value.
+     */
     tooltip?: string;
 }
 
 export interface GaugeLabels {
+    /**
+     * Specify to add a tooltip or change the default value for the min label
+     * @default value: options.min
+     */
     min?: GaugeLabel;
+
+    /**
+     * Specify to add a tooltip or change the default value for the max label
+     * @default value: options.max
+     */
     max?: GaugeLabel;
+
+    /**
+     * Specify to add a tooltip or change the default value for the value label
+     * @default value: value given to the component
+     */
     value?: GaugeLabel;
 }
 
