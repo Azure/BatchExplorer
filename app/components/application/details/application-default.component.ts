9b41a9f8ba788b5b48e75908427af1f11d0f250a
ascobie <andrew.scobie@microsoft.com>
2017-03-24 16:31:42 +1300

    Feature/application link (#266)
    * add application link to accout home page, fix delete resource file css
    
    * replace the last of the old application icons, change node files tab for task

--- app/components/application/details/application-default.component.ts
+++ app/components/application/details/application-default.component.ts
@@ -5,7 +5,7 @@ import { Component } from "@angular/core";
     template: `
         <bl-application-error-display></bl-application-error-display>
         <div class="no-entity-home">
-            <i class="fa fa-cogs"></i>
+            <i class="fa fa-file-archive-o"></i>
             <p>Please select an application from the list</p>
         </div>
     `,
