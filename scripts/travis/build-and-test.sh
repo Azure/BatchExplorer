2f01bab3862fad749ff1434f0bc4e5180c87d7d9
Timothee Guerin <timothee.guerin@outlook.com>
2017-04-14 15:08:49 -0700

    renable all scripts

--- scripts/travis/build-and-test.sh
+++ scripts/travis/build-and-test.sh
@@ -7,14 +7,15 @@ echo "=======  Starting build-and-test.sh  =====================================
 cd $(dirname $0)/../..
 
 # Normal build
-# npm run build -s
+npm run build -s
 
+# Only run prod build if on a branch build or PR for stable
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] || [ "${TRAVIS_BRANCH}" = "stable" ]; then
 	npm run build:prod
 fi
 
 # Run the test
-# npm run test -s
+npm run test -s
 
 # Run the lint
 npm run lint -s
