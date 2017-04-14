840c08f536ca59cabcb1c5316efe771632b4ce31
Timothee Guerin <timothee.guerin@outlook.com>
2017-04-14 10:00:50 -0700

    prod build only stable test

---
+++ scripts/travis/buid-and-test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -ev
+
+echo "=======  Starting build-and-test.sh  ========================================"
+
+# Go to project dir
+cd $(dirname $0)/../..
+
+# Normal build
+# npm run build -s
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] || ["${TRAVIS_BRANCH}" = "master"]; then
+	npm run build:prod
+fi
+
+# Run the test
+# npm run test -s
+
+# Run the lint
+npm run lint -s
