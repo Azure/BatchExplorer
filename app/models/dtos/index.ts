d13d1c96785c72181fb16af552dd9076d36b5ff7
Timothee Guerin <tiguerin@microsoft.com>
2017-04-17 12:24:49 -0700

    Pool rescale support forumla

--- app/models/dtos/index.ts
+++ app/models/dtos/index.ts
@@ -3,6 +3,7 @@
  * This folder is used for interfaces used for creating/updating entities
  */
 export * from "./pool-create.dto";
+export * from "./pool-enable-autoscale.dto";
 export * from "./job-create.dto";
 export * from "./task-create.dto";
 export * from "./metadata.dto";
