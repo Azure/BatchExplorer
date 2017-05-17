114c37c9accc5aa6ec36e51441fc6567f3679a6d
Yunlong Zhang <yunzh@microsoft.com>
2017-05-17 10:21:19 -0700

    refactor code per code review.

--- app/services/predefined-formula.service.ts
+++ app/services/predefined-formula.service.ts
@@ -17,6 +17,7 @@ interface SampleFormulaIndex {
 export class PredefinedFormulaService {
     public predefinedFormulas: Observable<AutoscaleFormula[]>;
     private _predefinedFormulas = new BehaviorSubject<AutoscaleFormula[]>(null);
+
     constructor(private githubData: GithubDataService) {
         this.predefinedFormulas = this._predefinedFormulas.asObservable();
     }
@@ -27,24 +28,29 @@ export class PredefinedFormulaService {
                 const result: AutoscaleFormula[] = new Array();
                 const data: SampleFormulaIndex[] = response.json();
                 for (let file of data) {
-                    this.githubData.get(`${predefinedFormulaPath}${file.value}`).subscribe({
-                        next: (fileResponse: Response) => {
-                            const text: string = fileResponse.text();
-                            const formula: AutoscaleFormula = new AutoscaleFormula({
-                                name: file.name,
-                                value: text,
-                            });
-                            result.push(formula);
-                        },
-                        error: (error) => {
-                            log.error(`Error ${file.value} from github`, error);
-                        },
-                    });
+                    const filePath = `${predefinedFormulaPath}${file.value}`;
+                    this._getPredefinedFormula(filePath, file.name, result);
                 }
                 this._predefinedFormulas.next(result);
             },
             error: (error) => {
                 log.error("Error loading predefined formula from github", error);
+            },
+        });
+    }
+
+    private _getPredefinedFormula(path: string, fileName: string, resultArray: AutoscaleFormula[]) {
+        this.githubData.get(path).subscribe({
+            next: (fileResponse: Response) => {
+                const text: string = fileResponse.text();
+                const formula: AutoscaleFormula = new AutoscaleFormula({
+                    name: fileName,
+                    value: text,
+                });
+                resultArray.push(formula);
+            },
+            error: (error) => {
+                log.error(`Error ${fileName} from github`, error);
             },
         });
     }
