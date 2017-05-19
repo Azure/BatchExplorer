a5b4b6570acc2ba4cf1898a81b12222d98429820
Pablo <paselem@microsoft.com>
2017-05-19 08:51:32 -0700

    unsubscribe from previous account change events

--- app/components/account/details/account-default.component.ts
+++ app/components/account/details/account-default.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { AccountService } from "app/services";
+import { Subscription } from "rxjs";
 
 @Component({
     selector: "bl-account-details-home",
@@ -12,10 +13,12 @@ import { AccountService } from "app/services";
     `,
 })
 
-export class AccountDefaultComponent implements OnInit {
+export class AccountDefaultComponent implements OnInit, OnDestroy {
     public static breadcrumb() {
         return { name: "Accounts" };
     }
+
+    private accountChangeSubscription: Subscription;
 
     constructor(
         private accountService: AccountService,
@@ -23,10 +26,16 @@ export class AccountDefaultComponent implements OnInit {
     }
 
     public ngOnInit(): void {
-        this.accountService.currentAccountId.subscribe((accountId) => {
+        // Subscribe to the current account id. If it has been set elsewhere (e.g. the top nav)
+        // then navigate the component to that account
+        this.accountChangeSubscription = this.accountService.currentAccountId.subscribe((accountId) => {
             if (accountId) {
                 this.router.navigate(["/accounts", accountId]);
             }
         });
+    }
+
+    public ngOnDestroy(): void {
+        this.accountChangeSubscription.unsubscribe();
     }
 }
