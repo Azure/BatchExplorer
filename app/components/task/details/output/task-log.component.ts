05f3db9ab09cab234f6389768c63dc85b3427f20
Andrew Scobie <andrew.scobie@gmail.com>
2017-05-30 16:11:07 +1200

    ability to add new output file to be read from node

--- app/components/task/details/output/task-log.component.ts
+++ app/components/task/details/output/task-log.component.ts
@@ -1,57 +1,136 @@
-import { Component, Input, OnChanges, OnDestroy } from "@angular/core";
-import { Subscription } from "rxjs";
+import { Component, Input, OnChanges, OnDestroy, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { BehaviorSubject, Observable, Subscription } from "rxjs";
 
-import { File, Task } from "app/models";
+import { File, Task, TaskState } from "app/models";
 import { FileService } from "app/services";
 import { prettyBytes } from "app/utils";
 
-const outputFileNames = ["stdout.txt", "stderr.txt"];
+const defaultOutputFileNames = ["stdout.txt", "stderr.txt"];
 
 @Component({
     selector: "bl-task-log",
     templateUrl: "task-log.html",
 })
-export class TaskLogComponent implements OnChanges, OnDestroy {
+export class TaskLogComponent implements OnInit, OnChanges, OnDestroy {
     @Input()
     public jobId: string;
 
     @Input()
     public task: Task;
 
-    public outputFileNames = outputFileNames;
-    public outputFilename: "stdout.txt" | "stderr.txt" = "stdout.txt";
+    public outputFileNames = defaultOutputFileNames.slice();
+    public selectedOutputFile: "stdout.txt" | "stderr.txt" = "stdout.txt";
     public fileSizes: { [filename: string]: string } = {};
+    public filterControl = new FormControl();
+    public filteredOptions: Observable<string[]>;
+    public addingFile = false;
 
     private _dataSubs: Subscription[] = [];
+    private _options: BehaviorSubject<string[]>;
+    private _currentTaskId: string = null;
 
     constructor(private fileService: FileService) {
+        this._options = new BehaviorSubject<string[]>([]);
+        // todo: read these from the node
+        // todo: future enhancement, read from storage as well
+        // https://material.angular.io/components/component/autocomplete
+        this._options.next([
+            "stdout.txt",
+            "stderr.txt",
+            "ProcessEnv.cmd",
+            "bob.cmd",
+        ]);
+        this.filteredOptions = this._options;
     }
+
+   public ngOnInit() {
+      this.filteredOptions = this.filterControl.valueChanges
+         .map((nameFilter) => {
+             return nameFilter
+                ? this._options.value.filter(option => new RegExp(`${nameFilter}`, "gi").test(option))
+                : this._options.value;
+        });
+   }
 
     public ngOnChanges(inputs) {
         if (inputs.jobId || inputs.task) {
+            /**
+             * ngOnChanges is fired multiple times for the same task selection
+             * so this should cut down on API chatter.
+             */
+            if (this.task && this._currentTaskId !== this.task.id) {
+                this._currentTaskId = this.task.id;
+                this.fileSizes = {};
+                this.addingFile = false;
+                this._updateFileData();
+            }
+        }
+    }
+
+    /**
+     * Navigating away from the job and task so reset the tabs
+     */
+    public ngOnDestroy() {
+        this.addingFile = false;
+        this.outputFileNames = defaultOutputFileNames.slice();
+        this.selectedOutputFile = defaultOutputFileNames[0] as any;
+        this._clearSubscriptions();
+    }
+
+    /**
+     * Enable/disable the filter control
+     */
+    public toggleFilter() {
+        this.addingFile = !this.addingFile;
+        this.filterControl.setValue(null);
+    }
+
+    /**
+     * Fires when an item is selected from the autocomplete option list
+     * @param event - the selection event
+     * @param item - selected item, add this to a tab if it doesn't already exist
+     */
+    public optionSelected($event: any, item: string) {
+        if (item && !this.outputFileNames.find(existing => existing === item)) {
+            this.outputFileNames.push(item);
+            this.selectedOutputFile = item as any;
+            this.toggleFilter();
             this._updateFileData();
         }
     }
 
-    public ngOnDestroy() {
-        this._clearSubscriptions();
-    }
-
+    /**
+     * Get the sizes for the output file name collection
+     */
     private _updateFileData() {
         this._clearSubscriptions();
-        outputFileNames.map((filename) => {
-            const data = this.fileService.getFilePropertiesFromTask(this.jobId, this.task.id, filename);
-            this._dataSubs.push(data.item.subscribe((file: File) => {
-                if (file) {
-                    const props = file.properties;
-                    this.fileSizes[filename] = prettyBytes(props && props.contentLength);
-                }
-            }));
+        this.outputFileNames.map((filename) => {
+            if (this._shouldGetFileSize(filename)) {
+                const data = this.fileService.getFilePropertiesFromTask(this.jobId, this.task.id, filename);
+                this._dataSubs.push(data.item.subscribe((file: File) => {
+                    if (file) {
+                        const props = file.properties;
+                        this.fileSizes[filename] = prettyBytes(props && props.contentLength);
+                    }
+                }));
 
-            data.fetch();
+                data.fetch();
+            }
         });
     }
 
+    /**
+     * Only get the size of this file if either the task hasn't completed, or
+     * we don't have the current file size for the file.
+     */
+    private _shouldGetFileSize(filename: string) {
+        return this.task.state !== TaskState.completed || !this.fileSizes[filename];
+    }
+
+    /**
+     * Unsubscribe from any existing saved subscriptions
+     */
     private _clearSubscriptions() {
         this._dataSubs.forEach(x => x.unsubscribe());
     }
