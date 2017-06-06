72b1975b69426bd318637e8805c985be91db3533
Andrew Scobie <andrew.scobie@gmail.com>
2017-06-06 16:04:56 +1200

    tests

--- test/app/components/task/details/output/task-log.component.spec.ts
+++ test/app/components/task/details/output/task-log.component.spec.ts
@@ -3,8 +3,6 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { MdAutocomplete } from "@angular/material";
 import { By } from "@angular/platform-browser";
 import { RouterTestingModule } from "@angular/router/testing";
-// import { List } from "immutable";
-// import { Observable } from "rxjs";
 
 import { TaskLogComponent } from "app/components/task/details/output";
 import { File } from "app/models";
@@ -15,10 +13,11 @@ import { RxMockEntityProxy, RxMockListProxy } from "test/utils/mocks";
 const fileSizeMap: Map<string, number> = new Map()
     .set("stdout.txt", 10)
     .set("stderr.txt", 20)
-    .set("1.txt", 30)
-    .set("2.txt", 40);
+    .set("banana.txt", 30)
+    .set("apple.txt", 40)
+    .set("pear.txt", 50);
 
-fdescribe("TaskDependenciesComponent", () => {
+describe("TaskDependenciesComponent", () => {
     let fixture: ComponentFixture<TaskLogComponent>;
     let component: TaskLogComponent;
 
@@ -31,8 +30,9 @@ fdescribe("TaskDependenciesComponent", () => {
             items: [
                 Fixtures.file.create({ name: "stdout.txt" }),
                 Fixtures.file.create({ name: "stderr.txt" }),
-                Fixtures.file.create({ name: "1.txt" }),
-                Fixtures.file.create({ name: "2.txt" }),
+                Fixtures.file.create({ name: "banana.txt" }),
+                Fixtures.file.create({ name: "apple.txt" }),
+                Fixtures.file.create({ name: "pear.txt" }),
             ],
         });
 
@@ -99,16 +99,17 @@ fdescribe("TaskDependenciesComponent", () => {
         });
     });
 
-    describe("when task depends on taskId array only", () => {
+    describe("loads file sizes based on current job and task", () => {
+        let anyComponent: any;
+
         beforeEach(() => {
+            anyComponent = component as any;
             component.ngOnChanges({ jobId: component.jobId, task: component.task });
             fixture.detectChanges();
         });
 
-        // todo: do these need to be asyncy
-
         it("current task id set", () => {
-            const anyComponent = component as any;
+            anyComponent = component as any;
             expect(anyComponent._currentTaskId).toBe("bobs-task");
         });
 
@@ -123,96 +124,84 @@ fdescribe("TaskDependenciesComponent", () => {
             expect(container.nativeElement.textContent).toContain("stdout.txt (10 B)");
             expect(container.nativeElement.textContent).toContain("stderr.txt (20 B)");
         });
+
+        it("default file tab is stdout.txt", () => {
+            const container = fixture.debugElement.query(By.css(".file-tab.active"));
+            expect(container.nativeElement.textContent).toContain("stdout.txt (10 B)");
+        });
     });
 
-    // describe("when task depends on taskIdRanges array only", () => {
-    //     beforeEach(() => {
-    //         component.task = new Task({
-    //             id: "2001",
-    //             state: TaskState.completed,
-    //             dependsOn: {
-    //                 taskIdRanges: [{ start: 1, end: 5 }, { start: 10, end: 12 }],
-    //             },
-    //         });
+    describe("loads additional task output files", () => {
+        let anyComponent: any;
 
-    //         fixture.detectChanges();
-    //     });
+        beforeEach(() => {
+            anyComponent = component as any;
+            component.ngOnChanges({ jobId: component.jobId, task: component.task });
+            fixture.detectChanges();
+        });
 
-    //     it("shoud have 8 dependent task id's", () => {
-    //         expect(component.dependentIds.length).toBe(8);
-    //         expect(component.dependencies.value.length).toBe(8);
-    //     });
+        it("has 3 extra outputs for auto-complete filter", () => {
+            expect(anyComponent._options.value.length).toBe(3);
+        });
 
-    //     it("should not show load more", () => {
-    //         expect(component.hasMore).toBe(false);
-    //     });
-    // });
+        it("won't add either default file to _options list", () => {
+            let file = { name: "stdout.txt", isDirectory: false };
+            expect(anyComponent._canAddFileToMap(file)).toBe(false);
 
-    // describe("when task depends on both taskId and taskIdRanges arrays", () => {
-    //     beforeEach(() => {
-    //         component.task = new Task({
-    //             id: "2001",
-    //             state: TaskState.completed,
-    //             dependsOn: {
-    //                 taskIds: ["1"],
-    //                 taskIdRanges: [{ start: 1, end: 5 }],
-    //             },
-    //         });
+            file.name = "stderr.txt";
+            expect(anyComponent._canAddFileToMap(file)).toBe(false);
 
-    //         fixture.detectChanges();
-    //     });
+            file.isDirectory = true;
+            expect(anyComponent._canAddFileToMap(file)).toBe(false);
+        });
 
-    //     it("shoud have 6 dependent task id's", () => {
-    //         expect(component.dependentIds.length).toBe(6);
-    //         expect(component.dependencies.value.length).toBe(6);
-    //     });
+        it("hasn't added new files to tab list", () => {
+            expect(component.outputFileNames.length).toBe(2);
+        });
+    });
 
-    //     it("should not show load more", () => {
-    //         expect(component.hasMore).toBe(false);
-    //     });
-    // });
+    describe("can add a new file to a tab", () => {
+        let anyComponent: any;
 
-    // describe("more than 20 dependencies enables load more", () => {
-    //     beforeEach(() => {
-    //         component.task = new Task({
-    //             id: "2001",
-    //             state: TaskState.completed,
-    //             dependsOn: {
-    //                 taskIdRanges: [{ start: 1, end: 25 }],
-    //             },
-    //         });
+        beforeEach(() => {
+            anyComponent = component as any;
+            component.ngOnChanges({ jobId: component.jobId, task: component.task });
+            fixture.detectChanges();
+        });
 
-    //         fixture.detectChanges();
-    //     });
+        it("click add shows filter", () => {
+            const tabElements = fixture.debugElement.queryAll(By.css(".file-tab-more"));
+            tabElements[0].nativeElement.click();
+            fixture.detectChanges();
+            expect(component.addingFile).toBe(true);
+        });
 
-    //     it("should enable load more button", () => {
-    //         expect(component.hasMore).toBe(true);
-    //     });
-    // });
+        it("can filter options", (done) => {
+            component.filteredOptions.subscribe((items) => {
+                expect(items.length).toBe(1);
+                expect(items[0]).toBe("banana.txt");
+                done();
+            });
 
-    // describe("correctly decorates dependsOn of returned dependency", () => {
-    //     beforeEach(() => {
-    //         component.task = new Task({
-    //             id: "2001",
-    //             state: TaskState.completed,
-    //             dependsOn: {
-    //                 taskIds: ["1", "2", "3"],
-    //             },
-    //         });
+            component.filterControl.setValue("banana");
+        });
 
-    //         fixture.detectChanges();
-    //     });
+        it("selecting option loads file into tab list and gets file size", () => {
+            component.optionSelected({}, "banana.txt");
+            expect(component.outputFileNames.length).toBe(3);
+            expect(component.selectedOutputFile).toBe("banana.txt");
+            expect(Object.keys(component.fileSizes).length).toBe(3);
+            expect(component.fileSizes["banana.txt"]).toBe("30 B");
+        });
 
-    //     it("shoud have 3 dependencies", () => {
-    //         expect(component.dependentIds.length).toBe(3);
-    //         expect(component.dependencies.value.length).toBe(3);
-    //     });
+        it("clicking reset will remove additional tabs", () => {
+            component.optionSelected({}, "apple.txt");
+            expect(component.outputFileNames.length).toBe(3);
+            expect(component.selectedOutputFile).toBe("apple.txt");
 
-    //     it("dependsOn property is correct", () => {
-    //         const dependencies = component.dependencies.value;
-    //         expect(dependencies[0].dependsOn).toBe("1,2");
-    //         expect(dependencies[1].dependsOn).toBe("3 tasks");
-    //         expect(dependencies[2].dependsOn).toBe("no tasks");
-    //     });
-    // });
+            component.resetTabs();
+            expect(component.outputFileNames.length).toBe(2);
+            expect(component.selectedOutputFile).toBe("stdout.txt");
+        });
+    });
 });
