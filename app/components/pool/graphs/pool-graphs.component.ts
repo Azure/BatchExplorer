050fbc5b8115ef4b08fe5ed9bdcd377d3e7f0b4e
Timothee Guerin <tiguerin@microsoft.com>
2017-06-06 08:49:44 -0700

    update max running tasks all the time

--- app/components/pool/graphs/pool-graphs.component.ts
+++ app/components/pool/graphs/pool-graphs.component.ts
@@ -78,12 +78,12 @@ export class PoolGraphsComponent implements OnChanges, OnDestroy {
         if (changes.pool) {
             const prev = changes.pool.previousValue;
             const cur = changes.pool.currentValue;
+            this.maxRunningTasks = this.pool ? this.pool.targetDedicated * this.pool.maxTasksPerNode : 0;
             if (prev.id === cur.id) {
                 return;
             }
             this.data.updateParams({ poolId: this.pool.id });
             this.data.refresh(false);
-            this.maxRunningTasks = this.pool ? this.pool.targetDedicated * this.pool.maxTasksPerNode : 0;
             this.runningNodesHistory.reset();
             this.runningTaskHistory.reset();
         }
