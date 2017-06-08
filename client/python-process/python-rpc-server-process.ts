08f9babac74dd2ce97a29a59ccd8f8c66d023b7f
Timothee Guerin <tiguerin@microsoft.com>
2017-06-08 10:24:44 -0700

    Python rpc server

--- client/python-process/python-rpc-server-process.ts
+++ client/python-process/python-rpc-server-process.ts
@@ -12,21 +12,31 @@ logger.info("Python path is", pythonFile);
 
 export class PythonRpcServerProcess {
     private _spawedProcess: ChildProcess;
+    private _askForKill: boolean;
     /**
      * Start the python server
      * @returns Promise when the process has spawned
      */
     public start(): Promise<void> {
+        this._askForKill = false;
         return getPythonPath().then((pythonPath) => {
-            this._spawedProcess = spawn(pythonPath, [pythonFile]);
+            const child = this._spawedProcess = spawn(pythonPath, ["pythonFile"]);
+            child.on("exit", (code) => {
+                if (this._askForKill) {
+                    logger.info("Python rpc server has stopped!");
+                } else {
+                    logger.error("Python Rpc server has exited unexpectedly with code!", code);
+                }
+            });
             logger.info("Python Rpc server started!");
         });
     }
 
     public stop() {
         if (this._spawedProcess) {
+            this._askForKill = true;
+            logger.info("Stopping python rpc server!");
             this._spawedProcess.kill();
-            logger.info("Python Rpc server stopped!");
         }
     }
 
