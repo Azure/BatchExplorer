ba35a95d261b11ea1e35b04d31f2a03b63e20581
Timothee Guerin <tiguerin@microsoft.com>
2017-06-09 13:19:53 -0700

    Build python

--- config/build-python.ts
+++ config/build-python.ts
@@ -1,12 +1,17 @@
 // tslint:disable:no-console
+/**
+ * This script will run the command "python setup.py build" to build the python
+ * but ensure it is using a valid version of python to do so.
+ */
 import * as path from "path";
 
-import { Constants } from "../client/client-constants";
-import { execCommand } from "../client/core";
-import { getPythonPath } from "../client/python-process";
+import { Constants } from "../src/client/client-constants";
+import { execCommand } from "../src/client/core";
+import { getPythonPath } from "../src/client/python-process";
 
 const setupPath = path.join(Constants.root, "python/setup.py");
 getPythonPath().then((pythonPath) => {
+    // Run "python setup.py build"
     execCommand(`${pythonPath} ${setupPath} build`);
 }).catch((errors) => {
     console.error(errors);
