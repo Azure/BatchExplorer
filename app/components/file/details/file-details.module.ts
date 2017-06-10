ba6da99c48609702f23632286b387c5b9e343589
Timothee Guerin <tiguerin@microsoft.com>
2017-06-09 17:01:51 -0700

    image viwer wip

--- app/components/file/details/file-details.module.ts
+++ app/components/file/details/file-details.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from "@angular/core";
 import { commonModules } from "app/common";
 import { FileContentComponent } from "./file-content.component";
 import { FileDetailsComponent } from "./file-details.component";
+import { ImageFileViewerComponent } from "./image-file-viewer";
 import { LogFileViewerComponent } from "./log-file-viewer";
 
-const components = [FileContentComponent, FileDetailsComponent, LogFileViewerComponent];
+const components = [FileContentComponent, FileDetailsComponent, LogFileViewerComponent, ImageFileViewerComponent];
 
 @NgModule({
     declarations: components,
