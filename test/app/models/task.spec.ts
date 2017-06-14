9b3e24ae9941736eb9ad63bf8f7539d73b8d5448
Timothee Guerin <timothee.guerin@outlook.com>
2017-06-14 14:25:28 -0700

    Feature: Load all tasks running on a pool (#455)
    * Load all tasks running on a pool
    
    * Fix typing issues
    
    * Better way to handle stripes for low pri
    
    * Update tooltip to use native title
    
    * cleaner
    
    * Clenaup
    
    * Fix specs & tslint
    
    * fix title and remove unused code
    
    * Added test for title

--- test/app/models/task.spec.ts
+++ test/app/models/task.spec.ts
@@ -21,19 +21,19 @@ function createTask(exitCode: number, startTime: Date, endTime: Date, hasFailure
         constraints: {
             maxWallClockTime: moment.duration("PT4M"),
         },
-    });
+    } as any);
 }
 
 describe("Task Model", () => {
     describe("#didTimeout()", () => {
         it("should return false if execution info or constraints are null", () => {
             expect(new Task().didTimeout).toBe(false);
-            expect(new Task({ executionInfo: { startTime: null } }).didTimeout).toBe(false);
-            expect(new Task({ constraints: {} }).didTimeout).toBe(false);
+            expect(new Task({ executionInfo: { startTime: null } } as any).didTimeout).toBe(false);
+            expect(new Task({ constraints: {} } as any).didTimeout).toBe(false);
         });
 
         it("Should return false if there is no maxWallClockTime", () => {
-            expect(new Task({ executionInfo: {}, constraints: {} }).didTimeout).toBe(false);
+            expect(new Task({ executionInfo: {}, constraints: {} } as any).didTimeout).toBe(false);
         });
 
         it("should return false if exit code is 0", () => {
