2c1c0dc3d3550c686bce891b429345827dc3dc5e
Timothee Guerin <tiguerin@microsoft.com>
2017-06-15 08:37:06 -0700

    Image file viewer is working with loading icon

--- app/components/file/details/image-file-viewer/image-file-viewer.component.ts
+++ app/components/file/details/image-file-viewer/image-file-viewer.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnChanges, OnInit } from "@angular/core";
+import { Component, Input, OnChanges } from "@angular/core";
 import * as path from "path";
 
+import { LoadingStatus } from "app/components/base/loading";
 import { FileSystemService } from "app/services";
 import { FileLoader } from "app/services/file";
 import "./image-file-viewer.scss";
@@ -9,29 +10,30 @@ import "./image-file-viewer.scss";
     selector: "bl-image-file-viewer",
     templateUrl: "image-file-viewer.html",
 })
-export class ImageFileViewerComponent implements OnInit, OnChanges {
+export class ImageFileViewerComponent implements OnChanges {
     @Input()
     public fileLoader: FileLoader;
 
     public src: string;
+    public loadingStatus = LoadingStatus.Loading;
 
     constructor(private fs: FileSystemService) {
 
     }
-    public ngOnInit() {
-        this._loadImage();
-    }
 
     public ngOnChanges(changes) {
+        console.log("Changes..", changes);
         this._loadImage();
     }
 
     private _loadImage() {
         const destination = path.join(this.fs.commonFolders.temp, "task-output", this.fileLoader.filename);
         console.log("Going to save to file", destination);
+        this.loadingStatus = LoadingStatus.Loading;
         this.fileLoader.download(destination).subscribe((result) => {
             console.log("Result", result);
             this.src = `file://${destination}`;
+            this.loadingStatus = LoadingStatus.Ready;
         });
     }
 }
