d9e2b27806936f758e8d0e21548e61d93734ce28
Andrew Scobie <andrew.scobie@gmail.com>
2017-06-28 11:20:55 +1200

    update the delete items dialog to use the new simple form

--- app/components/base/list-and-show-layout/delete-selected-items-dialog.component.ts
+++ app/components/base/list-and-show-layout/delete-selected-items-dialog.component.ts
@@ -1,36 +1,22 @@
 import { Component } from "@angular/core";
 import { MdDialogRef } from "@angular/material";
+import { autobind } from "core-decorators";
+import {  Observable } from "rxjs";
 
 @Component({
     selector: "bl-delete-selected-items-dialog",
-    template: `
-        <div class="confirmation-dialog">
-            <h2>Delete {{ entityName || "items" }}</h2>
-            <div class="message">
-                <p>You are about to delete the selected items, including:</p>
-                <ul class="list-point">
-                    <li *ngFor="let item of items">
-                        <span *ngIf="parentId">{{ parentId }} - {{item}}</span>
-                        <span *ngIf="!parentId">{{item}}</span>
-                    </li>
-                </ul>
-            </div>
-            <div class="buttons">
-                <bl-button type="wide" (click)="dialogRef.close(true)" color="danger">
-                    Yes
-                </bl-button>
-                <bl-button type="wide" type="primary" (click)="dialogRef.close(false)" color="light">No</bl-button>
-            </div>
-        </div>
-    `,
+    templateUrl: "delete-selected-items.html",
 })
 export class DeleteSelectedItemsDialogComponent {
     public parentId: string;
-
     public entityName: string;
-
     public items: string[];
 
     constructor(public dialogRef: MdDialogRef<DeleteSelectedItemsDialogComponent>) {
+    }
+
+    @autobind()
+    public destroyEntity() {
+        return Observable.of(this.dialogRef.close(true));
     }
 }
