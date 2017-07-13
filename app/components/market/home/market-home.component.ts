425f7ccb8f8734ead59288fbc3a2f892444b90bf
REDMOND\t-anjam <t-anjam@microsoft.com>
2017-07-13 11:27:52 -0700

    Added page navigation functionality to Market

--- app/components/market/home/market-home.component.ts
+++ app/components/market/home/market-home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component} from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { SidebarManager } from "../../base/sidebar";
 
@@ -8,8 +8,16 @@ import { SidebarManager } from "../../base/sidebar";
     templateUrl: "market-home.html",
 })
 export class MarketHomeComponent {
+    state='Page1';
+    selected="";
+    names = ["Maya Windows","Maya Linux","3DS Max Windows"];
+
     public static breadcrumb() {
         return { name: "Market" };
+    }
+
+    onClick(){
+        
     }
     constructor(
         sidebarManager: SidebarManager,
