0c93ff8e7b5512941379d32e101e16e628f59418
REDMOND\t-anjam <t-anjam@microsoft.com>
2017-07-20 15:14:46 -0700

    added Python RPC

--- app/components/market/home/market-home.component.ts
+++ app/components/market/home/market-home.component.ts
@@ -1,7 +1,8 @@
-import {Component} from "@angular/core";
+import { Component } from "@angular/core";
 import { FormGroup, FormControl } from "@angular/forms";
 import { FormBuilder } from "@angular/forms";
-
+import { PythonRpcService } from "app/services";
+//python python/main.py
 
 @Component({
     selector: "bl-market-home",
@@ -9,9 +10,10 @@ import { FormBuilder } from "@angular/forms";
 })
 export class MarketHomeComponent {
     form;
+    PythonService;
     state='Page1';
     selected="";
-    templates = [ 
+    templates = [
         {"name": "Maya Windows", "filecontent":  require("../templates/mayaSoftware-basic-windows.json")},
         {"name": "Maya Linux", "filecontent": require("../templates/mayaSoftware-basic-linux.json")},
         {"name": "Arnold Windows", "filecontent": require("../templates/arnold-basic-windows.json")},
@@ -24,6 +26,10 @@ export class MarketHomeComponent {
 
     onSubmit(formItem){
         console.log(formItem);
+        this.PythonService.call("submitNCJ", ["abc", "def",formItem]).subscribe({
+            next: (data) => console.log("Got NCJ", data),
+            error: (err) => console.log("Error NCJ", err),
+        });
     }
 
     getTemplate(name){
@@ -36,9 +42,9 @@ export class MarketHomeComponent {
     getParameters(name){
         var parameters =[]
         for (var i=0;i<this.templates.length;i++){
-            if (this.templates[i].name==name){
-                var parameterKeys = Object.keys(this.templates[i]["parameters"]);
-                for (var j=0;j<parameterKeys.length;j++){
+            if (this.templates[i].name ==name){
+                let parameterKeys = Object.keys(this.templates[i]["parameters"]);
+                for (let j=0;j<parameterKeys.length;j++){
                     parameters.push(parameterKeys[j]);
                 }
             }
@@ -47,10 +53,10 @@ export class MarketHomeComponent {
     }
 
     createForms(){
-        for (var i=0;i<this.templates.length;i++){
-            var parameterKeys = Object.keys(this.templates[i]["parameters"]);
-            var fg = {};
-            for (var j=0;j<parameterKeys.length;j++){
+        for (let i=0;i<this.templates.length;i++){
+            let parameterKeys = Object.keys(this.templates[i]["parameters"]);
+            let fg = {};
+            for (let j=0;j<parameterKeys.length;j++){
                 if ("defaultValue" in this.templates[i]["parameters"][parameterKeys[j]]){
                     fg[parameterKeys[j]] = new FormControl(String(this.templates[i]["parameters"][parameterKeys[j]]["defaultValue"]));
                 }
@@ -64,10 +70,11 @@ export class MarketHomeComponent {
     ngOnInit(){
         this.createForms();
     }
-    constructor(public formBuilder: FormBuilder) {
+    constructor(public formBuilder: FormBuilder, pythonRpcService: PythonRpcService) {
             console.log("Entered Market");
-            for (var i=0;i<this.templates.length;i++){
-                var file = JSON.parse(this.templates[i].filecontent);
+            this.PythonService = pythonRpcService;
+            for (let i=0;i<this.templates.length;i++){
+                let file = JSON.parse(this.templates[i].filecontent);
                 this.templates[i]["description"] = file.templateMetadata.description;
                 this.templates[i]["parameters"] = file.parameters;
             }
