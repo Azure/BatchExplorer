ff8788fa49ba4d343c26cda5cfb3b7f48ce637bf
REDMOND\t-anjam <t-anjam@microsoft.com>
2017-07-25 14:50:54 -0700

    auth changes

--- python/controllers/ncj_controller.py
+++ python/controllers/ncj_controller.py
@@ -1,22 +1,19 @@
 from server.app import app
 
 import azure.batch_extensions as batch
-from jsonrpc import JsonRpcErrorCodes, error
+from jsonrpc import JsonRpcErrorCodes, error, JsonRpcRequest
 from msrestazure.azure_active_directory import AdalAuthentication
+
+
 from azure.common.credentials import ServicePrincipalCredentials
 import azure.batch_extensions as batch
 from azure.batch_extensions import models
 
 @app.procedure("submitNCJ")
-def submitNCJ(params):
-    BATCH_ENDPOINT = "https://anasbatchaccount.eastus.batch.azure.com"
-    BATCH_ACCOUNT = "anasbatchaccount"
-    # Set up client
-    client = batch.BatchExtensionsClient(
-        base_url=BATCH_ENDPOINT,
-        batch_account=BATCH_ACCOUNT)
+def submitNCJ(request: JsonRpcRequest, template, parameters):
+    client = request.auth.client
 
-    job_json = client.job.expand_template(params[0], params[1])
+    job_json = client.job.expand_template(template, parameters)
     job = client.job.jobparameter_from_json(job_json)
     client.job.add(job)
-    return {"It": "Works!"}
+    return {"what": "it works2!"}
