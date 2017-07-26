bf3464520c6d643b93b936dfd8418d01ed237c3b
Timothee Guerin <tiguerin@microsoft.com>
2017-07-26 15:10:59 -0700

    Better home

--- app/components/market/home/market.component.ts
+++ app/components/market/home/market.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
 import { List } from "immutable";
+import { Subscription } from "rxjs";
 
 import { Application } from "app/models";
 import { NcjTemplateService } from "app/services";
@@ -9,14 +11,45 @@ import "./market.scss";
     selector: "bl-market",
     templateUrl: "market.html",
 })
-export class MarketComponent implements OnInit {
-    public applications: List<Application>;
+export class MarketComponent implements OnInit, OnDestroy {
+    public static breadcrumb() {
+        return { name: "Market" };
+    }
 
-    constructor(private templateService: NcjTemplateService) { }
+    public query: string = "";
+    public applications: List<Application>;
+    public displayedApplications: List<Application>;
+    public quicksearch = new FormControl("");
+
+    private _sub: Subscription;
+
+    constructor(private templateService: NcjTemplateService) {
+        this._sub = this.quicksearch.valueChanges.subscribe((query) => {
+            this.query = query;
+            this._filterApplications();
+        });
+    }
 
     public ngOnInit() {
         this.templateService.listApplications().subscribe((applications) => {
             this.applications = applications;
+            this._filterApplications();
         });
+    }
+
+    public ngOnDestroy() {
+        this._sub.unsubscribe();
+    }
+
+    /**
+     * Filter all the application according to the current filter.
+     * It will set the displayedApplication.
+     */
+    private _filterApplications() {
+        this.displayedApplications = List(this.applications.filter((application) => {
+            return this.query === ""
+                || application.id.contains(this.query)
+                || application.name.contains(this.query);
+        }));
     }
 }
