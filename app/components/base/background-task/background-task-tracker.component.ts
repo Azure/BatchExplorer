a528fec48fa518d1879a9a96ab154e66757ad678
Timothee Guerin <tiguerin@microsoft.com>
2017-08-01 16:39:36 -0700

    Show notification

--- app/components/base/background-task/background-task-tracker.component.ts
+++ app/components/base/background-task/background-task-tracker.component.ts
@@ -15,11 +15,29 @@ export class BackgroundTaskTrackerComponent {
     public currentTask: BackgroundTask;
     public otherTasks: List<BackgroundTask>;
 
+    /**
+     * When adding a new task we should flash to tell the user a new task is running
+     */
+    public showFlash = false;
+
+    private _lastTaskCount = 0;
+
     constructor(public taskManager: BackgroundTaskService) {
         taskManager.runningTasks.subscribe((tasks) => {
             this.currentTask = tasks.first();
             this.otherTasks = tasks.shift();
+            if (this._lastTaskCount < tasks.size) {
+                this.flash();
+            }
+            this._lastTaskCount = tasks.size;
         });
+    }
+
+    public flash() {
+        this.showFlash = true;
+        setTimeout(() => {
+            this.showFlash = false;
+        }, 1000);
     }
 }
 
