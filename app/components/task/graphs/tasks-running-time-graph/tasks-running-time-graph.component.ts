cf00d2d81814ba92d0ab53d56d1aafe32ad616ff
Timothee Guerin <tiguerin@microsoft.com>
2017-08-02 11:30:01 -0700

    Loading icon

--- app/components/task/graphs/tasks-running-time-graph/tasks-running-time-graph.component.ts
+++ app/components/task/graphs/tasks-running-time-graph/tasks-running-time-graph.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from "@angular/core";
 import { List } from "immutable";
+import * as moment from "moment";
 
-import { Job, Task } from "app/models";
+import { Job, Task, TaskState } from "app/models";
 import { TaskService } from "app/services";
 import { log } from "app/utils";
+import { FilterBuilder } from "app/utils/filter-builder";
 import "./tasks-running-time-graph.scss";
 
 interface TaskPoint {
@@ -15,7 +17,7 @@ interface TaskPoint {
     selector: "bl-tasks-running-time-graph",
     templateUrl: "tasks-running-time-graph.html",
 })
-export class TasksRunningTimeGraphComponent {
+export class TasksRunningTimeGraphComponent implements OnInit, OnChanges {
 
     @Input() public interactive: boolean = true;
     @Input() public job: Job;
@@ -38,6 +40,8 @@ export class TasksRunningTimeGraphComponent {
 
     public options: Chart.ChartOptions;
 
+    public loading = false;
+
     private _tasks: List<Task> = List([]);
     private _failedTasks: TaskPoint[];
     private _succeededTasks: TaskPoint[];
@@ -57,15 +61,33 @@ export class TasksRunningTimeGraphComponent {
     }
 
     public ngOnInit() {
-        // this.updateTasks();
+        // Nothing
+    }
+
+    public ngOnChanges(changes: SimpleChanges) {
+        if (changes.job) {
+
+            const { previousValue, currentValue } = changes.job;
+            if (!previousValue || previousValue.id !== currentValue.id) {
+                console.log("This change", changes.job);
+                this.updateTasks();
+            }
+        }
     }
 
     public updateTasks() {
+        console.log("Called this once");
+        this.loading = true;
         this.taskService.listAll(this.job.id, {
             select: "id,executionInfo",
+            filter: FilterBuilder.prop("state").eq(TaskState.completed).toOData(),
+            pageSize: 1000,
         }).subscribe({
             next: (tasks) => {
+                console.log("Got all tasks");
+                this.loading = false;
                 this._tasks = tasks;
+                this.updateData();
             },
             error: (error) => {
                 log.error(`Error retrieving all tasks for job ${this.job.id}`, error);
@@ -76,6 +98,7 @@ export class TasksRunningTimeGraphComponent {
         const hitRadius = this.interactive ? 3 : 0;
         this.options = {
             responsive: true,
+            animation: { duration: 0 },
             legend: {
                 display: false,
             },
@@ -110,7 +133,17 @@ export class TasksRunningTimeGraphComponent {
                     type: "linear",
                     ticks: {
                         min: 0,
-                        callback: (value) => { if (value % 1 === 0) { return value; } },
+                        callback: (value) => {
+                            if (value > 180) {
+                                if (value % 60 === 0) {
+                                    return value + "m";
+                                }
+                            } else {
+                                if (value % 1 === 0) {
+                                    return value + "s";
+                                }
+                            }
+                        },
                     },
                 }],
             },
@@ -145,7 +178,8 @@ export class TasksRunningTimeGraphComponent {
     }
 
     private _getTaskRunningTime(task: Task) {
-        return (Math.random() ** 2) * 50 + 30;
+        const { startTime, endTime } = task.executionInfo;
+        return Math.round(moment(endTime).diff(moment(startTime)) / 1000);
     }
 
     private _tasksToDataPoints(tasks: TaskPoint[]) {
