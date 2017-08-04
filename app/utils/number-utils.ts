02c7ea06fc534616d9b0efe46a13f28ec276a095
Timothee Guerin <tiguerin@microsoft.com>
2017-08-04 15:47:59 -0700

    Test wip

--- app/utils/number-utils.ts
+++ app/utils/number-utils.ts
@@ -1,5 +1,10 @@
 export class NumberUtils {
-    public static pretty(value: number): string {
-        return value.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    public static pretty(value: number, maxDecimals = 2): string {
+        const int = Math.floor(value);
+        const decimals = value - int;
+        const intStr = int.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        const decimalStr = Math.round(decimals * 10 ** maxDecimals);
+
+        return `${intStr}.${decimalStr}`;
     }
 }
