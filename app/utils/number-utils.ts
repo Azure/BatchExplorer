e38c0fbb5a6311e798170e1323e0d218b6068c8d
Timothee Guerin <timothee.guerin@outlook.com>
2017-08-15 12:57:33 -0700

    Fix labels and show more details (#613)

--- app/utils/number-utils.ts
+++ app/utils/number-utils.ts
@@ -7,4 +7,30 @@ export class NumberUtils {
         }
         return `${formated}.${decPart}`;
     }
+
+    /**
+     * Return the number ordinal suffix
+     * @param value
+     */
+    public static ordinal(value: number): string {
+        const j = value % 10;
+        const k = value % 100;
+        if (j === 1 && k !== 11) {
+            return "st";
+        }
+        if (j === 2 && k !== 12) {
+            return "nd";
+        }
+        if (j === 3 && k !== 13) {
+            return "rd";
+        }
+        return "th";
+    }
+    /**
+     * Return the number with the ordinal suffix
+     * @param value
+     */
+    public static nth(value: number): string {
+        return value.toString() + NumberUtils.ordinal(value);
+    }
 }
