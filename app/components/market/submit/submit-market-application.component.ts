ebef6098cfe1c7e03780455286b1bb85301caf9a
REDMOND\t-anjam <t-anjam@microsoft.com>
2017-08-22 15:28:06 -0700

    remove console logs

--- app/components/market/submit/submit-market-application.component.ts
+++ app/components/market/submit/submit-market-application.component.ts
@@ -72,9 +72,6 @@ export class SubmitMarketApplicationComponent implements OnInit {
             case Modes.NewPoolAndJob: {
                 obs = this.pythonRpcService.callWithAuth("expand-ncj-pool", [this.poolTemplate, this.poolParams.value])
                     .cascade((data) => this._runJobWithPool(data));
-                console.log(this.jobParametersWrapper);
-                console.log(this.jobParams.value);
-                console.log(this.jobTemplate);
                 break;
             }
             case Modes.OldPoolAndJob: {
