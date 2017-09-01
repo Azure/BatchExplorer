46c4a02ef0567f02254ef5127da39790e0c9385b
REDMOND\t-anjam <t-anjam@microsoft.com>
2017-09-01 14:15:20 -0700

    completed side menu

--- app/components/market/submit/side-menu.component.ts
+++ app/components/market/submit/side-menu.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input } from "@angular/core";
-import { FormControl, FormGroup } from "@angular/forms";
+import { Component, Input, OnChanges } from "@angular/core";
+import { FormGroup } from "@angular/forms";
+import { NcjPoolTemplate, ServerError } from "app/models";
+import { PricingService, PythonRpcService } from "app/services";
+import { NumberUtils } from "app/utils";
 import { Modes, NcjParameterWrapper } from "./market-application.model";
 import "./side-menu.scss";
 
@@ -7,11 +10,15 @@ import "./side-menu.scss";
     selector: "bl-side-menu",
     templateUrl: "side-menu.html",
 })
-export class SideMenuComponent {
+export class SideMenuComponent implements OnChanges {
     public static breadcrumb() {
         return { name: "Sidemenu" };
     }
     public Modes = Modes;
+    public estimatedCost = "-";
+    public modeText: string[] = ["None", "Run job with auto pool",
+        "Run job with existing pool", "Create pool for later use"];
+    public error: ServerError;
     @Input() public logo: string;
     @Input() public title: string;
     @Input() public action: string;
@@ -19,12 +26,19 @@ export class SideMenuComponent {
     @Input() public form: FormGroup;
     @Input() public poolParametersWrapper: NcjParameterWrapper[];
     @Input() public jobParametersWrapper: NcjParameterWrapper[];
+    @Input() public poolTemplate: NcjPoolTemplate;
 
-    public modeText: string[] = ["None", "Run job with auto pool",
-    "Run job with existing pool", "Create pool for later use"];
-
-    constructor() {
+    constructor(private pricingService: PricingService,
+                private pythonRpcService: PythonRpcService) {
         // do nothing
+    }
+
+    public ngOnChanges(changes) {
+        if (changes.form) {
+            this.form.valueChanges.subscribe((value) => {
+                this._updateEstimatedPrice();
+            });
+        }
     }
 
     public isJobComplete() {
@@ -45,4 +59,26 @@ export class SideMenuComponent {
         return this.form.value.pool.numberNodes && this.form.value.pool.vmSize;
     }
 
+    private _updateEstimatedPrice() {
+        if (this.isPoolComplete() && this.isCostComplete()) {
+            let obs;
+            this.error = null;
+            obs = this.pythonRpcService.callWithAuth("expand-ncj-pool", [this.poolTemplate, this.form.value.pool])
+                .cascade((data) => {
+                    data.vmSize = data.vmSize.toLowerCase();
+                    this.pricingService.computePoolPrice(data as any, { target: true }).subscribe((cost) => {
+                        if (cost) {
+                            this.estimatedCost = `${cost.unit} $${NumberUtils.pretty(cost.total)} / hour`;
+                        } else {
+                            this.estimatedCost = "-";
+                        }
+                    });
+            });
+            if (obs) {
+                obs.subscribe({
+                    error: (err) => this.error = ServerError.fromPython(err),
+                });
+            }
+        }
+    }
 }
