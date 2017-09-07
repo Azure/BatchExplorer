87942d8a0d0dda351fcc50b75d940d531f149b37
Timothee Guerin <tiguerin@microsoft.com>
2017-09-07 13:21:34 -0700

    View entity configuration as raw json

--- app/models/settings.ts
+++ app/models/settings.ts
@@ -2,9 +2,15 @@ export enum Theme {
     classic = "classic",
 }
 
+export enum EntityConfigurationView {
+    json = "json",
+    pretty = "pretty",
+}
+
 /**
  * Interface mapping how the settings should be
  */
 export interface Settings {
     fileTypes: StringMap<string[]>;
+    "configuration.default-view": EntityConfigurationView;
 }
