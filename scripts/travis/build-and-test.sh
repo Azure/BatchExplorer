cd7f0b887697470f2172b8ff984c716c95a75d05
Timothee Guerin <tiguerin@microsoft.com>
2017-09-12 13:58:08 -0700

    update to be ready to merge

--- scripts/travis/build-and-test.sh
+++ scripts/travis/build-and-test.sh
@@ -15,14 +15,13 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then # Only run the CI checks on the linux 
 fi
 
 # Only run prod build if on a branch build or PR for stable
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] || [ "${TRAVIS_BRANCH}" = "stable" ] || [ "${TRAVIS_BRANCH}" = "feature/publish"  ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] || [ "${TRAVIS_BRANCH}" = "stable" ]; then
     # Check third party notices is up to date
     npm run ts -s scripts/lca/generate-third-party -- --check
     # Build for production
 	npm run -s build:prod
     npm run -s build-python
-    npm run package -- --publish always --draft # TODO remove this
-    # npm run package -- --publish never # TODO replace with this
+    npm run package -- --publish never # TODO replace with this
 else
     if [[ $TRAVIS_OS_NAME == 'linux' ]]; then # Only run the CI checks on the linux build
         # Normal build
