4a9b6e0232143fe97242ac83797492a8694f3968
Timothee Guerin <tiguerin@microsoft.com>
2017-09-22 10:22:47 -0700

    Fix specs

--- test/app/components/job/graphs/job-graphs-home/helpers.spec.ts
+++ test/app/components/job/graphs/job-graphs-home/helpers.spec.ts
@@ -3,41 +3,47 @@ import { List } from "immutable";
 import { tasksToCsv } from "app/components/job/graphs/job-graphs-home/helpers";
 import { Task } from "app/models";
 
+const startDate1 = new Date(2017, 9, 9, 10, 23, 59);
+const endDate1 = new Date(2017, 9, 9, 11, 23, 59);
+
+const startDate2 = new Date(2017, 9, 9, 10, 22, 59);
+const endDate2 = new Date(2017, 9, 9, 11, 22, 23);
 describe("JobGraphsHome Helpers", () => {
     it("should convert to csv correctly", () => {
         const tasks = List([
             new Task({
                 id: "001", nodeInfo: { nodeId: "node-1" },
                 executionInfo: {
-                    startTime: new Date(2017, 9, 9, 10, 23, 59),
-                    endTime: new Date(2017, 9, 9, 11, 23, 59), exitCode: 0,
+                    startTime: startDate1,
+                    endTime: endDate1,
+                    exitCode: 0,
                 },
             } as any),
             new Task({
-                id: "001", nodeInfo: { nodeId: "node-1" },
+                id: "002", nodeInfo: { nodeId: "node-1" },
                 executionInfo: {
-                    startTime: new Date(2017, 9, 9, 10, 23, 59),
-                    endTime: new Date(2017, 9, 9, 12, 23, 59), exitCode: 0,
+                    startTime: startDate2,
+                    endTime: endDate2,
+                    exitCode: 0,
                 },
             } as any),
             new Task({
-                id: "001", nodeInfo: { nodeId: "node-1" },
+                id: "003", nodeInfo: { nodeId: "node-1" },
                 executionInfo: {
-                    startTime: new Date(2017, 9, 9, 10, 23, 59),
-                    endTime: new Date(2017, 9, 9, 13, 23, 59), exitCode: 0,
+                    startTime: startDate1,
+                    endTime: endDate1,
+                    exitCode: 1,
                 },
             } as any),
         ]);
         const csv = tasksToCsv(tasks);
 
-        // tslint:disable:max-line-length
         const expecetedCsv = [
             "Task id,Exit code,Start time,End time,Node id,Running time(ms)",
-            "001,0,Mon Oct 09 2017 10:23:59 GMT-0700 (Pacific Daylight Time),Mon Oct 09 2017 11:23:59 GMT-0700 (Pacific Daylight Time),node-1,3600000",
-            "001,0,Mon Oct 09 2017 10:23:59 GMT-0700 (Pacific Daylight Time),Mon Oct 09 2017 12:23:59 GMT-0700 (Pacific Daylight Time),node-1,7200000",
-            "001,0,Mon Oct 09 2017 10:23:59 GMT-0700 (Pacific Daylight Time),Mon Oct 09 2017 13:23:59 GMT-0700 (Pacific Daylight Time),node-1,10800000",
+            `001,0,${startDate1.toString()},${endDate1.toString()},node-1,3600000`,
+            `002,0,${startDate2.toString()},${endDate2.toString()},node-1,3564000`,
+            `003,1,${startDate1.toString()},${endDate1.toString()},node-1,3600000`,
         ].join("\n") + "\n";
-        // tslint:enable:max-line-length
 
         expect(csv).toEqual(expecetedCsv);
     });
