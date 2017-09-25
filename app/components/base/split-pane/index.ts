4bb6bc11343badda287f08731879030ea603e3c2
Timothee Guerin <tiguerin@microsoft.com>
2017-09-25 13:43:17 -0700

    V1 working

---
+++ app/components/base/split-pane/index.ts
@@ -0,0 +1,2 @@
+export * from "./split-pane.component";
+export * from "./split-pane.module";
---
+++ app/components/base/split-pane/split-pane.component.ts
@@ -0,0 +1,71 @@
+import { Component, ElementRef, HostListener, Input } from "@angular/core";
+
+import "./split-pane.scss";
+
+export interface SplitPaneConfig {
+    separatorThickness?: number;
+    firstPaneMinSize?: number;
+    secondPaneMinSize?: number;
+    initialDividerPosition?: number;
+}
+
+const defaultConfig: SplitPaneConfig = {
+    separatorThickness: 1,
+    firstPaneMinSize: 10,
+    secondPaneMinSize: 10,
+    initialDividerPosition: -1,
+};
+
+@Component({
+    selector: "bl-split-pane",
+    templateUrl: "split-pane.html",
+})
+export class SplitPaneComponent {
+    @Input() public set config(config: SplitPaneConfig) {
+        this._config = { ...defaultConfig, ...config };
+    }
+    public get config() { return this._config; }
+
+    public isResizing = false;
+
+    public firstPaneSize = "";
+    public secondPaneSize = "";
+
+    private _config: SplitPaneConfig = defaultConfig;
+
+    constructor(private elementRef: ElementRef) { }
+
+    public ngOnInit() {
+        this.updateSize(this.config.initialDividerPosition);
+    }
+
+    public handleStartResize(event: MouseEvent) {
+        console.log("Handle start resiz", event);
+        this.isResizing = true;
+    }
+
+    @HostListener("document:mouseup")
+    public stopResizing() {
+        this.isResizing = false;
+    }
+
+    @HostListener("mousemove", ["$event"])
+    public onMousemove(event: MouseEvent) {
+        const rect = this.elementRef.nativeElement.getBoundingClientRect();
+        if (this.isResizing) {
+            console.log("Event", event.clientX - rect.left);
+            this.updateSize(event.clientX - rect.left);
+        }
+    }
+
+    public updateSize(dividerPosition: number) {
+        if (dividerPosition === -1) {
+            this.firstPaneSize = "50%";
+            this.secondPaneSize = "50%";
+        } else {
+            this.firstPaneSize = `${dividerPosition}px`;
+            this.secondPaneSize = `calc(100% - ${dividerPosition}px)`;
+            console.log("FIrst", this.firstPaneSize, this.secondPaneSize);
+        }
+    }
+}
