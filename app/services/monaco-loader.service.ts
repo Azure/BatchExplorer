59911dde784924ce25fac769ccf133afd6965b04
Timothee Guerin <tiguerin@microsoft.com>
2017-11-14 15:17:15 -0800

    Missing file

---
+++ app/services/monaco-loader.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from "@angular/core";
+
+const anyWindow: any = window;
+
+@Injectable()
+export class MonacoLoader {
+    private _promise: Promise<any>;
+    constructor() {
+        this._promise = this.load();
+    }
+
+    public get(): Promise<any> {
+        return this._promise;
+    }
+
+    public load(): Promise<any> {
+        return new Promise((resolve) => {
+
+            const onGotAmdLoader = () => {
+                // anyWindow.amdRequire.config({ paths: { vs: "build/vendor/vs" } });
+                anyWindow.amdRequire.config({ baseUrl: "D:/dev/js/BatchLabs/build/vendor/" });
+                // workaround monaco-css not understanding the environment
+                (self as any).module = undefined;
+                // workaround monaco-typescript not understanding the environment
+                (self as any).process.browser = true;
+                anyWindow.amdRequire(["vs/editor/editor.main"], () => {
+                    resolve(anyWindow.monaco);
+                });
+            };
+
+            // Load AMD loader if necessary
+            if (!anyWindow.amdRequire) {
+                const nodeRequire = anyWindow.require;
+                let loaderScript = document.createElement("script");
+                loaderScript.type = "text/javascript";
+                loaderScript.src = "vendor/vs/loader.js";
+                loaderScript.addEventListener("load", () => {
+                    anyWindow.amdRequire = anyWindow.require;
+                    anyWindow.require = nodeRequire;
+                    onGotAmdLoader();
+                });
+                document.body.appendChild(loaderScript);
+            } else {
+                onGotAmdLoader();
+            }
+        });
+    }
+}
