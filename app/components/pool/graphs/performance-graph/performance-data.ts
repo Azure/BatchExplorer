aea981de06eed6e7ee5885ccbf795cf55496d627
Timothee Guerin <tiguerin@microsoft.com>
2017-11-16 12:15:50 -0800

    Performance data

--- app/components/pool/graphs/performance-graph/performance-data.ts
+++ app/components/pool/graphs/performance-graph/performance-data.ts
@@ -5,26 +5,51 @@ import {
     BatchPerformanceMetricType, BatchPerformanceMetrics, PerformanceMetric,
 } from "app/models/app-insights/metrics-result";
 import { AppInsightsQueryService } from "app/services";
+import { log } from "app/utils";
 
 export class PerformanceData {
-    public pool: Pool;
-    public historySize: number = 10;
+    public set pool(pool: Pool) {
+        this._pool = pool;
+        this.retrieveAppId();
+    }
+    public get pool() { return this._pool; }
 
+    public historySize: number = 10;
+    public appId: string = null;
     private _metrics = new BehaviorSubject<BatchPerformanceMetrics>({} as any);
+    private _pool: Pool;
 
     constructor(private appInsightsQueryService: AppInsightsQueryService) {
     }
 
     public update() {
-        const appId = "16e28541-cfd6-4848-976a-e6ac393aa4f3";
-        this.appInsightsQueryService.getPoolPerformance(appId, this.pool.id, this.historySize).subscribe((metrics) => {
-            this._metrics.next(metrics);
-        });
+        if (!this.appId) {
+            log.error("App id cannot be null!");
+        }
+        this.appInsightsQueryService.getPoolPerformance(this.appId, this.pool.id, this.historySize)
+            .subscribe((metrics) => {
+                this._metrics.next(metrics);
+            });
     }
 
     public observeMetric(name: BatchPerformanceMetricType): Observable<PerformanceMetric[]> {
         return this._metrics.map((metrics) => {
             return metrics[name] || [];
+        });
+    }
+
+    public retrieveAppId() {
+        const startTask = this.pool.startTask;
+        this.appId = null;
+        if (!startTask || startTask.environmentSettings.size === 0) {
+            return;
+        }
+
+        startTask.environmentSettings.forEach(({ name, value }) => {
+            if (name === "APP_INSIGHTS_APP_ID") {
+                this.appId = value;
+                return;
+            }
         });
     }
 }
