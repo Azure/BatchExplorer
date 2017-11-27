7ba0f518ab72382dac838d598ac0ad9123bba500
Yunlong Zhang <yunzh@microsoft.com>
2017-11-27 10:47:44 -0800

    Internal: Use route guard to prevent navigation until account is selected (#900)
    * added navigation guard
    
    * minor code review update

---
+++ app/components/base/guards/navigation-guard.component.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from "@angular/router";
+import { AccountService } from "app/services";
+
+@Injectable()
+export class NavigationGuard implements CanActivate  {
+    constructor(
+        private accountService: AccountService,
+    ) {
+    }
+
+    public canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        return this.accountService.currentAccountId.map((accountId) => {
+            return Boolean(accountId);
+        });
+    }
+}
