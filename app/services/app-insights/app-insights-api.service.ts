bbae95674f2088150a6d93d9369c82e06494828e
Timothee Guerin <tiguerin@microsoft.com>
2017-12-12 09:36:56 -0800

    Fix cr

--- app/services/app-insights/app-insights-api.service.ts
+++ app/services/app-insights/app-insights-api.service.ts
@@ -5,7 +5,7 @@ import { Observable } from "rxjs";
 
 import { ServerError } from "app/models";
 import { AccountService } from "app/services/account.service";
-import {  AdalService } from "app/services/adal";
+import { AdalService } from "app/services/adal";
 import { Constants } from "app/utils";
 import { AccessToken } from "client/core/aad/access-token";
 
