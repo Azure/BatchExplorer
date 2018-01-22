089cdb250e795c7fbc2bb71026645c929564cb87
Timothee Guerin <tiguerin@microsoft.com>
2018-01-22 15:00:53 -0800

    Updated changelog and version.

--- scripts/publish/github-api.ts
+++ scripts/publish/github-api.ts
@@ -38,8 +38,8 @@ export async function createIssue(repo: string, title: string, description: stri
 }
 
 export async function listPullRequests(repo: string, source: string, target = "master"): Promise<Issue[]> {
-    console.log("Listing prs", source, target);
-    const url = `${pullRequestsUrl(repo)}?head=${source}&base=${target}`;
+    const url = `${pullRequestsUrl(repo)}?head=Azure:${source}&base=${target}`;
+    console.log("Listing prs", source, target, url);
     return get(url);
 }
 
