7a495c59f3ecfc20c8ce4b71243f2197a8b5f27b
Timothee Guerin <tiguerin@microsoft.com>
2018-02-22 15:07:31 -0800

    more

--- app/components/base/browse-layout/browse-layout-list/browse-layout-list.directive.ts
+++ app/components/base/browse-layout/browse-layout-list/browse-layout-list.directive.ts
@@ -1,10 +1,14 @@
-import { Directive, Host, Self } from "@angular/core";
+import { Directive, Host, Optional, Self } from "@angular/core";
 import { ListBaseComponent } from "app/core/list";
 
 @Directive({
     selector: "[blBrowseLayoutList]",
 })
 export class BrowseLayoutListDirective {
-    constructor( @Host() @Self() public component: ListBaseComponent) {
+    constructor(@Host() @Self() @Optional() public component: ListBaseComponent) {
+        if (!component) {
+            throw new Error(`The component under this directive is not of type ListBaseComponent.`
+                + `Make sure you extends ListBaseComponent`);
+        }
     }
 }
