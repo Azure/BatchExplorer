6ab964681766841e17d1d1ea0f0013493e221686
Timothee Guerin <tiguerin@microsoft.com>
2018-02-23 09:52:48 -0800

    More

--- app/components/task/details/task-sub-tasks-tab.component.ts
+++ app/components/task/details/task-sub-tasks-tab.component.ts
@@ -1,37 +1,31 @@
-import { ChangeDetectorRef, Component, Input, ViewChild } from "@angular/core";
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnChanges, OnDestroy, ViewChild } from "@angular/core";
 import { autobind } from "app/core";
 import { Observable } from "rxjs";
 
 import { SubTaskDisplayListComponent } from "./sub-tasks";
 
 import { LoadingStatus } from "app/components/base/loading";
-import { SelectableList } from "app/components/base/selectable-list";
+import { ListBaseComponent } from "app/core/list";
 import { SubtaskInformation } from "app/models";
 import { SubtaskListParams, TaskService } from "app/services";
 import { ListView } from "app/services/core";
+import { List } from "immutable";
 
 @Component({
     selector: "bl-task-sub-tasks-tab",
     templateUrl: "task-sub-tasks-tab.html",
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TaskSubTasksTabComponent extends SelectableList {
-    @Input()
-    public quickList: boolean;
-
-    @Input()
-    public set jobId(value: string) {
+export class TaskSubTasksTabComponent extends ListBaseComponent implements OnChanges, OnDestroy {
+    @Input() public set jobId(value: string) {
         this._jobId = (value && value.trim());
     }
     public get jobId() { return this._jobId; }
 
-    @Input()
-    public set taskId(value: string) {
+    @Input() public set taskId(value: string) {
         this._taskId = (value && value.trim());
-        this.refresh();
     }
     public get taskId() { return this._jobId; }
-
-    public get selectedTask() { return this._selectedTask; }
 
     @ViewChild(SubTaskDisplayListComponent)
     public list: SubTaskDisplayListComponent;
@@ -39,31 +33,52 @@ export class TaskSubTasksTabComponent extends SelectableList {
     public data: ListView<SubtaskInformation, SubtaskListParams>;
     public status: Observable<LoadingStatus>;
 
+    public selectedTask: SubtaskInformation;
+    public selectedTaskId: string;
+    public subTasks: List<SubtaskInformation>;
+
     private _jobId: string;
     private _taskId: string;
-    private _selectedTask: SubtaskInformation;
 
-    constructor(private taskService: TaskService, private changeDetectorRef: ChangeDetectorRef) {
-        super();
+    constructor(private taskService: TaskService, changeDetector: ChangeDetectorRef) {
+        super(changeDetector);
 
         this.data = this.taskService.listSubTasksView({});
-        // TODO-TIM Check dis
-        // this.activatedItemChange.subscribe((item) => {
-        //     this.data.items.subscribe((subTasks) => {
-        //         this._selectedTask = subTasks.find((subTask) => {
-        //             return subTask.id === item.key;
-        //         });
-        //     });
-        // });
+        this.data.items.subscribe((subTasks) => {
+            this.subTasks = subTasks;
+            this._updateSelectedTask();
+            this.changeDetector.markForCheck();
+        });
+        this.status = this.data.status;
+    }
+
+    public ngOnChanges(changes) {
+        if (changes.jobId || changes.taskId) {
+            this.refresh();
+        }
+    }
+    public ngOnDestroy() {
+        this.data.dispose();
     }
 
     @autobind()
     public refresh(): Observable<any> {
         this.data.params = { jobId: this._jobId, taskId: this._taskId };
-        this._selectedTask = null;
-        this.status = this.data.status;
-        this.changeDetectorRef.detectChanges();
-
+        this.selectedTask = null;
+        this.selectedTaskId = null;
+        this.changeDetector.markForCheck();
         return this.data.fetchNext();
+    }
+
+    public selectedTaskChanged(id: string) {
+        this.selectedTaskId = id;
+        this._updateSelectedTask();
+        this.changeDetector.markForCheck();
+    }
+
+    private _updateSelectedTask() {
+        this.selectedTask = this.subTasks.find((subTask) => {
+            return subTask.id === this.selectedTaskId;
+        });
     }
 }
