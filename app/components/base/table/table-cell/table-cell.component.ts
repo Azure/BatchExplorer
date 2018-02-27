2fb28055645afb3fc644714b5001ec0b199d7321
Timothee Guerin <tiguerin@microsoft.com>
2018-02-27 14:24:12 -0800

    Fix

--- app/components/base/table/table-cell/table-cell.component.ts
+++ app/components/base/table/table-cell/table-cell.component.ts
@@ -6,13 +6,8 @@ import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TableCellComponent {
-    @Input()
-    public set value(value: string) {
-        this._value = value;
-    }
-    public get value() {
-        return this._value;
-    }
-
-    private _value: string;
+    /**
+     * Specify the value here if its different from the content
+     */
+    @Input() public value: string;
 }
