799cb8e7eeb13a6f172a960a0483689956f1b4f6
Timothee Guerin <tiguerin@microsoft.com>
2018-03-08 10:20:01 -0800

    Fix job, pool, job schedule

--- app/components/pool/browse/pool-list.component.ts
+++ app/components/pool/browse/pool-list.component.ts
@@ -56,6 +56,7 @@ export class PoolListComponent extends ListBaseComponent implements OnInit, OnDe
         ComponentUtils.setActiveItem(activatedRoute, this.data);
 
         this.data.status.subscribe((status) => {
+            console.log("Status", status);
             this.status = status;
         });
 
