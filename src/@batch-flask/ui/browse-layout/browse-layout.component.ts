2556d7ed08b03825d99b6561d9e4ee48c7b6f652
Timothee Guerin <tiguerin@microsoft.com>
2018-03-23 09:20:30 -0700

    Fix: selecting item close the filter

--- src/@batch-flask/ui/browse-layout/browse-layout.component.ts
+++ src/@batch-flask/ui/browse-layout/browse-layout.component.ts
@@ -161,6 +161,8 @@ export class BrowseLayoutComponent implements AfterContentInit {
     }
 
     public updateActiveItem(key: string) {
+        console.log("Updated activated item??/");
+        this.toggleFilter(false);
         this._activeItemKey = key;
         if (this.listDirective) {
             this.listDirective.component.activeItem = key;
