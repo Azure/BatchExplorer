1f1c8d11e0fcd766d7d12c6d854f49a3c6166a01
ascobie <andrew.scobie@microsoft.com>
2018-03-29 10:01:36 +1300

    Setup file-group sync via plugin before submit (#1214)
    * hard code parameters to test working
    
    * pretty much where i want it will test on monday
    
    * temp update to start url
    
    * happy that it's working
    
    * fix tests
    
    * remove application launch params
    
    * stop post back switching mode when editing pools form
    
    * test
    
    * change the color of the info text
    
    * remove redirect checkbox, change mode to job after pool creation if there is a job template
    
    * change create file group title

--- src/common/constants/constants.ts
+++ src/common/constants/constants.ts
@@ -33,7 +33,7 @@ export const forms = {
             displayName: 1024,
             applicationName: 64,
             version: 64,
-            fileGroup: 63,
+            fileGroup: 55,
         },
         regex: {
             id: /^[\w\_-]+$/i,
@@ -198,5 +198,8 @@ export const ListPageSizes = {
 };
 
 export const KnownQueryParameters = {
-    useAutoPool: "useAutoPool",
+    useAutoPool: "auto-pool",
+    inputParameter: "input-parameter",
+    assetContainer: "asset-container",
+    assetPaths: "asset-paths",
 };
