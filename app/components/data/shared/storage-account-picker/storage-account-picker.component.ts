47e3db7fe12093278675de0783d7d59a5f87a8b2
Timothee Guerin <tiguerin@microsoft.com>
2018-04-06 09:16:43 -0700

    Special for auto storage

--- app/components/data/shared/storage-account-picker/storage-account-picker.component.ts
+++ app/components/data/shared/storage-account-picker/storage-account-picker.component.ts
@@ -1,9 +1,11 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, forwardRef } from "@angular/core";
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit, forwardRef } from "@angular/core";
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
 import { AccountService, StorageAccountService } from "app/services";
 import { List } from "immutable";
 
 import { StorageAccount } from "app/models";
+import { AutoStorageService } from "app/services/storage";
+import { Subscription } from "rxjs";
 import "./storage-account-picker.scss";
 
 @Component({
@@ -15,26 +17,39 @@ import "./storage-account-picker.scss";
         { provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => StorageAccountPickerComponent), multi: true },
     ],
 })
-export class StorageAccountPickerComponent implements OnInit, ControlValueAccessor {
+export class StorageAccountPickerComponent implements OnInit, OnDestroy, ControlValueAccessor {
+    public autoStorageAccountId: string;
     public loading: boolean = true;
     public storageAccounts: List<StorageAccount>;
     public pickedStorageAccountId: string;
     private _propagateChange: (value: string) => void;
+    private _sub: Subscription;
+    private _storageAccounts: List<StorageAccount>;
 
     constructor(
+        private autoStorageService: AutoStorageService,
         private batchAccountService: AccountService,
         private storageAccountService: StorageAccountService,
         private changeDetector: ChangeDetectorRef) {
+
+        this._sub = this.autoStorageService.storageAccountId.subscribe((id) => {
+            this.autoStorageAccountId = id;
+            this._updateStorageAccounts();
+        });
     }
 
     public ngOnInit() {
         this.batchAccountService.currentAccount.first().subscribe((currentAccount) => {
             this.storageAccountService.list(currentAccount.subscription.subscriptionId).subscribe((storageAccounts) => {
-                this.storageAccounts = storageAccounts;
+                this._storageAccounts = storageAccounts;
                 this.loading = false;
-                this.changeDetector.markForCheck();
+                this._updateStorageAccounts();
             });
         });
+    }
+
+    public ngOnDestroy() {
+        this._sub.unsubscribe();
     }
 
     public pickStorageAccountId(storageAccountId: string) {
@@ -59,5 +74,28 @@ export class StorageAccountPickerComponent implements OnInit, ControlValueAccess
 
     public trackStorageAccount(index, storageAccount: StorageAccount) {
         return storageAccount.id;
+    }
+
+    private _updateStorageAccounts() {
+        let autoStorageAccount;
+        const accounts = [];
+        this._storageAccounts.forEach((account) => {
+            if (account.id === this.autoStorageAccountId) {
+                autoStorageAccount = new StorageAccount({
+                    ...account.toJS(),
+                    name: `${account.name} (Auto storage)`,
+                });
+            } else {
+                accounts.push(account);
+            }
+        });
+        accounts.sortBy(x => x.name);
+        if (autoStorageAccount) {
+            accounts.unshift(autoStorageAccount);
+        }
+        this.storageAccounts = List(accounts);
+
+        this.changeDetector.markForCheck();
+
     }
 }
--- src/@batch-flask/extensions/array.ts
+++ src/@batch-flask/extensions/array.ts
@@ -17,6 +17,12 @@ interface Array<T> {
      * @example [[1, 2], [3, 4]] => [1, 2, 3, 4];
      */
     flatten(): any[];
+
+    /**
+     * Sort by the given attribute
+     */
+
+    sortBy(attr: (item: T) => any);
 }
 
 // First, checks if it isn't implemented yet.
@@ -34,5 +40,22 @@ if (!Array.prototype.last) {
 if (!Array.prototype.flatten) {
     Array.prototype.flatten = function <T>(this: T[]) {
         return [].concat(...this);
+    };
+}
+
+if (!Array.prototype.sortBy) {
+    Array.prototype.sortBy = function <T>(this: T[], attr: (item: T) => any) {
+        return this.sort((a, b) => {
+            const aAttr = attr(a);
+            const bAttr = attr(b);
+
+            if (aAttr < bAttr) {
+                return -1;
+            } else if (aAttr > bAttr) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
     };
 }
