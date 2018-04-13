cc8b790ca49ae431d9b775498e46bc49d0124865
Timothee Guerin <tiguerin@microsoft.com>
2018-04-13 13:53:57 -0700

    Set user agent to Mozilla

--- src/@batch-flask/utils/fetch/fetch.ts
+++ src/@batch-flask/utils/fetch/fetch.ts
@@ -1,5 +1,5 @@
 import * as HttpsProxyAgent from "https-proxy-agent";
-import nodeFetch, { RequestInit } from "node-fetch";
+import nodeFetch, { Headers, RequestInit } from "node-fetch";
 
 import { log } from "@batch-flask/utils/logging";
 
@@ -9,11 +9,27 @@ export async function fetch(url: string, options: RequestInit = {}) {
     };
     if (!options.agent && process.env.HTTP_PROXY) {
         // TODO-TIM remove
-        log.info("Calling node fetch with HTTP_PROXY", process.env.HTTP_PROXY);
+        log.info("Calling node fetch with HTTP_PROXY");
         instanceOptions.agent = new HttpsProxyAgent(process.env.HTTP_PROXY);
     } else {
         log.info("Calling node fetch without HTTP_PROXY");
     }
 
-    return nodeFetch(url, instanceOptions);
+    if (!options.headers) {
+        options.headers = new Headers();
+    }
+    if (options.headers instanceof Headers) {
+        options.headers.set("User-Agent", "Mozilla/5.0");
+    } else {
+        options.headers["User-Agent"] = "Mozilla/5.0";
+    }
+
+    const response = await nodeFetch(url, instanceOptions);
+    if (response.status >= 200 && response.status < 300) {
+        return response;
+    } else {
+        const content = await response.text();
+        log.error(`Error fetch ${url}`, content);
+        throw response;
+    }
 }
