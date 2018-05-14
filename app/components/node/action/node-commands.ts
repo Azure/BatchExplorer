e5f2d3d0a2482b597c433b9065419fd183d799ab
Yunlong Zhang <yunzh@microsoft.com>
2018-05-14 13:43:34 -0700

    Added node command list

---
+++ app/components/node/action/node-commands.ts
@@ -0,0 +1,110 @@
+import { Injectable, Injector } from "@angular/core";
+import { DialogService, EntityCommand, EntityCommands, Permission } from "@batch-flask/ui";
+
+import { SidebarManager } from "@batch-flask/ui/sidebar";
+import { StartTaskEditFormComponent } from "app/components/pool/start-task";
+import { Node, Pool } from "app/models";
+import { NodeService, PoolParams } from "app/services";
+import { EntityView } from "app/services/core";
+import { NodeConnectComponent } from "../connect";
+
+@Injectable()
+export class NodeCommands extends EntityCommands<Node> {
+    public connect: EntityCommand<Node, void>;
+    public delete: EntityCommand<Node, void>;
+    public reboot: EntityCommand<Node, void>;
+    public editStartTask: EntityCommand<Node, void>;
+    public uploadLog: EntityCommand<Node, void>;
+
+    public pool: Pool;
+    public poolData: EntityView<Pool, PoolParams>;
+
+    constructor(
+        injector: Injector,
+        private nodeService: NodeService,
+        private dialog: DialogService,
+        private sidebarManager: SidebarManager) {
+        super(
+            injector,
+            "Node",
+        );
+        this._buildCommands();
+    }
+
+    public get(nodeId: string) {
+        return this.nodeService.get(this.params["poolId"], nodeId);
+    }
+
+    public getFromCache(nodeId: string) {
+        return this.nodeService.getFromCache(this.params["poolId"], nodeId);
+    }
+
+    private _buildCommands() {
+        this.connect = this.simpleCommand({
+            label: "Connect to node",
+            icon: "fa fa-plug",
+            action: (node) => this._connect(node),
+            multiple: false,
+            confirm: false,
+            notify: false,
+            permission: Permission.Write,
+        });
+
+        this.delete = this.simpleCommand({
+            label: "Delete",
+            icon: "fa fa-trash-o",
+            action: (node: Node) => this._delete(node),
+            permission: Permission.Write,
+        });
+
+        this.reboot = this.simpleCommand({
+            label: "Reboot node",
+            icon: "fa fa-power-off",
+            action: (node: Node) => this._reboot(node),
+            permission: Permission.Write,
+        });
+
+        this.editStartTask = this.simpleCommand({
+            label: "Edit start task",
+            icon: "fa fa-pencil-square-o",
+            action: (node) => this._editStartTask(node),
+            multiple: false,
+            confirm: false,
+            notify: false,
+            permission: Permission.Write,
+        });
+
+        this.commands = [
+            this.connect,
+            this.delete,
+            this.reboot,
+            this.editStartTask,
+        ];
+    }
+
+    private _connect(node: Node) {
+        const ref = this.sidebarManager.open(`connect-node-${node.id}`, NodeConnectComponent);
+        ref.component.node = node;
+        ref.component.pool = this.pool;
+    }
+
+    private _reboot(node: Node) {
+        return this.nodeService.reboot(this.pool.id, node.id)
+            .cascade(() => this.nodeService.get(this.pool.id, node.id));
+    }
+
+    private _delete(node: Node) {
+        this.dialog.confirm("Are you sure you want to delete this node?", {
+            yes: () => {
+                return this.nodeService.delete(this.pool.id, node.id)
+                    .cascade(() => this.nodeService.get(this.pool.id, node.id));
+            },
+        });
+    }
+
+    private _editStartTask(node: Node) {
+        const ref = this.sidebarManager.open(`edit-start-task-${this.pool.id}`, StartTaskEditFormComponent);
+        ref.component.pool = this.pool;
+        ref.component.fromNode = node.id;
+    }
+}
