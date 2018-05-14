938d57163d3e31ab410761a1de83e4e5cf0531c3
Yunlong Zhang <yunzh@microsoft.com>
2018-05-14 11:07:40 -0700

    Update application command list

--- app/components/application/action/batch-application-commands.ts
+++ app/components/application/action/batch-application-commands.ts
@@ -1,13 +1,15 @@
 import { Injectable, Injector } from "@angular/core";
-import { EntityCommand, EntityCommands } from "@batch-flask/ui";
+import { EntityCommand, EntityCommands, Permission } from "@batch-flask/ui";
 
 import { SidebarManager } from "@batch-flask/ui/sidebar";
 import { BatchApplication } from "app/models";
 import { ApplicationService, PinnedEntityService } from "app/services";
+import { ApplicationCreateDialogComponent } from "./create";
 import { ApplicationEditDialogComponent } from "./edit";
 
 @Injectable()
 export class BatchApplicationCommands extends EntityCommands<BatchApplication> {
+    public add: EntityCommand<BatchApplication, void>;
     public edit: EntityCommand<BatchApplication, void>;
     public delete: EntityCommand<BatchApplication, void>;
     public pin: EntityCommand<BatchApplication, void>;
@@ -34,20 +36,39 @@ export class BatchApplicationCommands extends EntityCommands<BatchApplication> {
     }
 
     private _buildCommands() {
+        this.add = this.simpleCommand({
+            label: "Add package",
+            icon: "fa fa-plus",
+            action: (application) => this._addPackage(application),
+            multiple: false,
+            confirm: false,
+            notify: false,
+            permission: Permission.Write,
+        });
+
         this.edit = this.simpleCommand({
             label: "Edit",
+            icon: "fa fa-edit",
             action: (application) => this._editApplication(application),
             multiple: false,
             confirm: false,
             notify: false,
+            permission: Permission.Write,
         });
+
         this.delete = this.simpleCommand({
             label: "Delete",
+            icon: "fa fa-trash-o",
             action: (application: BatchApplication) => this.applicationService.delete(application.id),
+            permission: Permission.Write,
         });
+
         this.pin = this.simpleCommand({
             label: (application: BatchApplication) => {
                 return this.pinnedEntityService.isFavorite(application) ? "Unpin favorite" : "Pin to favorites";
+            },
+            icon: (application: BatchApplication) => {
+                return this.pinnedEntityService.isFavorite(application) ? "fa fa-chain-broken" : "fa fa-link";
             },
             action: (application: BatchApplication) => this._pinApplication(application),
             confirm: false,
@@ -55,10 +76,19 @@ export class BatchApplicationCommands extends EntityCommands<BatchApplication> {
         });
 
         this.commands = [
+            this.add,
             this.edit,
             this.delete,
             this.pin,
         ];
+    }
+
+    private _addPackage(application: BatchApplication) {
+        const sidebarRef = this.sidebarManager.open("add-package", ApplicationCreateDialogComponent);
+        sidebarRef.component.setValue(application);
+        sidebarRef.afterCompletion.subscribe(() => {
+            this.get(application.id);
+        });
     }
 
     private _editApplication(application: BatchApplication) {
