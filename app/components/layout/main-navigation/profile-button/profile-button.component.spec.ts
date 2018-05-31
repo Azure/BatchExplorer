7bbbae94ea50a5ba33fa898753375dd7b2e9bb9b
Timothee Guerin <tiguerin@microsoft.com>
2018-05-31 16:46:01 -0700

    Added profile button tests

--- app/components/layout/main-navigation/profile-button/profile-button.component.spec.ts
+++ app/components/layout/main-navigation/profile-button/profile-button.component.spec.ts
@@ -1,7 +1,19 @@
 import { Component, DebugElement } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { MatTooltip, MatTooltipModule } from "@angular/material";
 import { By } from "@angular/platform-browser";
-
+import { RouterTestingModule } from "@angular/router/testing";
+import {
+    AutoUpdateService,
+    ClickableComponent,
+    ElectronRemote,
+    ElectronShell,
+    UpdateStatus,
+} from "@batch-flask/ui";
+import { AdalService, BatchLabsService, FileSystemService } from "app/services";
+import { BehaviorSubject } from "rxjs";
+import { click } from "test/utils/helpers";
+import { ContextMenuServiceMock, NotificationServiceMock } from "test/utils/mocks";
 import { ProfileButtonComponent } from "./profile-button.component";
 
 @Component({
@@ -10,24 +22,103 @@ import { ProfileButtonComponent } from "./profile-button.component";
 class TestComponent {
 }
 
-fdescribe("profile-button", () => {
+describe("ProfileButtonComponent", () => {
     let fixture: ComponentFixture<TestComponent>;
-    let testComponent: TestComponent;
-    let component: ProfileButtonComponent;
     let de: DebugElement;
+    let clickableEl: DebugElement;
+    let adalServiceSpy;
+    let autoUpdateServiceSpy;
+    let batchLabsServiceSpy;
+    let contextMenuServiceSpy: ContextMenuServiceMock;
+    let notificationServiceSpy: NotificationServiceMock;
 
     beforeEach(() => {
+        contextMenuServiceSpy = new ContextMenuServiceMock();
+        notificationServiceSpy = new NotificationServiceMock();
+        adalServiceSpy = {
+            currentUser: new BehaviorSubject(null),
+        };
+
+        autoUpdateServiceSpy = {
+            status: new BehaviorSubject<UpdateStatus>(null),
+        };
+
+        batchLabsServiceSpy = {};
         TestBed.configureTestingModule({
-            imports: [],
-            declarations: [ProfileButtonComponent, TestComponent],
+            imports: [MatTooltipModule, RouterTestingModule],
+            declarations: [ProfileButtonComponent, ClickableComponent, TestComponent],
+            providers: [
+                { provide: AdalService, useValue: adalServiceSpy },
+                { provide: AutoUpdateService, useValue: autoUpdateServiceSpy },
+                { provide: BatchLabsService, useValue: batchLabsServiceSpy },
+                { provide: ElectronShell, useValue: null },
+                { provide: ElectronRemote, useValue: null },
+                { provide: FileSystemService, useValue: null },
+                contextMenuServiceSpy.asProvider(),
+                notificationServiceSpy.asProvider(),
+            ],
         });
         fixture = TestBed.createComponent(TestComponent);
-        testComponent = fixture.componentInstance;
         de = fixture.debugElement.query(By.css("bl-profile-button"));
-        component = de.componentInstance;
+
+        clickableEl = fixture.debugElement.query(By.css("bl-clickable"));
         fixture.detectChanges();
     });
 
-    it("", () => {
+    it("shows the current user info in tooltip", () => {
+        adalServiceSpy.currentUser.next({
+            name: "Some Name",
+            unique_name: "some.name@example.com",
+        });
+        fixture.detectChanges();
+        const tooltip: MatTooltip = clickableEl.injector.get(MatTooltip);
+        expect(tooltip.message).toBe("Some Name (some.name@example.com)");
+    });
+
+    describe("update status", () => {
+        it("flash checking icon when checking for update", () => {
+            autoUpdateServiceSpy.status.next(UpdateStatus.Checking);
+            fixture.detectChanges();
+            const notificationOverlay = de.query(By.css(".notification-overlay"));
+            expect(notificationOverlay).not.toBeFalsy();
+            expect(notificationOverlay.nativeElement.classList).toContain("checking");
+            expect(notificationOverlay.nativeElement.classList).not.toContain("downloading");
+            expect(notificationOverlay.nativeElement.classList).not.toContain("ready");
+        });
+
+        it("shows downloading icon when downloading update", () => {
+            autoUpdateServiceSpy.status.next(UpdateStatus.Downloading);
+            fixture.detectChanges();
+            const notificationOverlay = de.query(By.css(".notification-overlay"));
+            expect(notificationOverlay).not.toBeFalsy();
+            expect(notificationOverlay.nativeElement.classList).not.toContain("checking");
+            expect(notificationOverlay.nativeElement.classList).toContain("downloading");
+            expect(notificationOverlay.nativeElement.classList).not.toContain("ready");
+        });
+
+        it("shows ready icon when update is ready to be installed", () => {
+            autoUpdateServiceSpy.status.next(UpdateStatus.Ready);
+            fixture.detectChanges();
+            const notificationOverlay = de.query(By.css(".notification-overlay"));
+            expect(notificationOverlay).not.toBeFalsy();
+            expect(notificationOverlay.nativeElement.classList).not.toContain("checking");
+            expect(notificationOverlay.nativeElement.classList).not.toContain("downloading");
+            expect(notificationOverlay.nativeElement.classList).toContain("ready");
+        });
+
+        it("doesn't show any overlay when no updates are available", () => {
+            autoUpdateServiceSpy.status.next(UpdateStatus.NotAvailable);
+            fixture.detectChanges();
+            const notificationOverlay = de.query(By.css(".notification-overlay"));
+            expect(notificationOverlay).toBeFalsy();
+        });
+    });
+
+    it("show context menu when clicking on it", () => {
+        click(clickableEl);
+        fixture.detectChanges();
+        expect(contextMenuServiceSpy.openMenu).toHaveBeenCalledOnce();
+        const items = contextMenuServiceSpy.lastMenu.items;
+        expect(items.length).toBe(11);
     });
 });
