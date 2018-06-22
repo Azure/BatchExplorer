c2cd177e88227a1fc26614b756380fdefae28488
Timothee Guerin <tiguerin@microsoft.com>
2018-06-22 16:01:15 -0700

    job schedule

--- app/services/azure-batch/index.ts
+++ app/services/azure-batch/index.ts
@@ -1,6 +1,7 @@
 export * from "./file";
 export * from "./certificate";
 export * from "./job";
+export * from "./job-schedule";
 export * from "./node";
 export * from "./pool";
 export * from "./task";
--- app/services/batch-api/batch-client-proxy.ts
+++ app/services/batch-api/batch-client-proxy.ts
@@ -1,7 +1,5 @@
 import { BatchServiceClient } from "azure-batch";
 
-import { JobScheduleProxy } from "./jobScheduleProxy";
-
 export interface Options {
     account: string;
     key: string;
@@ -11,23 +9,7 @@ export interface Options {
 export class BatchClientProxy {
     public client: BatchServiceClient;
 
-    private _jobSchedule: JobScheduleProxy;
-
     constructor(credentials, url) {
         this.client = new BatchServiceClient(credentials, url);
-
-        this._jobSchedule = new JobScheduleProxy(this.client);
-    }
-
-    get jobSchedule(): JobScheduleProxy {
-        return this.checkProxy(this._jobSchedule);
-    }
-
-    private checkProxy(proxy: any): any {
-        if (!proxy) {
-            throw new Error("BatchClientProxy has not been initialized, please call setOptions(options)");
-        }
-
-        return proxy;
     }
 }
