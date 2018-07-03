e49d9d407b8cbf49bef0d47c52dc70216ce70e8d
Andrew Scobie <ascobie@microsoft.com>
2018-07-03 15:12:25 +1200

    moar definitions

--- app/models/workspace-definition.ts
+++ app/models/workspace-definition.ts
@@ -1,4 +1,13 @@
-// TODO: expand on these.
+/**
+ * Define the visible state of conponents in the UI
+ */
+export interface WorkspaceDefinition {
+    id: string;
+    displayName: string;
+    description: string;
+    features: FeatureDefinition;
+}
+
 export interface FeatureDefinition {
     job: boolean | JobFeatures;
     schedule: boolean | ScheduleFeatures;
@@ -11,11 +20,29 @@ export interface FeatureDefinition {
 
 export interface CommonFeatures {
     view: boolean | any;
-    add: boolean | any;
+    action: boolean | any;
 }
 
-export interface JobFeatures extends CommonFeatures {
+// TODO: Still a little unsure if this is correct.
+export interface JobFeatures  extends CommonFeatures {
+    view: {
+        graphs: boolean;
+        statistics: boolean;
+        tags: boolean;
+        asJson: boolean;
+    };
 
+    action: {
+        add: boolean;
+        delete: boolean;
+        edit: boolean;
+        addTask: boolean;
+        terminate: boolean;
+        clone: boolean;
+        schedule: boolean;
+        export: boolean;
+        pin: boolean;
+    };
 }
 
 export interface ScheduleFeatures extends CommonFeatures {
@@ -23,31 +50,65 @@ export interface ScheduleFeatures extends CommonFeatures {
 }
 
 export interface PoolFeatures extends CommonFeatures {
+    view: {
+        cost: boolean;
+        tags: boolean;
+        asJson: boolean;
+        availableNodes: boolean;
+        runningTasks: boolean;
+        appInsights: boolean;
+        nodes: boolean;
+    };
 
+    action: {
+        add: boolean;
+        delete: boolean;
+        addJob: boolean;
+        resize: boolean;
+        clone: boolean;
+        export: boolean;
+        pin: boolean;
+    };
 }
 
 export interface PackageFeatures extends CommonFeatures {
-
+    action: {
+        add: boolean;
+        delete: boolean;
+        addVersion: boolean;
+        edit: boolean;
+        pin: boolean;
+    };
 }
 
 export interface CertificateFeatures extends CommonFeatures {
-
+    action: {
+        add: boolean;
+        delete: boolean;
+        reactivate: boolean;
+        export: boolean;
+        pin: boolean;
+    };
 }
 
 export interface DataFeatures extends CommonFeatures {
+    view: {
+        fileGroups: boolean;
+        containers: boolean;
+        accounts: boolean;
+    };
 
+    action: {
+        addContainer: boolean;
+        deleteContainer: boolean;
+        addFileGroup: boolean;
+        deleteFileGroup: boolean;
+        modify: boolean;
+        download: boolean;
+        pin: boolean;
+    };
 }
 
 export interface GalleryFeatures extends CommonFeatures {
 
-}
-
-/**
- * Define the visible state of conponents in the UI
- */
-export interface WorkspaceDefinition {
-    id: string;
-    displayName: string;
-    description: string;
-    features: FeatureDefinition;
 }
