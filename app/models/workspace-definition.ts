c6946b62c0fddaf7a6e227958945bc815e9a96ba
Nehal Jain <t-nejain@microsoft.com>
2018-07-03 13:51:32 +1200

    expanded workspace definitions with interfaces for each CommonFeature

--- app/models/workspace-definition.ts
+++ app/models/workspace-definition.ts
@@ -1,26 +1,54 @@
-export interface WorkspaceMetaData {
-    id: string;
-    displayName: string;
-    description: string;
+// TODO: expand on these.
+export interface FeatureDefinition {
+    job: boolean | JobFeatures;
+    schedule: boolean | ScheduleFeatures;
+    pool: boolean | PoolFeatures;
+    package: boolean | PackageFeatures;
+    certificate: boolean | CertificateFeatures;
+    data: boolean | DataFeatures;
+    gallery: boolean | GalleryFeatures;
 }
 
 // TODO: expand on these.
-export interface WorkspaceSettings {
-    job: any;
-    schedule: any;
-    pool: any;
-    package: any;
-    certificate: any;
-    data: any;
-    gallery: any;
+export interface CommonFeatures {
+    view: boolean;
+    add: boolean;
+}
+
+export interface JobFeatures extends CommonFeatures {
+
+}
+
+export interface ScheduleFeatures extends CommonFeatures {
+
+}
+
+export interface PoolFeatures extends CommonFeatures {
+
+}
+
+export interface PackageFeatures extends CommonFeatures {
+
+}
+
+export interface CertificateFeatures extends CommonFeatures {
+
+}
+
+export interface DataFeatures extends CommonFeatures {
+
+}
+
+export interface GalleryFeatures extends CommonFeatures {
+
 }
 
 /**
  * Define the visible state of conponents in the UI
  */
 export interface WorkspaceDefinition {
-    id: WorkspaceMetaData["id"];
-    displayName: WorkspaceMetaData["displayName"];
-    description: WorkspaceMetaData["displayName"];
-    features: WorkspaceSettings;
+    id: string;
+    displayName: string;
+    description: string;
+    features: FeatureDefinition;
 }
