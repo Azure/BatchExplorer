33bd8229ab7a724f7f90fbc13fe2ec05423790b9
Timothee Guerin <tiguerin@microsoft.com>
2018-07-05 15:12:14 -0700

    Entity configuration

--- src/@batch-flask/ui/property-list/entity-configuration/entity-configuration.component.ts
+++ src/@batch-flask/ui/property-list/entity-configuration/entity-configuration.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges } from "@angular/core";
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnChanges, OnInit } from "@angular/core";
 import { Record } from "@batch-flask/core";
 import { BatchFlaskSettingsService, EntityConfigurationView } from "@batch-flask/ui/batch-flask-settings";
 import { EditorConfig } from "@batch-flask/ui/editor";
@@ -8,9 +8,11 @@ import "./entity-configuration.scss";
 @Component({
     selector: "bl-entity-configuration",
     templateUrl: "entity-configuration.html",
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EntityConfigurationComponent implements OnChanges {
+export class EntityConfigurationComponent implements OnInit, OnChanges {
     @Input() public value: Record<any>;
+    @Input() public enableJsonView = true;
 
     public showJson = false;
     public jsonValue = "{}";
@@ -20,20 +22,31 @@ export class EntityConfigurationComponent implements OnChanges {
         tabSize: 2,
     } as any;
 
-    constructor(settingsService: BatchFlaskSettingsService) {
-        const defaultView = settingsService.settings["configuration.default-view"];
-        if (defaultView === EntityConfigurationView.json) {
+    constructor(private settingsService: BatchFlaskSettingsService, private changeDetector: ChangeDetectorRef) {
+
+    }
+
+    public ngOnInit() {
+        const defaultView = this.settingsService.settings["configuration.default-view"];
+        if (this.enableJsonView && defaultView === EntityConfigurationView.json) {
             this.showJson = true;
         }
     }
-
     public ngOnChanges(changes) {
         if (changes.value) {
             this.jsonValue = JSON.stringify(this.value._original, null, 2);
         }
+
+        if (changes.enableJsonView) {
+            if (!this.enableJsonView) {
+                this.showJson = false; // Make sure we don't show the json form if it got disabled
+            }
+        }
     }
 
     public toggleShowJson(value: boolean) {
+        if (!this.enableJsonView) { return; }
         this.showJson = value;
+        this.changeDetector.markForCheck();
     }
 }
