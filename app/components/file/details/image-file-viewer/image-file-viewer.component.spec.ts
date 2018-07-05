b387c5577aabd924d7d1bfeff16f375910fcea82
Timothee Guerin <tiguerin@microsoft.com>
2018-07-05 10:27:36 -0700

    File viewer alt tag

---
+++ app/components/file/details/image-file-viewer/image-file-viewer.component.spec.ts
@@ -0,0 +1,57 @@
+import { Component, DebugElement, NO_ERRORS_SCHEMA } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+import { Subject, of } from "rxjs";
+import { ImageFileViewerComponent } from "./image-file-viewer.component";
+
+@Component({
+    template: `<bl-image-file-viewer [fileLoader]="fileLoader"></bl-image-file-viewer>`,
+})
+class TestComponent {
+    public fileLoader;
+}
+
+fdescribe("ImageFileViewerComponent", () => {
+    let fixture: ComponentFixture<TestComponent>;
+    let testComponent: TestComponent;
+    let de: DebugElement;
+    let imgEl: DebugElement;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [],
+            declarations: [ImageFileViewerComponent, TestComponent],
+            schemas: [NO_ERRORS_SCHEMA],
+        });
+
+        fixture = TestBed.createComponent(TestComponent);
+        testComponent = fixture.componentInstance;
+        testComponent.fileLoader = {
+            filename: "foo.png",
+            fileChanged: new Subject(),
+            cache: jasmine.createSpy().and.returnValue(of("/local/path/to/file.txt")),
+        };
+        de = fixture.debugElement.query(By.css("bl-image-file-viewer"));
+        fixture.detectChanges();
+        imgEl = fixture.debugElement.query(By.css("img"));
+    });
+
+    it("chache the image", () => {
+        expect(testComponent.fileLoader.cache).toHaveBeenCalledOnce();
+    });
+
+    it("point to the cached file locally", () => {
+        expect(imgEl).not.toBeFalsy();
+        expect(imgEl.nativeElement.getAttribute("src")).toEqual("file:///local/path/to/file.txt");
+    });
+
+    it("it shows an alt text for the image", () => {
+        expect(imgEl).not.toBeFalsy();
+        expect(imgEl.nativeElement.getAttribute("alt")).toEqual("Displaying image foo.png");
+    });
+
+    it("refresh again when file has changed", () => {
+        testComponent.fileLoader.fileChanged.next(true);
+        expect(testComponent.fileLoader.cache).toHaveBeenCalledTimes(2);
+    });
+});
