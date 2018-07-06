5a9a3d06e99329feed5048422a0d55a4b360d55c
Timothee Guerin <timothee.guerin@outlook.com>
2018-07-06 09:39:35 -0700

    Missing files

---
+++ src/@batch-flask/ui/workspace/workspace.model.ts
@@ -0,0 +1,52 @@
+
+/**
+ * Define the visiblity state of certain components in the UI
+ */
+export interface WorkspaceDefinition {
+    id: string;
+    displayName: string;
+    description: string;
+    features: FeatureDefinition;
+}
+
+export interface FeatureDefinition {
+
+}
+
+export class Workspace {
+    public id: string;
+    public name: string;
+    public template: FeatureDefinition;
+
+    public constructor(definition: WorkspaceDefinition) {
+        this.id = definition.id;
+        this.name = definition.displayName;
+        this.template = definition.features;
+    }
+
+    public isFeatureEnabled(requestedFeature: string): boolean {
+        // no feature supplied, default to visible
+        if (!requestedFeature) { return true; }
+
+        const features = requestedFeature.split(".");
+        // console.log(`Workspace.isFeatureEnabled: ${requestedFeature}`, this.template);
+
+        let definition = this.template;
+        for (const feat of features) {
+            definition = definition[feat];
+            if (definition === true) {
+                return true;
+            }
+        }
+
+        // TODO: debug - remove
+        if (definition) {
+            // console.log(`traversed feature '${requestedFeature}' to end ... definition tree ->`, definition);
+        }
+
+        // if there are any remaining template definition children after we
+        // have got to the end of the requested feature, we can assume this means
+        // that it should be enabled.
+        return definition ? true : false;
+    }
+}
