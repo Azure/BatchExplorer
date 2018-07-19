338e711c9883c30a0499c99e337d808f9172e0f2
Timothee Guerin <tiguerin@microsoft.com>
2018-07-19 13:23:13 -0700

    Switch to xml2js

--- src/@batch-flask/compiler/xliff-serializer.ts
+++ src/@batch-flask/compiler/xliff-serializer.ts
@@ -1,4 +1,4 @@
-import * as convert from "xml-js";
+import * as xml2js from "xml2js";
 
 // tslint:disable:max-line-length
 const rootAttributes = {
@@ -9,72 +9,40 @@ const rootAttributes = {
 };
 // tslint:enable:max-line-length
 
-function makeElement(name, attributes, elements) {
-    const el: any = {
-        type: "element",
-        name: name,
-    };
-    if (attributes !== null && attributes !== undefined) {
-        el.attributes = attributes;
-    }
-    if (Array.isArray(elements)) {
-        el.elements = elements;
-    } else if (elements === true) {
-        el.elements = [];
-    }
-    return el;
-}
-
-function makeText(text) {
-    return {
-        type: "text",
-        text,
-    };
-}
-
-const map = {
-    "&": "&amp;", // this must be the first !!!
-    '"': "&quot;",
-    "'": "&apos;",
-    "<": "&lt;",
-    ">": "&gt;",
-};
-
-function escape(str: string) {
-    Object.keys(map).forEach((char) => {
-        str = str.replace(new RegExp(char, "g"), map[char]);
-    });
-    return str;
-}
-
-const options = {
-    spaces: "  ",
-};
-
 export class XliffSerializer {
     /**
      * @param translations Key value object containing the translations
      */
     public static encode(translations: StringMap<string>): string {
-        const root = makeElement("xliff", rootAttributes, true);
-        const body = makeElement("body", null, true);
+        const units = [];
+
         const fileAttributes = {
             "original": "Batch",
             "datatype": "plaintext",
             "source-language": "en",
         };
 
-        const file = makeElement("file", fileAttributes, [body]);
-        root.elements.push(file);
+        const root = {
+            $: rootAttributes,
+            file: {
+                $: fileAttributes,
+                body: {
+                    "trans-unit": units,
+                },
+            },
+        };
 
         for (const key of Object.keys(translations)) {
-            const unit = makeElement("trans-unit", { id: escape(key) }, true);
-            unit.elements.push(makeElement("source", null, [makeText(translations[key])]));
-            body.elements.push(unit);
+            units.push({
+                $: { id: key },
+                source: translations[key],
+            });
         }
-        const xml = {
-            elements: [root],
-        };
-        return convert.js2xml(xml, options);
+        const builder = new xml2js.Builder({
+            headless: true,
+        });
+        return builder.buildObject({
+            xliff: root,
+        });
     }
 }
