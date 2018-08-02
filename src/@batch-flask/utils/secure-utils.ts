6f9cca155d6452bd96dfb2820a0d9c285b57e024
Johnny Zhang <yunzh@microsoft.com>
2018-08-02 13:50:38 -0700

    using crypto.getRandomValues to generate random number

--- src/@batch-flask/utils/secure-utils.ts
+++ src/@batch-flask/utils/secure-utils.ts
@@ -16,17 +16,23 @@ export class SecureUtils {
     public static username(length: number = 6): string {
         const charset = "abcdefghijklmnopqrstuvwxyz";
         let retVal = "";
-        for (let i = 0, n = charset.length; i < length; ++i) {
-            retVal += charset.charAt(Math.floor(Math.random() * n));
-        }
+        // create an array and fill with random integer
+        const randomArray = new Uint32Array(length);
+        window.crypto.getRandomValues(randomArray);
+        for (let i = 0; i < length; i++) {
+            retVal += charset.charAt(randomArray[i] % charset.length);
+        }        
         return retVal;
     }
 
     public static password(length: number = 15): string {
         const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]\:;?><,./-=";
         let retVal = "";
-        for (let i = 0, n = charset.length; i < length; ++i) {
-            retVal += charset.charAt(Math.floor(Math.random() * n));
+        // create an array and fill with random integer
+        const randomArray = new Uint32Array(length);
+        window.crypto.getRandomValues(randomArray);
+        for (let i = 0; i < length; i++) {
+            retVal += charset.charAt(randomArray[i] % charset.length);
         }
         return retVal;
     }
@@ -42,17 +48,20 @@ export class SecureUtils {
             + "!@#$%^&*()_+~`|}{[]\\:;?><,./-=";
 
         let retVal = "";
-
         // generate a random symbol from each charset
-        for (let i = 0; i < 4; ++i) {
-            retVal += charsets[i].charAt(Math.floor(Math.random() * charsets[i].length));
+        const randomPrimary = new Uint32Array(4);
+        window.crypto.getRandomValues(randomPrimary);
+        for (let i = 0; i < 4; i++) {
+            retVal += charsets[i].charAt(randomPrimary[i] % charsets[i].length);
         }
 
         // fill the rest of the password with random characters from any charset
-        for (let i = 4, n = charsetJoined.length; i < length; ++i) {
-            retVal += charsetJoined.charAt(Math.floor(Math.random() * n));
+        const remainingLength = length - 4;
+        const randomSecondary = new Uint32Array(remainingLength);
+        window.crypto.getRandomValues(randomSecondary);
+        for (let i = 0; i < remainingLength; i++) {
+            retVal += charsetJoined.charAt(randomSecondary[i] % charsetJoined.length);
         }
-
         return retVal;
     }
 
