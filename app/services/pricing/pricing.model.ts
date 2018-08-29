09dd39a40ea9610ca68bcef28d2f0f4d17e4ab58
Timothee Guerin <tiguerin@microsoft.com>
2018-08-29 12:48:27 -0700

    Update pricing.model.ts

--- app/services/pricing/pricing.model.ts
+++ app/services/pricing/pricing.model.ts
@@ -122,7 +122,7 @@ export class NodePricing {
             lowpri = true;
             name = name.replace(" Low Priority", "");
         }
-        const vmSizes = name.split("/").map(x => x.replace(/ /g, "_");
+        const vmSizes = name.split("/").map(x => x.replace(/ /g, "_"));
         const os = category.includes("Windows") ? "windows" : "linux";
         return { os, lowpri, vmSizes };
     }
