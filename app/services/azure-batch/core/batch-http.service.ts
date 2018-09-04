41dd69615695694988a95001ad2e076269622929
Timothee Guerin <tiguerin@microsoft.com>
2018-09-04 09:19:04 -0700

    Shared key wip

--- app/services/azure-batch/core/batch-http.service.ts
+++ app/services/azure-batch/core/batch-http.service.ts
@@ -1,5 +1,5 @@
 import { Location } from "@angular/common";
-import { HttpHandler, HttpParams } from "@angular/common/http";
+import { HttpHandler, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { HttpRequestOptions, HttpService, ServerError } from "@batch-flask/core";
 import { UrlUtils } from "@batch-flask/utils";
@@ -8,8 +8,9 @@ import { AccountService } from "app/services/account.service";
 import { AdalService } from "app/services/adal";
 import { BatchExplorerService } from "app/services/batch-labs.service";
 import { Constants } from "common";
-import { Observable, throwError } from "rxjs";
+import { Observable, from, throwError } from "rxjs";
 import { catchError, flatMap, retryWhen, shareReplay, take } from "rxjs/operators";
+import { BatchSharedKeyCredentials } from "./shared-key-utils";
 
 @Injectable()
 export class AzureBatchHttpService extends HttpService {
@@ -26,24 +27,29 @@ export class AzureBatchHttpService extends HttpService {
     }
 
     public request(method: any, uri?: any, options?: any): Observable<any> {
+        options = this._addApiVersion(uri, options);
         return this.accountService.currentAccount.pipe(
             take(1),
             flatMap((account) => {
                 const tenantId = account.subscription.tenantId;
                 return this.adal.accessTokenData(tenantId, this.serviceUrl).pipe(
+                    flatMap(() => {
+                        // TODO-TIM remove
+                        const sharedKey = new BatchSharedKeyCredentials("", "");
+                        return from(sharedKey.signRequest(method, uri, options));
+                    }),
                     flatMap((accessToken) => {
-                        options = this.addAuthorizationHeader(options, accessToken);
-                        options = this._addApiVersion(uri, options);
+                        // options = this.addAuthorizationHeader(options, accessToken);
                         return super.request(
                             method,
                             this._computeUrl(uri, account),
                             options).pipe(
-                            retryWhen(attempts => this.retryWhen(attempts)),
-                            catchError((error) => {
-                                const err = ServerError.fromBatchHttp(error);
-                                return throwError(err);
-                            }),
-                        );
+                                retryWhen(attempts => this.retryWhen(attempts)),
+                                catchError((error) => {
+                                    const err = ServerError.fromBatchHttp(error);
+                                    return throwError(err);
+                                }),
+                            );
                     }),
                 );
             }),
@@ -59,7 +65,9 @@ export class AzureBatchHttpService extends HttpService {
         if (!options.params.has("api-version") && !uri.contains("api-version")) {
             options.params = options.params.set("api-version", Constants.ApiVersion.batchService);
         }
-
+        if (!options.headers) {
+            options.headers = new HttpHeaders();
+        }
         options.headers = (options.headers as any)
             .set("Content-Type", "application/json; odata=minimalmetadata; charset=utf-8")
             .set("Cache-Control", "no-cache");
--- app/services/azure-batch/core/shared-key-utils.ts
+++ app/services/azure-batch/core/shared-key-utils.ts
@@ -1,3 +1,7 @@
+import { HttpHeaders, HttpParams } from "@angular/common/http";
+import { HttpRequestOptions } from "@batch-flask/core";
+import { UrlUtils } from "@batch-flask/utils";
+import * as url from "url";
 
 class HmacSha256Sign {
     public async getKey(key: string): Promise<CryptoKey> {
@@ -15,128 +19,107 @@ class HmacSha256Sign {
             ["sign"]);
     }
 
-    public async sign(key: CryptoKey, stringToSign: string) {
-        return crypto.subtle.sign("sha256", key, new Buffer(stringToSign));
+    public async sign(key: CryptoKey, stringToSign: any) {
+        return crypto.subtle.sign(({ name: "hmac", hash: { name: "sha-256" } }), key, new Buffer(stringToSign));
     }
 }
 
-/**
- * @class
- * Creates a new BatchSharedKeyCredentials object.
- * @constructor
- * @param {string} accountName The batch account name.
- * @param {string} accountKey The batch account key.
- */
-function BatchSharedKeyCredentials(accountName, accountKey) {
-    if (!Boolean(accountName) || typeof accountName.valueOf() !== "string") {
-        throw new Error("accountName must be a non empty string.");
+function getvalueToAppend(headers: HttpHeaders, name: string) {
+    if (headers.has(name)) {
+        return `${headers.get(name)}\n`;
+    } else {
+        return "\n";
     }
-
-    if (!Boolean(accountKey) || typeof accountKey.valueOf() !== "string") {
-        throw new Error("accountKey must be a non empty string.");
-    }
-
-    this.accountName = accountName;
-    this.accountKey = accountKey;
-    this.signer = new HmacSha256Sign(accountKey);
 }
 
-/**
- * Signs a request with the Authentication header.
- *
- * @param {webResource} The WebResource to be signed.
- * @param {function(error)}  callback  The callback function.
- * @return {undefined}
- */
-BatchSharedKeyCredentials.prototype.signRequest = function (webResource, callback) {
-    const self = this;
-
-    // Help function to get header value, if header without value, append a newline
-    const getvalueToAppend = function (value, headerName) {
-        if (!Boolean(value) || !Boolean(value[headerName])) {
+function getContentLengthToAppend(value, method: string, body: string) {
+    if (!Boolean(value) || !Boolean(value["Content-Length"])) {
+        // Get content length from body if available
+        if (body) {
+            return Buffer.byteLength(body) + "\n";
+        }
+        // For GET verb, do not add content-length
+        if (method === "GET") {
             return "\n";
         } else {
-            return value[headerName] + "\n";
+            return "0\n";
         }
-    };
+    } else {
+        return value["Content-Length"] + "\n";
+    }
+}
 
-    // Help function to get content length
-    const getContentLengthToAppend = function (value, method, body) {
-        if (!Boolean(value) || !Boolean(value["Content-Length"])) {
-            // Get content length from body if available
-            if (body) {
-                return Buffer.byteLength(body) + "\n";
-            }
-            // For GET verb, do not add content-length
-            if (method === "GET") {
-                return "\n";
-            } else {
-                return "0\n";
-            }
-        } else {
-            return value["Content-Length"] + "\n";
-        }
-    };
+export class BatchSharedKeyCredentials {
+    public signer: HmacSha256Sign;
 
-    // Set Headers
-    if (!Boolean(webResource.headers["ocp-date"])) {
-        webResource.headers["ocp-date"] = new Date().toUTCString();
+    constructor(public name: string, public key: string) {
+        this.signer = new HmacSha256Sign();
     }
 
-    // Add verb and standard HTTP header as single line
-    let stringToSign = webResource.method + "\n" +
-        getvalueToAppend(webResource.headers, "Content-Encoding") +
-        getvalueToAppend(webResource.headers, "Content-Language") +
-        getContentLengthToAppend(webResource.headers, webResource.method, webResource.body) +
-        getvalueToAppend(webResource.headers, "Content-MD5") +
-        getvalueToAppend(webResource.headers, "Content-Type") +
-        getvalueToAppend(webResource.headers, "Date") +
-        getvalueToAppend(webResource.headers, "If-Modified-Since") +
-        getvalueToAppend(webResource.headers, "If-Match") +
-        getvalueToAppend(webResource.headers, "If-None-Match") +
-        getvalueToAppend(webResource.headers, "If-Unmodified-Since") +
-        getvalueToAppend(webResource.headers, "Range");
+    public async signRequest(method: string, uri: string, request: HttpRequestOptions) {
+        let headers = request.headers instanceof HttpHeaders ? request.headers : new HttpHeaders(request.headers);
+        // Set Headers
+        if (!headers.has("ocp-date")) {
+            headers = headers.set("ocp-date", new Date().toUTCString());
+        }
+        let stringToSign = method + "\n" +
+            getvalueToAppend(headers, "Content-Encoding") +
+            getvalueToAppend(headers, "Content-Language") +
+            getContentLengthToAppend(headers, method, request.body) +
+            getvalueToAppend(headers, "Content-MD5") +
+            getvalueToAppend(headers, "Content-Type") +
+            getvalueToAppend(headers, "Date") +
+            getvalueToAppend(headers, "If-Modified-Since") +
+            getvalueToAppend(headers, "If-Match") +
+            getvalueToAppend(headers, "If-None-Match") +
+            getvalueToAppend(headers, "If-Unmodified-Since") +
+            getvalueToAppend(headers, "Range");
 
-    // Add customize HTTP header
-    stringToSign += this._getCanonicalizedHeaders(webResource);
+        // Add customize HTTP header
+        stringToSign += this._getCanonicalizedHeaders(headers);
 
-    // Add path/query from uri
-    stringToSign += this._getCanonicalizedResource(webResource);
+        // Add path/query from uri
+        stringToSign += this._getCanonicalizedResource(uri, request);
 
-    // Signed with sha256
-    const signature = this.signer.sign(stringToSign);
+        // Signed with sha256
+        const encoder = new TextEncoder();
+        const key = await this.signer.getKey(this.key);
+        const buffer = await this.signer.sign(key, encoder.encode(stringToSign));
+        console.log("Sign", stringToSign);
+        const signature = btoa(String.fromCharCode.apply(null, new Uint8Array(buffer)));
+        console.log("Sig", this.name, signature);
+        // Add authrization header
+        headers = headers.set("Authorization", `SharedKey ${this.name}:${signature}`);
 
-    // Add authrization header
-    webResource.headers[Constants.HeaderConstants.AUTHORIZATION] = `SharedKey ${self.accountName}:${signature}`;
-    callback(null);
-};
+        request.headers = headers;
+    }
 
-/*
- * Constructs the Canonicalized Headers string.
- *
- * To construct the CanonicalizedHeaders portion of the signature string,
- * follow these steps: 1. Retrieve all headers for the resource that begin
- * with ocp-, including the ocp-date header. 2. Convert each HTTP header
- * name to lowercase. 3. Sort the headers lexicographically by header name,
- * in ascending order. Each header may appear only once in the
- * string. 4. Unfold the string by replacing any breaking white space with a
- * single space. 5. Trim any white space around the colon in the header. 6.
- * Finally, append a new line character to each canonicalized header in the
- * resulting list. Construct the CanonicalizedHeaders string by
- * concatenating all headers in this list into a single string.
- *
- * @param {object} The webresource object.
- * @return {string} The canonicalized headers.
- */
-BatchSharedKeyCredentials.prototype._getCanonicalizedHeaders = function (webResource) {
-    // Build canonicalized headers
-    let canonicalizedHeaders = "";
-    if (webResource.headers) {
+    /*
+     * Constructs the Canonicalized Headers string.
+     *
+     * To construct the CanonicalizedHeaders portion of the signature string,
+     * follow these steps: 1. Retrieve all headers for the resource that begin
+     * with ocp-, including the ocp-date header. 2. Convert each HTTP header
+     * name to lowercase. 3. Sort the headers lexicographically by header name,
+     * in ascending order. Each header may appear only once in the
+     * string. 4. Unfold the string by replacing any breaking white space with a
+     * single space. 5. Trim any white space around the colon in the header. 6.
+     * Finally, append a new line character to each canonicalized header in the
+     * resulting list. Construct the CanonicalizedHeaders string by
+     * concatenating all headers in this list into a single string.
+     *
+     * @param {HttpHeaders} The headers object.
+     * @return {string} The canonicalized headers.
+     */
+    private _getCanonicalizedHeaders(headers: HttpHeaders) {
+        if (!headers) { return ""; }
+
+        let canonicalizedHeaders = "";
         const canonicalizedHeadersArray = [];
 
         // Retrieve all headers for begin with ocp-
-        for (const header in webResource.headers) {
-            if (header.indexOf("ocp-") === 0) {
+        for (const header of headers.keys()) {
+            if (header.includes("ocp-")) {
                 canonicalizedHeadersArray.push(header);
             }
         }
@@ -144,48 +127,45 @@ BatchSharedKeyCredentials.prototype._getCanonicalizedHeaders = function (webReso
         // Sort the header by header name
         canonicalizedHeadersArray.sort();
 
-        _.each(canonicalizedHeadersArray, function (currentHeader) {
-            const value = webResource.headers[currentHeader];
-            if (Boolean(value)) {
+        for (const header of canonicalizedHeadersArray) {
+            const value = headers.get(header);
+            if (value) {
                 // Make header value lower case and apend a new line for each header
-                canonicalizedHeaders += currentHeader.toLowerCase() + ":" + value + "\n";
+                canonicalizedHeaders += header.toLowerCase() + ":" + value + "\n";
             }
-        });
+        }
+        return canonicalizedHeaders;
     }
 
-    return canonicalizedHeaders;
-};
+    private _getCanonicalizedResource(uri: string, request: HttpRequestOptions) {
+        let path = uri;
 
-/*
- * Retrieves the webresource's canonicalized resource string.
- * @param {WebResource} webResource The webresource to get the canonicalized resource string from.
- * @return {string} The canonicalized resource string.
- */
-BatchSharedKeyCredentials.prototype._getCanonicalizedResource = function (webResource) {
-    let path = "/";
-    const urlstring = url.parse(webResource.url, true);
-    if (urlstring.pathname) {
-        path = urlstring.pathname;
+        // Get the raw query string values for signing
+        let params = request.params instanceof HttpParams
+            ? request.params
+            : new HttpParams({ fromObject: request.params });
+
+        if (UrlUtils.isHttpUrl(uri)) {
+            const urlstring = url.parse(uri, true);
+            if (urlstring.pathname) {
+                path = urlstring.pathname;
+            } else {
+                path = "/";
+            }
+            for (const param of Object.keys(urlstring.query)) {
+                params = params.set(param, urlstring.query[param] as string);
+            }
+        }
+
+        let canonicalizedResource = "/" + this.name + path;
+
+        // Build the canonicalized resource by sorting the values by name.
+        const paramNames = params.keys().sort();
+
+        for (const name of paramNames) {
+            canonicalizedResource += "\n" + name + ":" + params.get(name);
+        }
+
+        return canonicalizedResource;
     }
-
-    let canonicalizedResource = "/" + this.accountName + path;
-
-    // Get the raw query string values for signing
-    const queryStringValues = urlstring.query;
-
-    // Build the canonicalized resource by sorting the values by name.
-    if (queryStringValues) {
-        let paramNames = [];
-        Object.keys(queryStringValues).forEach(function (n) {
-            paramNames.push(n);
-        });
-
-        // All the queries sorted by query name
-        paramNames = paramNames.sort();
-        Object.keys(paramNames).forEach(function (name) {
-            canonicalizedResource += "\n" + paramNames[name] + ":" + queryStringValues[paramNames[name]];
-        });
-    }
-
-    return canonicalizedResource;
-};
+}
