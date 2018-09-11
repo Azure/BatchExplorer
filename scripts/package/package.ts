322e83cf62d74377490caac0043b9f8135d1d498
Timothee Guerin <tiguerin@microsoft.com>
2018-09-11 10:19:14 -0700

    Package app programticaly to have better options

---
+++ scripts/package/package.ts
@@ -0,0 +1,83 @@
+import * as electronBuilder from "electron-builder";
+import * as fs from "fs";
+import * as path from "path";
+
+const root = path.resolve(path.join(__dirname, "../.."));
+const packageDef = JSON.parse(fs.readFileSync(path.join(root, "package.json")).toString());
+
+enum BuildType {
+    // When building locally or from a pull request(Not signed)
+    Dev = "dev",
+    // Official build release
+    Stable = "stable",
+    // Build off master
+    Insider = "insider",
+}
+
+/**
+ * Base build function which will update common attributes to all platforms
+ */
+async function baseBuild(options?: electronBuilder.CliOptions) {
+    const buildVersion = process.env.BUILD_NUMBER;
+    const buildType = process.env.BUILD_TYPE || "dev";
+
+    let version = `${packageDef.version}-${buildType}`;
+    if (buildVersion) {
+        version += `.${buildVersion}`;
+    }
+
+    await electronBuilder.build({
+        config: {
+            extraMetadata: {
+                version,
+            },
+        },
+        ...options,
+    });
+}
+
+/**
+ * Just create the windows executable.
+ * This is so exe can be signed before creating installer
+ */
+async function createWindowsExecutable() {
+    await baseBuild({
+        dir: true,
+        platform: "windows",
+    });
+}
+
+/**
+ * Just create the windows executable.
+ * This is so exe can be signed before creating installer
+ */
+async function createWindowsInstaller() {
+    await baseBuild({
+        win: ["nsis", "zip"],
+        prepackaged: "./release/win-unpacked",
+        projectDir: ".",
+    });
+}
+
+async function buildDefault() {
+    await baseBuild();
+}
+
+async function build() {
+    const action: string = process.argv[2] || "default";
+
+    switch (action) {
+        case "win-exe":
+            return createWindowsExecutable();
+        case "win-installer":
+            return createWindowsInstaller();
+        default:
+            return buildDefault();
+    }
+}
+
+build().catch((err) => {
+    // tslint:disable-next-line:no-console
+    console.error(err);
+    process.exit(1);
+});
