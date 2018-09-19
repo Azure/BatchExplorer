bfd1ef6310b3b4d93a7218d62ffe07751aefef9e
Timothee Guerin <tiguerin@microsoft.com>
2018-09-19 15:20:41 -0700

    Fix tslint

--- src/client/core/telemetry/client-telemetry.module.ts
+++ src/client/core/telemetry/client-telemetry.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { TELEMETRY_UPLOADER, TelemetryModule } from "@batch-flask/core";
-import { MachineIdService } from "client/core/telemetry/machine-id.service";
 import { ApplicationInsightsUploader } from "client/core/telemetry/application-insights-uploader";
+import { MachineIdService } from "client/core/telemetry/machine-id.service";
 import { TelemetryManager } from "client/core/telemetry/telemetry-manager";
 
 @NgModule({
