fe9aa6d9156c09a22a5bcfe8d5a67ccc7e173160
Timothee Guerin <tiguerin@microsoft.com>
2018-10-08 11:11:51 -0700

    Fix Url validation to accept ips

--- src/@batch-flask/utils/url-utils/url-utils.spec.ts
+++ src/@batch-flask/utils/url-utils/url-utils.spec.ts
@@ -9,4 +9,14 @@ describe("UrlUtils", () => {
         expect(UrlUtils.isHttpUrl("ftp://example.com")).toBe(false);
         expect(UrlUtils.isHttpUrl("data://abcdefghi")).toBe(false);
     });
+    it("#isUrl()", () => {
+        expect(UrlUtils.isUrl("https://banana")).toBe(true);
+        expect(UrlUtils.isUrl("https://example.com")).toBe(true);
+        expect(UrlUtils.isUrl("https://example.com/long/path/to/file.txt")).toBe(true);
+        expect(UrlUtils.isUrl("http://example.com")).toBe(true);
+        expect(UrlUtils.isUrl("https://171.0.0.1:8080/some/path")).toBe(true);
+        expect(UrlUtils.isUrl("http://0.0.0.0/some/path")).toBe(true);
+        expect(UrlUtils.isUrl("ftp://example.com")).toBe(false);
+        expect(UrlUtils.isUrl("data://abcdefghi")).toBe(false);
+    });
 });
