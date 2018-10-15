aef688c582e961cebed60b8e0e749c7c0ef43ee5
Timothee Guerin <timothee.guerin@outlook.com>
2018-10-15 08:09:50 -0700

    Ncj template file viewer

---
+++ app/components/market/ncj-template-viewer/ncj-template-viewer.component.ts
@@ -0,0 +1,73 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from "@angular/core";
+import { FileViewer } from "@batch-flask/ui";
+import { EditorConfig } from "@batch-flask/ui/editor";
+import { LoadingStatus } from "@batch-flask/ui/loading";
+import { Uri } from "monaco-editor";
+import { Subscription } from "rxjs";
+
+import "./ncj-template-viewer.scss";
+
+@Component({
+    selector: "bl-ncj-template-viewer",
+    templateUrl: "ncj-template-viewer.html",
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NcjTemplateViewerComponent extends FileViewer {
+    public static readonly MAX_FILE_SIZE = 1000000; // 1MB
+
+    public value: string = "";
+    public loadingStatus: LoadingStatus = LoadingStatus.Loading;
+    public editorConfig: EditorConfig;
+
+    public commands = [
+        {
+            label: "Run",
+            icon: "fa fa-play",
+            color: "success",
+            execute: () => console.log("Exec"),
+        },
+    ];
+
+    private _contentSub: Subscription;
+
+    constructor(changeDetector: ChangeDetectorRef) {
+        super(changeDetector);
+    }
+
+    public onFileLoaderChanges() {
+        this.loadingStatus = LoadingStatus.Loading;
+        this._loadContent();
+        this._computeEditorOptions();
+    }
+
+    public onFileChanges() {
+        this._loadContent();
+    }
+
+    private _loadContent() {
+        if (!this.fileLoader) { return; }
+        this._cleanupSub();
+        this._contentSub = this.fileLoader.content().subscribe((result) => {
+            this.value = result.content.toString();
+            this.loadingStatus = LoadingStatus.Ready;
+            this.changeDetector.markForCheck();
+        });
+    }
+
+    private _computeEditorOptions() {
+        this.editorConfig = {
+            readOnly: true,
+            minimap: {
+                enabled: false,
+            },
+            language: "json",
+            uri: this.fileLoader && Uri.file(this.fileLoader.filename),
+        };
+    }
+
+    private _cleanupSub() {
+        if (this._contentSub) {
+            this._contentSub.unsubscribe();
+        }
+    }
+}
