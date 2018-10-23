6540da36f4b3521fd58eb374e490cead7bc72703
Timothee Guerin <tiguerin@microsoft.com>
2018-10-23 12:48:10 -0700

    Fix

--- src/@batch-flask/ui/quick-list/quick-list.component.spec.ts
+++ src/@batch-flask/ui/quick-list/quick-list.component.spec.ts
@@ -52,7 +52,7 @@ class TestComponent {
     ];
 }
 
-fdescribe("QuickListComponent", () => {
+describe("QuickListComponent", () => {
     let fixture: ComponentFixture<TestComponent>;
     let testComponent: TestComponent;
     let de: DebugElement;
