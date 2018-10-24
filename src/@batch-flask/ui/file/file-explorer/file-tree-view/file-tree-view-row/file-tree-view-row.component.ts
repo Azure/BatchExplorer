f396b2a14129a90e5e7f8f575fcb58cd17cc77b0
Timothee Guerin <timothee.guerin@outlook.com>
2018-10-24 16:20:44 -0700

    Accessibility: Tree view accessible (#1717)

--- src/@batch-flask/ui/file/file-explorer/file-tree-view/file-tree-view-row/file-tree-view-row.component.ts
+++ src/@batch-flask/ui/file/file-explorer/file-tree-view/file-tree-view-row/file-tree-view-row.component.ts
@@ -24,18 +24,36 @@ export class FileTreeViewRowComponent {
         return `${(this.treeRow.indent + 1) * 12}px`;
     }
 
-    @HostBinding("attr.title")
+    @HostBinding("class.virtual")
+    public get virtual() {
+        return this.treeRow.virtual;
+    }
+
+    // Aria
+    @HostBinding("attr.role") public role = "treeitem";
+    @HostBinding("attr.aria-expanded") public get ariaExpanded() {
+        if (this.treeRow.isDirectory) {
+            return this.treeRow.expanded;
+        } else {
+            return null;
+        }
+    }
+    @HostBinding("attr.aria-level") public get ariaLevel() {
+        return this.treeRow.indent + 1;
+    }
+    @HostBinding("attr.aria-selected") public get ariaSelected() {
+        return this.active;
+    }
+    @HostBinding("attr.aria-label") public get ariaLabel() {
+        return this.treeRow.name;
+    }
+
     public get title() {
         if (this.treeRow.virtual) {
             return `${this.treeRow.path} (Virtual)`;
         } else {
             return this.treeRow.path;
         }
-    }
-
-    @HostBinding("class.virtual")
-    public get virtual() {
-        return this.treeRow.virtual;
     }
 
     public handleCaretClick(event: MouseEvent) {
