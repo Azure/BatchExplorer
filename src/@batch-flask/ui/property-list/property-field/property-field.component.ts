eba06417a2c9be449c57e10fd48bb12ce63a23f4
Timothee Guerin <tiguerin@microsoft.com>
2018-10-25 09:42:04 -0700

    Tweaks

--- src/@batch-flask/ui/property-list/property-field/property-field.component.ts
+++ src/@batch-flask/ui/property-list/property-field/property-field.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input, HostBinding } from "@angular/core";
+import { ChangeDetectionStrategy, Component, HostBinding, Input } from "@angular/core";
 
 import "./property-field.scss";
 
