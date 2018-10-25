d07d4df8a9d2904f298f5c4f5ceb2e0bbc0b8640
Timothee Guerin <tiguerin@microsoft.com>
2018-10-25 09:14:56 -0700

    WIp

--- src/@batch-flask/ui/property-list/table-property/table-property.component.ts
+++ src/@batch-flask/ui/property-list/table-property/table-property.component.ts
@@ -1,6 +1,8 @@
 import {
-    Component, ContentChild, ContentChildren, HostBinding, HostListener, Input, QueryList, TemplateRef, ViewChild,
+    Component, ContentChild, ContentChildren, HostBinding,
+    Injector, Input, QueryList, TemplateRef, ViewChild,
 } from "@angular/core";
+import { ClickableComponent } from "@batch-flask/ui/buttons";
 import { ClipboardService } from "@batch-flask/ui/electron";
 
 import "./table-property.scss";
@@ -14,6 +16,8 @@ import "./table-property.scss";
 export class TablePropertyHeaderComponent {
     @ViewChild(TemplateRef)
     public content: TemplateRef<any>;
+
+    @HostBinding("attr.role") public role = "row";
 }
 
 @Component({
@@ -26,29 +30,38 @@ export class TablePropertyRowComponent {
     @ViewChild(TemplateRef)
     public content: TemplateRef<any>;
 
+    @HostBinding("attr.role") public role = "row";
+
 }
 
 @Component({
     selector: "bl-tp-cell",
     template: `
         <div class="cell-value">{{value}}</div>
-        <div class="copied-notification" [class.hidden]="copyNotificationHidden">Copied.</div>
+        <div class="copied-notification"
+            [class.hidden]="copyNotificationHidden"
+            [attr.aria-hidden]="copyNotificationHidden">
+            Copied.
+        </div>
     `,
 })
-export class TablePropertyCellComponent {
+export class TablePropertyCellComponent extends ClickableComponent {
     @Input()
     @HostBinding("attr.title")
     public value;
     @HostBinding("class.property-content-box")
     public box = true;
 
+    @HostBinding("attr.role") public role = "gridcell";
+
     public copyNotificationHidden = true;
 
-    constructor(private clipboard: ClipboardService) {
-
+    constructor(private clipboard: ClipboardService, injector: Injector) {
+        super(injector, null);
     }
-    @HostListener("click")
-    public onClick() {
+
+    public handleAction(event) {
+        super.handleAction(event);
         this.copyNotificationHidden = false;
         this.clipboard.writeText(this.value);
         setTimeout(() => {
@@ -64,17 +77,17 @@ export class TablePropertyCellComponent {
 export class TablePropertyComponent {
     public readonly defaultDisplay = 2;
 
-    @Input()
-    public label: string;
+    @Input() public label: string;
 
-    @Input()
-    public expandable = true;
+    @Input() public expandable = true;
 
     @ContentChild(TablePropertyHeaderComponent)
     public header: TablePropertyHeaderComponent;
 
     @ContentChildren(TablePropertyRowComponent)
     public rows: QueryList<TablePropertyRowComponent>;
+
+    @HostBinding("attr.role") public role = "grid";
 
     public expanded = false;
 
