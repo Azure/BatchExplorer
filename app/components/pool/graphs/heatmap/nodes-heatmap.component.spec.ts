051ebed64caf093ea898ba142a72f2b1933eb136
Timothee Guerin <tiguerin@microsoft.com>
2018-11-02 14:16:45 -0700

    Wip

--- app/components/pool/graphs/heatmap/nodes-heatmap.component.spec.ts
+++ app/components/pool/graphs/heatmap/nodes-heatmap.component.spec.ts
@@ -1,4 +1,4 @@
-import { Component, DebugElement, Input } from "@angular/core";
+import { Component, DebugElement } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { By } from "@angular/platform-browser";
 import { Router } from "@angular/router";
