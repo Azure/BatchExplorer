057730ef91186b7a0865c4c84a8e53ac91bbb459
Timothee Guerin <tiguerin@microsoft.com>
2019-01-07 14:22:55 -0800

    Fixes

--- src/app/models/network-configuration.ts
+++ src/app/models/network-configuration.ts
@@ -1,9 +1,15 @@
 import { Model, Prop, Record } from "@batch-flask/core";
 import { PoolEndpointConfiguration, PoolEndpointConfigurationAttributes } from "./pool-endpoint-configuration";
 
+export enum DynamicVNetAssignmentScope {
+    Job = "job",
+    None = "none",
+}
+
 export interface NetworkConfigurationAttributes {
     subnetId: string;
     endpointConfiguration: PoolEndpointConfigurationAttributes;
+    dynamicVNetAssignmentScope: DynamicVNetAssignmentScope;
 }
 
 /**
@@ -13,4 +19,5 @@ export interface NetworkConfigurationAttributes {
 export class NetworkConfiguration extends Record<NetworkConfigurationAttributes> {
     @Prop() public subnetId: string;
     @Prop() public endpointConfiguration: PoolEndpointConfiguration;
+    @Prop() public dynamicVNetAssignmentScope: DynamicVNetAssignmentScope = DynamicVNetAssignmentScope.None;
 }
