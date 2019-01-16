b9fa5f22ce18a680f9908afb2a9e9246a3b459f4
Timothee Guerin <tiguerin@microsoft.com>
2019-01-16 10:47:25 -0800

    Wip

--- src/app/components/pool/user-account/user-account-picker.component.ts
+++ src/app/components/pool/user-account/user-account-picker.component.ts
@@ -1,35 +1,60 @@
-import { Component, OnDestroy, forwardRef } from "@angular/core";
+import { ChangeDetectionStrategy, Component, Input, OnDestroy, forwardRef } from "@angular/core";
 import {
     ControlValueAccessor, FormBuilder, FormControl, FormGroup, NG_VALIDATORS, NG_VALUE_ACCESSOR, Validator, Validators,
 } from "@angular/forms";
-import { Subscription } from "rxjs";
-
-import { UserAccountElevationLevel } from "app/models";
+import { LoginMode, OSType, UserAccountElevationLevel } from "app/models";
 import { UserAccountDto } from "app/models/dtos";
+import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 
 export interface UserAccountFormModel {
     name: string;
     password: string;
     runElevated: boolean;
-    sshPrivateKey: string;
+    windowsUserConfiguration?: {
+        loginMode?: LoginMode;
+    },
+    linuxUserConfiguration?: {
+        sshPrivateKey?: string;
+        gid?: number;
+        uid?: number;
+    }
 }
 
-function userAccountToFormModel(userAccount: UserAccountDto) {
-    return {
+function userAccountToFormModel(userAccount: UserAccountDto): UserAccountFormModel {
+    const value: UserAccountFormModel = {
         name: userAccount.name,
         password: userAccount.password,
         runElevated: userAccount.elevationLevel === "admin",
-        sshPrivateKey: userAccount.sshPrivateKey,
     };
+
+    if (userAccount.linuxUserConfiguration) {
+        value.linuxUserConfiguration = userAccount.linuxUserConfiguration;
+    }
+    if (userAccount.windowsUserConfiguration) {
+        value.windowsUserConfiguration = userAccount.windowsUserConfiguration;
+    }
+    return value;
 }
 
-function userAccountToDto(userAccount: UserAccountFormModel): UserAccountDto {
-    return new UserAccountDto({
-        name: userAccount.name,
-        password: userAccount.password,
-        elevationLevel: userAccount.runElevated ? UserAccountElevationLevel.admin : UserAccountElevationLevel.nonadmin,
-        sshPrivateKey: userAccount.sshPrivateKey,
-    });
+function userAccountToDto(value: UserAccountFormModel, osType: OSType): UserAccountDto {
+    const commonProperties = {
+        name: value.name,
+        password: value.password,
+        elevationLevel: value.runElevated ? UserAccountElevationLevel.admin : UserAccountElevationLevel.nonadmin,
+
+    };
+    if (osType === OSType.Linux) {
+        return new UserAccountDto({
+            ...commonProperties,
+            linuxUserConfiguration: value.linuxUserConfiguration,
+        });
+    } else {
+        return new UserAccountDto({
+            ...commonProperties,
+            windowsUserConfiguration: value.windowsUserConfiguration,
+        });
+    }
 }
 
 @Component({
@@ -39,37 +64,70 @@ function userAccountToDto(userAccount: UserAccountFormModel): UserAccountDto {
         { provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => UserAccountPickerComponent), multi: true },
         { provide: NG_VALIDATORS, useExisting: forwardRef(() => UserAccountPickerComponent), multi: true },
     ],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserAccountPickerComponent implements ControlValueAccessor, Validator, OnDestroy {
+    public OSType = OSType;
+    public LoginMode = LoginMode;
+
+    @Input() public osType: OSType;
+
     public form: FormGroup;
 
     private _propagateChange: (value: UserAccountDto) => void = null;
-    private _sub: Subscription;
+    private _destroy = new Subject();
 
     constructor(formBuilder: FormBuilder) {
         this.form = formBuilder.group({
             name: ["", Validators.required],
             password: ["", Validators.required],
             runElevated: [false],
+            windowsUserConfiguration: formBuilder.group({
+                loginMode: [LoginMode.Batch],
+            }),
+            linuxUserConfiguration: formBuilder.group({
+                sshPrivateKey: [null],
+                gid: [null],
+                uid: [null],
+            }),
         });
 
-        this._sub = this.form.valueChanges.subscribe((val: any) => {
+        this.form.valueChanges.pipe(takeUntil(this._destroy)).subscribe((val: any) => {
             if (this._propagateChange) {
-                this._propagateChange(userAccountToDto(val));
+                this._propagateChange(userAccountToDto(val, this.osType));
             }
         });
     }
 
     public ngOnDestroy() {
-        this._sub.unsubscribe();
+        this._destroy.next();
+        this._destroy.complete();
 
     }
+
     public writeValue(value: UserAccountDto) {
+        console.log("Write value?", value);
         if (value) {
             this.form.patchValue(userAccountToFormModel(value));
         } else {
-            this.form.reset();
+            this.reset();
         }
+    }
+
+    public reset() {
+        this.form.setValue({
+            name: "",
+            password: "",
+            runElevated: false,
+            windowsUserConfiguration: {
+                loginMode: LoginMode.Batch,
+            },
+            linuxUserConfiguration: {
+                sshPrivateKey: null,
+                gid: null,
+                uid: null,
+            }
+        });
     }
 
     public registerOnChange(fn) {
--- src/app/models/dtos/user-account.dto.ts
+++ src/app/models/dtos/user-account.dto.ts
@@ -1,9 +1,20 @@
 import { Dto, DtoAttr } from "@batch-flask/core";
-import { UserAccountElevationLevel } from "../user-account";
+import { LoginMode, UserAccountElevationLevel } from "../user-account";
+
+export class WindowsUserConfigurationDto extends Dto<UserAccountDto> {
+    @DtoAttr() public loginMode?: LoginMode;
+}
+
+export class LinuxUserConfigurationDto extends Dto<UserAccountDto> {
+    @DtoAttr() public gid?: number;
+    @DtoAttr() public uid?: number;
+    @DtoAttr() public sshPrivateKey?: string;
+}
 
 export class UserAccountDto extends Dto<UserAccountDto> {
     @DtoAttr() public name: string;
     @DtoAttr() public password: string;
     @DtoAttr() public elevationLevel?: UserAccountElevationLevel;
-    @DtoAttr() public sshPrivateKey?: string;
+    @DtoAttr() public linuxUserConfiguration?: LinuxUserConfigurationDto;
+    @DtoAttr() public windowsUserConfiguration?: WindowsUserConfigurationDto;
 }
