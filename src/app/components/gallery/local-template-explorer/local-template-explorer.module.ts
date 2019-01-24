317ad1010313643eff421a6e440cee3bafdef8df
Timothee Guerin <tiguerin@microsoft.com>
2019-01-24 12:44:38 -0800

    Added button as well

--- src/app/components/gallery/local-template-explorer/local-template-explorer.module.ts
+++ src/app/components/gallery/local-template-explorer/local-template-explorer.module.ts
@@ -4,11 +4,13 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BaseModule } from "@batch-flask/ui";
 import { LocalTemplateDropZoneComponent } from "./local-template-drop-zone";
 import { LocalTemplateExplorerComponent } from "./local-template-explorer.component";
+import { LocalTemplatePickButtonComponent } from "./local-template-pick-button";
 import { LocalTemplateSourceFormComponent } from "./local-template-source-form";
 
 const publicComponents = [
     LocalTemplateExplorerComponent,
     LocalTemplateDropZoneComponent,
+    LocalTemplatePickButtonComponent,
 ];
 const privateComponents = [LocalTemplateSourceFormComponent];
 
