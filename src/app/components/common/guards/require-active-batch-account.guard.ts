d68d513036791cda5e2851b0f19bc5343bb15f36
Timothee Guerin <timothee.guerin@outlook.com>
2019-02-19 13:46:58 -0800

    Feature: Prompt for account when navigating (#1916)

---
+++ src/app/components/common/guards/require-active-batch-account.guard.ts
@@ -0,0 +1,40 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Router, RouterStateSnapshot } from "@angular/router";
+import { DialogService } from "@batch-flask/ui";
+import { BatchAccountService } from "app/services";
+import { of } from "rxjs";
+import { map, switchMap, take } from "rxjs/operators";
+import { SelectAccountDialogComponent } from "./select-acccount-dialog";
+
+@Injectable()
+export class RequireActiveBatchAccountGuard implements CanActivate, CanLoad {
+    constructor(
+        private accountService: BatchAccountService,
+        private dialogService: DialogService,
+        private router: Router,
+    ) {
+    }
+
+    public canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        return this.accountService.currentAccountId.pipe(
+            take(1),
+            switchMap((accountId) => {
+                if (accountId) { return of(true); }
+                const ref = this.dialogService.open(SelectAccountDialogComponent);
+                return ref.beforeClosed().pipe(
+                    map((pickedAccountId) => {
+                        if (pickedAccountId) {
+                            return true;
+                        } else {
+                            return this.router.createUrlTree(["/accounts"]);
+                        }
+                    }),
+                );
+            }),
+        );
+    }
+
+    public canLoad() {
+        return true;
+    }
+}
