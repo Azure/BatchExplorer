cdc3b90f059f8b4d6b5298d9cbfc170dd71c5256
Timothee Guerin <timothee.guerin@outlook.com>
2019-03-07 08:58:20 -0800

    Internal: Get ready for billing to switch to be at the pool level (#1941)

--- src/app/services/azure-cost-management/azure-cost-management.service.spec.ts
+++ src/app/services/azure-cost-management/azure-cost-management.service.spec.ts
@@ -7,7 +7,9 @@ const sub1 = new ArmSubscription({
     subscriptionId: "sub1",
 });
 
-describe("UsageDetailsService", () => {
+const accountId = "/subscriptions/sub-1/resourcegroups/rg1/providers/Microsoft.Batch/batchaccounts/acc-1";
+
+describe("AzureCostManagementService", () => {
     let service: AzureCostManagementService;
     let accountServiceSpy;
     let azureHttpSpy;
@@ -15,7 +17,7 @@ describe("UsageDetailsService", () => {
     beforeEach(() => {
         accountServiceSpy = {
             currentAccount: new BehaviorSubject(new ArmBatchAccount({
-                id: "/subs/sub-1/batchaccounts/acc-1",
+                id: accountId,
                 name: "acc-1",
                 location: "westus",
                 properties: {} as any,
@@ -31,19 +33,20 @@ describe("UsageDetailsService", () => {
                         columns: [
                             { name: "PreTaxCost" },
                             { name: "UsageDate" },
+                            { name: "ResourceId" },
                             { name: "MeterCategory" },
                             { name: "MeterSubCategory" },
                             { name: "Currency" },
                         ],
                         rows: [
-                            [1, 20190201, "virtual machines", "a series", "USD"],
-                            [79, 20190201, "virtual machines", "d series", "USD"],
-                            [1.5, 20190202, "virtual machines", "a series", "USD"],
-                            [12, 20190202, "virtual machines", "d series", "USD"],
-                            [8, 20190203, "virtual machines", "a series", "USD"],
-                            [39, 20190203, "virtual machines", "d series", "USD"],
-                            [3.8, 20190204, "virtual machines", "a series", "USD"],
-                            [27, 20190204, "virtual machines", "d series", "USD"],
+                            [1, 20190201, accountId, "virtual machines", "a series", "USD"],
+                            [79, 20190201, accountId, "virtual machines", "d series", "USD"],
+                            [1.5, 20190202, accountId, "virtual machines", "a series", "USD"],
+                            [12, 20190202, accountId, "virtual machines", "d series", "USD"],
+                            [8, 20190203, accountId, "virtual machines", "a series", "USD"],
+                            [39, 20190203, accountId, "virtual machines", "d series", "USD"],
+                            [3.8, 20190204, accountId, "virtual machines", "a series", "USD"],
+                            [27, 20190204, accountId, "virtual machines", "d series", "USD"],
                         ],
                     },
                 });
@@ -55,19 +58,21 @@ describe("UsageDetailsService", () => {
 
     it("get the usage for the current account", async () => {
         const usages = await service.getCost().toPromise();
+        const resourceId = accountId;
+        const currency = "USD";
         expect(azureHttpSpy.post).toHaveBeenCalledTimes(1);
         expect(azureHttpSpy.post).toHaveBeenCalledWith(sub1,
-            "subscriptions/sub1/providers/Microsoft.CostManagement/query",
+            "/subscriptions/sub-1/resourceGroups/rg1/providers/Microsoft.CostManagement/query",
             jasmine.anything());
         expect(usages).toEqual([
-            { preTaxCost: 1, date: new Date(2019, 1, 1), meter: "virtual machines (a series)", currency: "USD" },
-            { preTaxCost: 79, date: new Date(2019, 1, 1), meter: "virtual machines (d series)", currency: "USD" },
-            { preTaxCost: 1.5, date: new Date(2019, 1, 2), meter: "virtual machines (a series)", currency: "USD" },
-            { preTaxCost: 12, date: new Date(2019, 1, 2), meter: "virtual machines (d series)", currency: "USD" },
-            { preTaxCost: 8, date: new Date(2019, 1, 3), meter: "virtual machines (a series)", currency: "USD" },
-            { preTaxCost: 39, date: new Date(2019, 1, 3), meter: "virtual machines (d series)", currency: "USD" },
-            { preTaxCost: 3.8, date: new Date(2019, 1, 4), meter: "virtual machines (a series)", currency: "USD" },
-            { preTaxCost: 27, date: new Date(2019, 1, 4), meter: "virtual machines (d series)", currency: "USD" },
+            { preTaxCost: 1, date: new Date(2019, 1, 1), meter: "virtual machines (a series)", currency, resourceId },
+            { preTaxCost: 79, date: new Date(2019, 1, 1), meter: "virtual machines (d series)", currency, resourceId },
+            { preTaxCost: 1.5, date: new Date(2019, 1, 2), meter: "virtual machines (a series)", currency, resourceId },
+            { preTaxCost: 12, date: new Date(2019, 1, 2), meter: "virtual machines (d series)", currency, resourceId },
+            { preTaxCost: 8, date: new Date(2019, 1, 3), meter: "virtual machines (a series)", currency, resourceId },
+            { preTaxCost: 39, date: new Date(2019, 1, 3), meter: "virtual machines (d series)", currency, resourceId },
+            { preTaxCost: 3.8, date: new Date(2019, 1, 4), meter: "virtual machines (a series)", currency, resourceId },
+            { preTaxCost: 27, date: new Date(2019, 1, 4), meter: "virtual machines (d series)", currency, resourceId },
         ]);
     });
 });
