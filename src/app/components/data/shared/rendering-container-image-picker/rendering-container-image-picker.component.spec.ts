b6650c610853dbb323bf851925d4a3cbbd7c7c19
David Kydd <dakydd@microsoft.com>
2019-03-11 11:55:46 +1300

    Remove fdescribe

--- src/app/components/data/shared/rendering-container-image-picker/rendering-container-image-picker.component.spec.ts
+++ src/app/components/data/shared/rendering-container-image-picker/rendering-container-image-picker.component.spec.ts
@@ -26,7 +26,7 @@ class TestComponent {
     public imageReferenceId: string;
 }
 
-fdescribe("RenderingContainerImagePickerComponent", () => {
+describe("RenderingContainerImagePickerComponent", () => {
     let fixture: ComponentFixture<TestComponent>;
     let testComponent: TestComponent;
     let de: DebugElement;
