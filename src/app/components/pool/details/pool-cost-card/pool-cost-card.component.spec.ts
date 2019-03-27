5fb59a4cccc242b05faee5745f3599f344109b89
Timothee Guerin <tiguerin@microsoft.com>
2019-03-27 16:27:02 -0700

    wip

---
+++ src/app/components/pool/details/pool-cost-card/pool-cost-card.component.spec.ts
@@ -0,0 +1,137 @@
+import { Component, DebugElement, Input } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { By } from "@angular/platform-browser";
+import {
+    I18nTestingModule, MockControlValueAccessorComponent, controlValueAccessorProvider,
+} from "@batch-flask/core/testing";
+import { ChartsModule, QuickRange, TimeRange } from "@batch-flask/ui";
+import { ArmBatchAccount, ArmSubscription, LocalBatchAccount } from "app/models";
+import { BatchAccountService, Theme, ThemeService } from "app/services";
+import { AzureCostManagementService } from "app/services/azure-cost-management";
+import { BehaviorSubject, of } from "rxjs";
+import { PoolCostCardComponent } from "./pool-cost-card.component";
+
+const sub1 = new ArmSubscription({
+    id: "/subscriptions/sub1",
+    subscriptionId: "sub1",
+});
+
+const day1 = new Date(2019, 1, 1);
+const day2 = new Date(2019, 1, 2);
+const day3 = new Date(2019, 1, 3);
+const day4 = new Date(2019, 1, 4);
+
+const costs = [
+    { preTaxCost: 1, date: day1, meter: "virtual machines (a series)", currency: "USD" },
+    { preTaxCost: 1.5, date: day2, meter: "virtual machines (a series)", currency: "USD" },
+    { preTaxCost: 8, date: day3, meter: "virtual machines (a series)", currency: "USD" },
+    { preTaxCost: 39, date: day3, meter: "virtual machines (d series)", currency: "USD" },
+    { preTaxCost: 3.8, date: day4, meter: "virtual machines (a series)", currency: "USD" },
+    { preTaxCost: 27, date: day4, meter: "virtual machines (d series)", currency: "USD" },
+];
+
+@Component({
+    selector: "bl-time-range-picker", template: "",
+    providers: [controlValueAccessorProvider(() => FakeTimeRangePickerComponent)],
+})
+class FakeTimeRangePickerComponent extends MockControlValueAccessorComponent<TimeRange | null> {
+    @Input() public quickRanges: QuickRange[];
+}
+
+@Component({
+    template: `<bl-pool-cost-card></bl-pool-cost-card>`,
+})
+class TestComponent {
+}
+
+describe("AccountCostCardComponent", () => {
+    let fixture: ComponentFixture<TestComponent>;
+    let component: PoolCostCardComponent;
+    let de: DebugElement;
+
+    let costServiceSpy;
+    let accountServiceSpy;
+    let themeServiceSpy;
+
+    beforeEach(() => {
+        costServiceSpy = {
+            getCost: jasmine.createSpy("getCost").and.callFake(() => {
+                return of(costs);
+            }),
+        };
+
+        accountServiceSpy = {
+            currentAccount: new BehaviorSubject(new ArmBatchAccount({
+                id: "/subs/sub-1/batchaccounts/acc-1",
+                name: "acc-1",
+                location: "westus",
+                properties: {} as any,
+                subscription: sub1,
+            })),
+        };
+
+        themeServiceSpy = {
+            currentTheme: of(new Theme({
+                "chart-colors": [
+                    "#003f5c",
+                    "#aa3939",
+                    "#4caf50",
+                    "#ffa600",
+                ],
+            } as any)),
+        };
+
+        TestBed.configureTestingModule({
+            imports: [I18nTestingModule, ChartsModule, FormsModule, ReactiveFormsModule],
+            declarations: [PoolCostCardComponent, FakeTimeRangePickerComponent, TestComponent],
+            providers: [
+                { provide: AzureCostManagementService, useValue: costServiceSpy },
+                { provide: ThemeService, useValue: themeServiceSpy },
+                { provide: BatchAccountService, useValue: accountServiceSpy },
+            ],
+        });
+        fixture = TestBed.createComponent(TestComponent);
+        de = fixture.debugElement.query(By.css("bl-account-cost-card"));
+        component = de.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it("shows unsupported message when using local batch account", () => {
+        accountServiceSpy.currentAccount.next(new LocalBatchAccount({ name: "foo" }));
+        fixture.detectChanges();
+        const info = de.query(By.css(".unavailable-info"));
+        expect(info).not.toBeFalsy();
+        expect(info.nativeElement.textContent).toContain("account-cost-card.usingLocalAccount");
+        expect(info.nativeElement.textContent).not.toContain("account-cost-card.unsupportedSubscription");
+    });
+
+    it("builds the datasets", () => {
+        const dataset1 = {
+            label: "virtual machines (a series)",
+            backgroundColor: "#003f5c",
+            borderColor: "#003f5c",
+            data: [
+                { x: day1, y: 1 },
+                { x: day2, y: 1.5 },
+                { x: day3, y: 8 },
+                { x: day4, y: 3.8 },
+            ],
+        };
+        const dataset2 = {
+            label: "virtual machines (d series)",
+            backgroundColor: "#aa3939",
+            borderColor: "#aa3939",
+            data: [
+                { x: day1, y: 0 },
+                { x: day2, y: 0 },
+                { x: day3, y: 39 },
+                { x: day4, y: 27 },
+            ],
+        };
+        expect(component.datasets).toEqual([
+            dataset1,
+            dataset2,
+        ]);
+    });
+});
---
+++ src/app/components/pool/details/pool-cost-card/pool-cost-card.component.ts
@@ -0,0 +1,172 @@
+import {
+    ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnChanges, OnDestroy, OnInit,
+} from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { QuickRange, TimeRange } from "@batch-flask/ui";
+import { log } from "@batch-flask/utils";
+import { ArmBatchAccount } from "app/models";
+import { BatchAccountService, Theme, ThemeService } from "app/services";
+import {
+    UsageDetailsUnsupportedSubscription,
+} from "app/services/azure-consumption";
+import { AzureCostManagementService, BatchPoolCost } from "app/services/azure-cost-management";
+import { DateTime } from "luxon";
+import { BehaviorSubject, Subject, combineLatest, of } from "rxjs";
+import { catchError, filter, startWith, switchMap, takeUntil } from "rxjs/operators";
+
+import "./pool-cost-card.scss";
+
+const today = DateTime.local();
+
+const thisMonthRange = new QuickRange({
+    label: "This month",
+    start: today.startOf("month").toJSDate(),
+    end: today.endOf("month").toJSDate(),
+});
+
+@Component({
+    selector: "bl-pool-cost-card",
+    templateUrl: "pool-cost-card.html",
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PoolCostCardComponent implements OnInit, OnChanges, OnDestroy {
+    @Input() public poolId: string;
+
+    public chartType = "bar";
+    public datasets: Chart.ChartDataSets[] = [];
+    public options: Chart.ChartOptions = {};
+    public currency: string;
+    public isArmBatchAccount: boolean;
+    public unsupportedSubscription: boolean;
+    public total: string;
+
+    public timeRange = new FormControl<TimeRange>(thisMonthRange);
+
+    private _poolId = new BehaviorSubject<string | null>(null);
+    private _destroy = new Subject();
+
+    constructor(
+        private costService: AzureCostManagementService,
+        private accountService: BatchAccountService,
+        private themeService: ThemeService,
+        private changeDetector: ChangeDetectorRef) {
+    }
+
+    public ngOnInit() {
+        const currentAccountObs = this.accountService.currentAccount.pipe(
+            filter((account) => {
+                this.isArmBatchAccount = account instanceof ArmBatchAccount;
+                if (this.isArmBatchAccount) {
+                    this._updateUsages();
+                } else {
+                    this.unsupportedSubscription = false;
+                }
+                this.changeDetector.markForCheck();
+                return this.isArmBatchAccount;
+            }),
+        );
+        const obs = combineLatest(
+            this.timeRange.valueChanges.pipe(startWith(this.timeRange.value)),
+            currentAccountObs,
+        ).pipe(
+            switchMap(([timeRange, _]: [TimeRange, ArmBatchAccount]) => {
+                return this.costService.getCost(timeRange).pipe(
+                    catchError((error) => {
+                        if (error instanceof UsageDetailsUnsupportedSubscription) {
+                            this.unsupportedSubscription = true;
+                            this.changeDetector.markForCheck();
+                        } else {
+                            log.error("Error retrieving cost", error);
+                        }
+                        return of(null);
+                    }),
+                );
+            }),
+        );
+
+        combineLatest(obs, this._poolId, this.themeService.currentTheme).pipe(
+            takeUntil(this._destroy),
+        ).subscribe({
+            next: ([accountCost, poolId, theme]) => {
+                if (accountCost) {
+                    this.unsupportedSubscription = false;
+                    this._computeDataSets(accountCost.currency, poolId, accountCost.pools[poolId], theme);
+                }
+            },
+        });
+        this._setChartOptions();
+    }
+
+    public ngOnChanges(changes) {
+        if (changes.poolId) {
+            this._poolId.next(this.poolId);
+        }
+    }
+    public ngOnDestroy() {
+        this._destroy.next();
+        this._destroy.complete();
+    }
+
+    private _updateUsages() {
+        this.unsupportedSubscription = false;
+        this.changeDetector.markForCheck();
+    }
+
+    private _computeDataSets(currency: string, poolId: string, poolCost: BatchPoolCost | null, theme: Theme) {
+        this.currency = currency;
+        this.total = poolCost.totalForPeriod.toFixed(2);
+
+        const color = theme.chartColors.get(2);
+        this.datasets = [{
+                label: poolId,
+                backgroundColor: color,
+                borderColor: color,
+                data: poolCost ? poolCost.costs.map((x) => {
+                    return { x: x.date, y: x.preTaxCost };
+                }) : [],
+        }];
+        this._setChartOptions();
+        this.changeDetector.markForCheck();
+    }
+
+    private _setChartOptions() {
+        this.options = {
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: {
+                display: false,
+            },
+            elements: {
+                point: {
+                    radius: 0,
+                    hitRadius: 10,
+                    hoverRadius: 3,
+                },
+                line: {
+                    tension: 0.05,
+                    borderWidth: -10,
+                },
+            },
+            scales: {
+                yAxes: [{
+                    stacked: true,
+                    ticks: {
+                        beginAtZero: true,
+                    },
+                }],
+                xAxes: [{
+                    stacked: true,
+                    type: "time",
+                    display: false,
+                    distribution: "linear",
+                    position: "bottom",
+                    time: {
+                        unit: "day",
+                        unitStepSize: 1,
+                    },
+                }],
+            },
+        };
+        this.changeDetector.markForCheck();
+    }
+}
