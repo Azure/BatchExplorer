0e0acac69cfb7e5ac843f7ee09206f5b6b3bae8b
Timothee Guerin <tiguerin@microsoft.com>
2019-04-24 08:13:10 -0700

    Pool picker

--- src/app/components/job/action/add/pool-picker/pool-picker.component.ts
+++ src/app/components/job/action/add/pool-picker/pool-picker.component.ts
@@ -14,7 +14,6 @@ import {
     NG_VALIDATORS,
     NG_VALUE_ACCESSOR,
 } from "@angular/forms";
-import { isNotNullOrUndefined } from "@batch-flask/core";
 import { LoadingStatus } from "@batch-flask/ui";
 import { Offer, Pool } from "app/models";
 import {
@@ -33,7 +32,6 @@ import { List } from "immutable";
 import { BehaviorSubject, Subject, combineLatest } from "rxjs";
 import {
     distinctUntilChanged,
-    filter,
     map,
     startWith,
     takeUntil,
@@ -115,7 +113,6 @@ export class PoolPickerComponent
         ).pipe(
             takeUntil(this._destroy),
             map(([pools, filters, inputs, containerImages]) => {
-                console.log("pools", pools, filters, inputs, containerImages);
                 return pools
                     .filter(pool => {
                         return this._filterPool(
