3154a2b748ac397a279463b88e0af9dc79790b93
rechen <rechen@microsoft.com>
2019-10-01 14:46:49 -0700

    Minor cleanup

--- src/app/components/pool/start-task/start-task-picker.component.spec.ts
+++ src/app/components/pool/start-task/start-task-picker.component.spec.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from "@angular/common";
 import { Component, DebugElement, Input } from "@angular/core";
-import { ComponentFixture, TestBed, async } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatCheckboxModule } from "@angular/material";
 import { By } from "@angular/platform-browser";
