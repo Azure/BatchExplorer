b18eccbd628cbbc93ff2bfb7ab31d8ea4f383336
Brandon Klein <bgklein@wisc.edu>
2019-10-15 11:08:26 -0700

    Add ability to filter tasks on execution result (#2099)
    * Add ability to filter tasks on execution result
    
    * fix lint:
    
    * Trailing commas
    
    * add scss and move to folders

--- src/app/components/task/browse/filter/task-advanced-filter.component.ts
+++ src/app/components/task/browse/filter/task-advanced-filter.component.ts
@@ -1,7 +1,12 @@
 import { Component, EventEmitter, Output } from "@angular/core";
 import { Filter } from "@batch-flask/core";
-import { AdvancedFilter, ListFilterControl, StatePickerControl } from "@batch-flask/ui/advanced-filter";
-import { TaskState } from "app/models";
+import {
+    AdvancedFilter,
+    ListFilterControl,
+    ResultPickerControl,
+    StatePickerControl,
+} from "@batch-flask/ui/advanced-filter";
+import { TaskResult, TaskState } from "app/models";
 import { ODataFields } from "common/constants";
 
 @Component({
@@ -17,6 +22,9 @@ export class TaskAdvancedFilterComponent {
         this.advancedFilter = new AdvancedFilter({
             [ODataFields.state]: new StatePickerControl("State", [
                 TaskState.active, TaskState.completed, TaskState.running, TaskState.preparing,
+            ]),
+            [ODataFields.result]: new ResultPickerControl("Result", [
+                TaskResult.success, TaskResult.failure,
             ]),
             [ODataFields.taskExitCode]: new ListFilterControl("Exit code", {
                 number: true,
