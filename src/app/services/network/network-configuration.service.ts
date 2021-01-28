7b00e8ad2edef5e9f18cf5794117adf5931ce080
Rena Chen <rena.chen@microsoft.com>
2021-01-27 22:31:08 -0800

    Hotfix for classic vnet not found issue

--- src/app/services/network/network-configuration.service.ts
+++ src/app/services/network/network-configuration.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
+import { log } from "@batch-flask/utils/logging";
+import { of } from "rxjs";
 import { Observable } from "rxjs";
-import { first, map } from "rxjs/operators";
+import { catchError, first, map } from "rxjs/operators";
 import { ArmHttpService } from "../arm-http.service";
 import { ArmListResponse } from "../core";
 
@@ -47,6 +49,10 @@ export class NetworkConfigurationService {
                 }
                 return this._filterByLocation(response.value, location, type);
             }),
+            catchError((error) => {
+                log.error("Unable to list ARM vnets", error);
+                return of([]);
+            }),
             first(),
         );
     }
@@ -65,6 +71,10 @@ export class NetworkConfigurationService {
                     return [];
                 }
                 return this._filterByLocation(response.value, location, type);
+            }),
+            catchError((error) => {
+                log.error("Unable to list classic vnets", error);
+                return of([]);
             }),
             first(),
         );
