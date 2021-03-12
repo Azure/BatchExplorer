5f70ba747f7a64f56ae73ebc1646f91a09761af2
Rena <cRui861@users.noreply.github.com>
2021-03-12 15:22:32 -0800

    Update to Resource Skus to retrieve vm size data (#2294)
    * ARM API versions can now be specified per-resource
    
    Added an API version for listing compute SKUs
    
    * Update vm-size.ts and vm-size.service.ts with new resourceSku API data
    * Check for response.value always 0 indexed
    * Clean up console logs
    
    * Fix url for arm response call
    * Update vmSize test suite for new response data
    
    * Update tests for bad response data and extra capability response data
    
    * Clean up console logs
    
    * Fix linting issues
    
    * Fix vm sizes table not showing up
    
    Co-authored-by: David Watrous <dave.watrous@microsoft.com>

--- src/app/models/vm-size.ts
+++ src/app/models/vm-size.ts
@@ -1,8 +1,11 @@
 import { Model, Prop, Record } from "@batch-flask/core";
+import { symlink } from "fs";
+import { List } from "immutable";
 
 export interface VmSizeAttributes {
     name: string;
     numberOfCores: number;
+    numberOfGpus: number;
     osDiskSizeInMB: number;
     resourceDiskSizeInMB: number;
     memoryInMB: number;
@@ -12,12 +15,12 @@ export interface VmSizeAttributes {
 @Model()
 export class VmSize extends Record<VmSizeAttributes> {
     /**
-     * Id for the vmSize(This is computed form the name attribute)
+     * Id for the vmSize (this is computed from the name attribute).
      */
     @Prop() public id: string;
 
     /**
-     * Specifies the name of the virtual machine size
+     * Specifies the name of the virtual machine size.
      */
     @Prop() public name: string;
 
@@ -25,6 +28,11 @@ export class VmSize extends Record<VmSizeAttributes> {
      * Specifies the number of available CPU cores.
      */
     @Prop() public numberOfCores: number;
+
+    /**
+     * Specifies the number of available GPUs.
+     */
+    @Prop() public numberOfGpus: number;
 
     /**
      * Specifies the size in MB of the operating system disk.
@@ -49,4 +57,48 @@ export class VmSize extends Record<VmSizeAttributes> {
     constructor(data: VmSizeAttributes) {
         super({ ...data, id: data.name && data.name.toLowerCase() } as any);
     }
+}
+
+/**
+ * Maps capabilities from Resource SKUs JSON response to VM size model.
+ * @param skuJson response value from ARM
+ */
+export function mapResourceSkuToVmSize(skuJson: any[]): List<VmSize> {
+    const skuToCapabilities = _getCapabilitiesMap(skuJson);
+    const skuToVmSize: VmSize[] = [];
+    for (const skuName in skuToCapabilities) {
+        const vmSize: Partial<VmSizeAttributes> = {};
+        const capabilities = skuToCapabilities[skuName];
+        vmSize.name = skuName;
+        vmSize.numberOfCores = _parseIntOrReturnDefault(capabilities.get("vCPUs"));
+        vmSize.numberOfGpus = _parseIntOrReturnDefault(capabilities.get("GPUs"));
+        vmSize.osDiskSizeInMB = _parseIntOrReturnDefault(capabilities.get("OSVhdSizeMB"));
+        vmSize.resourceDiskSizeInMB = _parseIntOrReturnDefault(capabilities.get("MaxResourceVolumeMB"));
+        vmSize.memoryInMB = _parseFloatOrReturnDefault(capabilities.get("MemoryGB")) * 1024;
+        vmSize.maxDataDiskCount = _parseIntOrReturnDefault(capabilities.get("MaxDataDiskCount"));
+        skuToVmSize.push(new VmSize(vmSize as VmSizeAttributes));
+    }
+    return List<VmSize>(skuToVmSize);
+}
+
+function _getCapabilitiesMap(skuJson: any[]): any {
+    const skuToCapabilities = {};
+    for (let i = 0; i < skuJson.length; i++) {
+        const sku = skuJson[i];
+        if (sku.capabilities) {
+            const capabilities = new Map(sku.capabilities.map(capability => [capability.name, capability.value]));
+            skuToCapabilities[sku.name] = capabilities;
+        }
+    }
+    return skuToCapabilities;
+}
+
+function _parseIntOrReturnDefault(skuCapabilityValue : string): number {
+    const value = parseInt(skuCapabilityValue);
+    return isNaN(value) ? 0 : value;
+}
+
+function _parseFloatOrReturnDefault(skuCapabilityValue : string): number {
+    const value = parseFloat(skuCapabilityValue);
+    return isNaN(value) ? 0 : value;
 }
