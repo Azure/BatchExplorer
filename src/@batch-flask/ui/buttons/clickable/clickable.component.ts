5f007e087948b47c8eec988e82aed3dc7f044829
Sanjana Kapur <t-skapur@microsoft.com>
2021-05-26 19:16:17 -0400

    Fixed so only selected gallery app has true value

--- src/@batch-flask/ui/buttons/clickable/clickable.component.ts
+++ src/@batch-flask/ui/buttons/clickable/clickable.component.ts
@@ -31,10 +31,6 @@ export class ClickableComponent implements OnChanges, OnDestroy {
      */
     @Input() public disabled = false;
 
-    /**
-     * Set this to false to show that the element has not been selected yet
-     */
-    @Input() public selected = false;
 
     /**
      * Optional requirement to be able to access this button
@@ -61,10 +57,6 @@ export class ClickableComponent implements OnChanges, OnDestroy {
     // Aria
     @Input() @HostBinding("attr.role") public role = "button";
     @HostBinding("attr.aria-disabled") public get ariaDisabled() { return this.disabled; }
-
-    @HostBinding("attr.aria-selected") public get ariaSelected() {
-        return this.selected;
-    } 
  
     public subtitle = "";
 
@@ -104,7 +96,6 @@ export class ClickableComponent implements OnChanges, OnDestroy {
 
     @HostListener("click", ["$event"])
     public handleClick(event: Event) {
-        this.selected = true;
         event.preventDefault();
         event.stopPropagation();
         this.handleAction(event);
