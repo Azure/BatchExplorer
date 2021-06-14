d8a4384758d2b60771e4f8edb43b3de9e2a54e60
Shiran Pasternak <shiran.pasternak@microsoft.com>
2021-06-14 18:23:47 -0400

    Publish script bumps version post-release
    * Creates merge branch for bumping version in package.json and merging stable back into master (subsequent manual step).
    * Manually bumped version for insider builds.

--- scripts/publish/publish.ts
+++ scripts/publish/publish.ts
@@ -7,6 +7,8 @@ import {
     createIssue, createPullRequest, getMilestone, githubToken, listMilestoneIssues, listPullRequests,
 } from "./github-api";
 
+const MAIN_BRANCH = "master";
+
 const root = path.resolve(path.join(__dirname, "../.."));
 const allMessages: string[] = [];
 const repoName = "Azure/BatchExplorer";
@@ -54,14 +56,14 @@ function checkGithubToken() {
 /**
  * This goes back to master and pull
  */
-async function gotoMaster() {
-    await run("git checkout master");
+async function gotoMainBranch() {
+    await run(`git checkout ${MAIN_BRANCH}`);
     await run("git pull");
-    success("Checkout to master branch and pulled latest");
+    success(`Checkout to ${MAIN_BRANCH} branch and pulled latest`);
 }
 
-async function loadMillestone(millestoneId: number) {
-    return getMilestone(repoName, millestoneId);
+async function loadMilestone(milestoneId: number) {
+    return getMilestone(repoName, milestoneId);
 }
 
 async function getCurrentBranch(): Promise<string> {
@@ -69,24 +71,29 @@ async function getCurrentBranch(): Promise<string> {
     return stdout.trim();
 }
 
-function getMillestoneId() {
+function getMilestoneId() {
     if (process.argv.length < 3) {
-        throw new Error("No millestone id was provided.");
+        throw new Error("No milestone id was provided.");
     }
     return parseInt(process.argv[2], 10);
 }
 
 async function confirmVersion(version: string) {
     return new Promise((resolve, reject) => {
-        ask(`Up program to be version ${version} (From millestone title) [Y/n]`, true, (ok) => {
+        ask(`Up program to be version ${version} (From milestone title) [Y/n]`, true, (ok) => {
             if (ok) {
                 success(`A new release for version ${version} will be prepared`);
                 resolve(null);
             } else {
-                reject(new Error("Millestone version wasn't confirmed. Please change millestone title"));
+                reject(new Error("milestone version wasn't confirmed. Please change milestone title"));
             }
         });
     });
+}
+
+function calcNextVersion(version: string) {
+    const match = /^(\d+\.)(\d+)(\.\d+)$/.exec(version);
+    return `${match[1]}${parseInt(match[2], 10) + 1}${match[3]}`;
 }
 
 function getPreparationBranchName(version: string) {
@@ -94,17 +101,26 @@ function getPreparationBranchName(version: string) {
 }
 
 async function switchToNewBranch(branchName: string) {
-    await run(`git checkout -b ${branchName}`);
+    await run(`git checkout -B ${branchName}`);
     success(`Created a new branch ${branchName}`);
 }
 
 async function bumpVersion(version) {
-    await run(`npm version --no-git-tag-version --allow-same-version ${version}`);
-    success(`Updated version in package.json to ${version}`);
+    const currentBranch = getCurrentBranch();
+    const nextVersion = calcNextVersion(version);
+    const bumpBranch = `release/bump-${nextVersion}`;
+    await gotoMainBranch();
+    await switchToNewBranch(bumpBranch);
+    await run(`npm version --no-git-tag-version --allow-same-version ${nextVersion}`);
+
+    await run(`git commit -am "Bump version to ${nextVersion}"`);
+    await run(`git push origin ${bumpBranch}`);
+    await run(`git checkout "${currentBranch}"`);
+    success(`Updated version in package.json to ${nextVersion} (branch: ${bumpBranch})`);
 }
 
-async function updateChangeLog(version, millestoneId) {
-    const { stdout } = await run(`gh-changelog-gen --repo ${repoName} ${millestoneId} --formatter markdown`);
+async function updateChangeLog(version, milestoneId) {
+    const { stdout } = await run(`gh-changelog-gen --repo ${repoName} ${milestoneId} --formatter markdown`);
     const changelogFile = path.join(root, "CHANGELOG.md");
     const changelogContent = fs.readFileSync(changelogFile);
 
@@ -129,14 +145,14 @@ async function push(branchName: string) {
     await run(`git push --set-upstream origin ${branchName}`);
 }
 
-async function createIssueIfNot(millestoneId, version) {
+async function createIssueIfNot(milestoneId, version) {
     const title = `Prepare for release of version ${version}`;
-    const issues = await listMilestoneIssues(repoName, millestoneId);
+    const issues = await listMilestoneIssues(repoName, milestoneId);
     let issue = issues.filter(x => x.title === title)[0];
     if (issue) {
         success(`Issue was already created earlier ${issue.html_url}`);
     } else {
-        issue = await createIssue(repoName, title, newIssueBody, millestoneId);
+        issue = await createIssue(repoName, title, newIssueBody, milestoneId);
         success(`Created a new issue ${issue.html_url}`);
     }
     return issue;
@@ -166,27 +182,27 @@ async function buildApp() {
 
 async function startPublish() {
     checkGithubToken();
-    const millestoneId = getMillestoneId();
-    const millestone = await loadMillestone(millestoneId);
-    if (!millestone.title && millestone["message"]) {
-        throw new Error(`Error fetching milestone: ${millestone["message"]}`);
+    const milestoneId = getMilestoneId();
+    const milestone = await loadMilestone(milestoneId);
+    if (!milestone.title && milestone["message"]) {
+        throw new Error(`Error fetching milestone: ${milestone["message"]}`);
     }
-    const version = millestone.title;
+    const version = milestone.title;
     await confirmVersion(version);
     const releaseBranch = getPreparationBranchName(version);
     const branch = await getCurrentBranch();
     if (branch !== releaseBranch) {
-        await gotoMaster();
+        await gotoMainBranch();
         await switchToNewBranch(releaseBranch);
     }
-    await bumpVersion(version);
-    await updateChangeLog(version, millestoneId);
+    await updateChangeLog(version, milestoneId);
     await updateThirdParty();
     await commitChanges();
     await push(releaseBranch);
-    const issue = await createIssueIfNot(millestoneId, version);
+    const issue = await createIssueIfNot(milestoneId, version);
     await createPullrequestIfNot(version, releaseBranch, issue);
     await buildApp();
+    await bumpVersion(version);
 }
 
 startPublish().then(() => {
