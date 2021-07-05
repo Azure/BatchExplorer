b07bc82d5106709091913554f5b8b9db3f2811da
David Watrous <509299+dpwatrous@users.noreply.github.com>
2021-07-05 10:08:47 -0400

    Added show/hide toggle to property group

--- packages/react/src/ui-react/components/property/property-group.tsx
+++ packages/react/src/ui-react/components/property/property-group.tsx
@@ -1,20 +1,53 @@
+import { Icon } from "@fluentui/react/lib/Icon";
 import { Stack } from "@fluentui/react/lib/Stack";
 import * as React from "react";
+import { useAppTheme } from "../../theme";
 
 export interface PropertyGroupProps {
     title?: string;
-    label?: string;
+    collapsed?: boolean;
 }
 
 /**
  * Displays a grouping of key/value pairs with an optional title
  */
 export const PropertyGroup: React.FC<PropertyGroupProps> = (props) => {
-    const titleEl = props.title ? <div>{props.title}</div> : undefined;
+    const theme = useAppTheme();
+
+    const [collapsed, setCollapsed] = React.useState<boolean>(
+        props.collapsed ?? false
+    );
+
+    const toggleCollapsed: (
+        event: React.MouseEvent<HTMLElement, MouseEvent>
+    ) => void = React.useCallback(() => {
+        setCollapsed(!collapsed);
+    }, [setCollapsed, collapsed]);
+
+    const titleEl = props.title ? (
+        <h3
+            style={{ color: theme.palette.themePrimary }}
+            onClick={toggleCollapsed}
+        >
+            <Icon
+                iconName={collapsed ? "ChevronRight" : "ChevronDown"}
+                style={{ marginRight: "8px", verticalAlign: "middle" }}
+            ></Icon>
+            {props.title}
+        </h3>
+    ) : undefined;
+
     return (
         <>
             {titleEl}
-            <Stack tokens={{ childrenGap: 8 }}>{props.children}</Stack>
+            <Stack
+                style={{
+                    display: collapsed ? "none" : undefined,
+                }}
+                tokens={{ childrenGap: 8 }}
+            >
+                {props.children}
+            </Stack>
         </>
     );
 };
