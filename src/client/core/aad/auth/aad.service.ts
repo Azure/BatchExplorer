f5100aa5054d84f763e8689683a7f5555615770b
David Watrous <509299+dpwatrous@users.noreply.github.com>
2021-07-29 09:21:10 -0400

    Fix: Unable to log in with personal (MSA) accounts
    Fixes #2344

--- src/client/core/aad/auth/aad.service.ts
+++ src/client/core/aad/auth/aad.service.ts
@@ -15,6 +15,7 @@ import { Deferred } from "common/deferred";
 import { dialog } from "electron";
 import { BehaviorSubject, Observable } from "rxjs";
 import { AADConfig } from "../aad-config";
+import { defaultTenant } from "../aad-constants";
 import AuthProvider from "../auth-provider";
 import {
     AuthenticationService, AuthenticationState, AuthorizeResult, LogoutError
@@ -110,7 +111,7 @@ export class AADService {
 
     private async _loginInCurrentCloud() {
         try {
-            await this.accessTokenData("common");
+            await this.accessTokenData(defaultTenant);
             this._authenticationState.next(AuthenticationState.Authenticated);
         } catch (error) {
             if (error instanceof LogoutError) {
@@ -207,7 +208,7 @@ export class AADService {
     }
 
     private async _loadTenants(): Promise<TenantDetails[]> {
-        const token = await this.accessTokenData("common");
+        const token = await this.accessTokenData(defaultTenant);
 
         const headers = {
             Authorization: `${token.token_type} ${token.access_token}`,
