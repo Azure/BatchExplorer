72be803ca4a8e6ef3552b8413158e4fbfb42475e
Sanjana Kapur <t-skapur@microsoft.com>
2021-08-06 13:53:12 -0400

    Mocked out Monaco API in web tests

--- web/src/explorer-web/__tests__/setup-tests.ts
+++ web/src/explorer-web/__tests__/setup-tests.ts
@@ -3,3 +3,10 @@ import { destroyEnvironment } from "@batch/ui-common";
 afterEach(() => {
     destroyEnvironment();
 });
+
+// KLUDGE: Mock out the monaco API so that imports to MonacoEditor do not error.
+// Note that the editor itself currently does not work in a Node.js
+// environment regardless
+jest.mock("monaco-editor/esm/vs/editor/editor.api", () => {
+    return {};
+});
