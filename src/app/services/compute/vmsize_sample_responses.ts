c2f9aa29cccfdbf7430aa4ace888ae60c27ca94b
Rena <cRui861@users.noreply.github.com>
2022-02-09 18:08:29 -0800

    Revert "Update to call List Supported Virtual Machine and Cloud Service SKUs (#2400)"
    This reverts commit 7e9ce2e5dd4becf9892a09db78bf1e1f2b08e57c.

--- src/app/services/compute/vmsize_sample_responses.ts
+++ src/app/services/compute/vmsize_sample_responses.ts
@@ -1,5 +1,109 @@
-export const cloudServiceResponse = {
+export const vmSizeSampleResponse = {
     "value": [
+        {
+            "resourceType": "virtualMachines",
+            "locations": [
+                "westus"
+            ],
+            "capabilities": [
+                {
+                    "name": "MaxResourceVolumeMB",
+                    "value": "20480"
+                },
+                {
+                    "name": "OSVhdSizeMB",
+                    "value": "1047552"
+                },
+                {
+                    "name": "vCPUs",
+                    "value": "1"
+                },
+                {
+                    "name": "MemoryGB",
+                    "value": "0.75"
+                },
+                {
+                    "name": "MaxDataDiskCount",
+                    "value": "1"
+                },
+                {
+                    "name": "GPUs",
+                    "value": "0"
+                },
+            ],
+            "locationInfo": [
+                {
+                    "location": "westus",
+                    "zones": [
+                        "2",
+                        "1"
+                    ],
+                    "zoneDetails": [
+                        {
+                            "name": [
+                                "2"
+                            ],
+                            "capabilities": [
+                                {
+                                    "name": "UltraSSDAvailable",
+                                    "value": "True"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ],
+            "name": "Standard_A0",
+            "tier": "Standard",
+            "size": "A0",
+            "family": "standardA0_A7Family"
+        },
+        {
+            "resourceType": "virtualMachines",
+            "locations": [
+                "westus"
+            ],
+            "capabilities": [
+                {
+                    "name": "MaxResourceVolumeMB",
+                    "value": "71680"
+                },
+                {
+                    "name": "OSVhdSizeMB",
+                    "value": "1047552"
+                },
+                {
+                    "name": "vCPUs",
+                    "value": "1"
+                },
+                {
+                    "name": "MemoryGB",
+                    "value": "1.75"
+                },
+                {
+                    "name": "MaxDataDiskCount",
+                    "value": "2"
+                },
+                {
+                    "name": "GPUs",
+                    "value": "0"
+                }
+            ],
+            "locationInfo": [
+                {
+                    "location": "westus",
+                    "zones": [
+                        "1",
+                        "2",
+                        "3"
+                    ]
+                }
+            ],
+            "name": "Standard_A1",
+            "tier": "Standard",
+            "size": "A1",
+            "family": "standardA0_A7Family"
+        },
         {
             "resourceType": "virtualMachines",
             "locations": [
@@ -89,103 +193,8 @@ export const cloudServiceResponse = {
             "size": "D1",
             "family": "standardDFamily"
         }
-    ]
-}
-
-export const virtualMachineResponse = {
-    "value": [
-        {
-            "resourceType": "virtualMachines",
-            "locations": [
-                "westus"
-            ],
-            "capabilities": [
-                {
-                    "name": "MaxResourceVolumeMB",
-                    "value": "71680"
-                },
-                {
-                    "name": "OSVhdSizeMB",
-                    "value": "1047552"
-                },
-                {
-                    "name": "vCPUs",
-                    "value": "1"
-                },
-                {
-                    "name": "MemoryGB",
-                    "value": "1.75"
-                },
-                {
-                    "name": "MaxDataDiskCount",
-                    "value": "2"
-                },
-                {
-                    "name": "GPUs",
-                    "value": "0"
-                }
-            ],
-            "locationInfo": [
-                {
-                    "location": "westus",
-                    "zones": [
-                        "1",
-                        "2",
-                        "3"
-                    ]
-                }
-            ],
-            "name": "Standard_A1",
-            "tier": "Standard",
-            "size": "A1",
-            "family": "standardA0_A7Family"
-        },
-        {
-            "resourceType": "virtualMachines",
-            "locations": [
-                "westus"
-            ],
-            "capabilities": [
-                {
-                    "name": "MaxResourceVolumeMB",
-                    "value": "51200"
-                },
-                {
-                    "name": "OSVhdSizeMB",
-                    "value": "1047552"
-                },
-                {
-                    "name": "vCPUs",
-                    "value": "1"
-                },
-                {
-                    "name": "MemoryGB",
-                    "value": "3.5"
-                },
-                {
-                    "name": "MaxDataDiskCount",
-                    "value": "4"
-                },
-                {
-                    "name": "GPUs",
-                    "value": "2"
-                }
-            ],
-            "locationInfo": [
-                {
-                    "location": "eastus",
-                    "zones": [
-                        "1",
-                    ],
-                    "zoneDetails": []
-                }
-            ],
-            "name": "Standard_D1",
-            "tier": "Standard",
-            "size": "D1",
-            "family": "standardDFamily"
-        }
-    ]
+    ],
+    "nextLink": null
 }
 
 export const badResponseIsNaN = {
