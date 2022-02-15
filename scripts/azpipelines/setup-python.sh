49de3606b8f2a09faa0981505a700776879b0804
Shiran Pasternak <shiran.pasternak@microsoft.com>
2022-02-15 16:38:46 -0500

    Pipelines use private feed for NPM and PyPi

--- scripts/azpipelines/setup-python.sh
+++ scripts/azpipelines/setup-python.sh
@@ -40,3 +40,11 @@ echo "Path is $PATH"
 
 echo "Upgrading pip..."
 python -m pip install --upgrade pip
+pip install keyring artifacts-keyring
+
+echo "Configuring private feed..."
+# If the target conf file doesn't exist, pip config creates one at the user dir.
+# This doesn't matter all that much for build agents.
+touch "$conf_file"
+pip config set global.index-url https://azurebatch.pkgs.visualstudio.com/_packaging/BatchExplorer/pypi/simple/
+pip config list
