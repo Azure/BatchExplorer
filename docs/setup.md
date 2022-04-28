17fe4eb741be2700087374005b2b68ccf2bd9fd7
Shiran Pasternak <shiran.pasternak@microsoft.com>
2022-04-27 22:21:28 -0400

    TS CLI with configure and link commands

--- docs/setup.md
+++ docs/setup.md
@@ -1,22 +1,32 @@
 # Setup your environment
 
-## 1. Prerequisites
+The following are instructions for building and running Batch Explorer and the Shared Libraries in a development environment.
 
-Follow the **Building Batch Explorer yourself** instruction of the [wiki](https://github.com/Azure/BatchExplorer/wiki/Build-batch-explorer-yourself) in order to install all require dependencies.
+1. Install dependencies:
 
-Start the dev server
+    ```bash
+    npm run dev-setup
+    npm install
+    pip3 install -r python/requirements.txt
+    ```
+
+2. Build the libraries:
+
+    ```bash
+    npm run build
+    ```
+
+## Running Batch Shared Libraries Web
 
 ```bash
-npm run dev-server
+npm run start:web
 ```
 
-Start electron
+## Running Batch Explorer Desktop
 
 ```bash
-# In the command line
-npm run dev-electron
-
-# In VSCode just press F5
+npm run dev-server # Start the dev server (in one shell)
+npm run dev-electron # Start Electron (in another shell)
 ```
 
 The dev-server and dev-electron support hot reload for a better development experience. Simply saving a file will cause the UI to refresh to your updated changes.
@@ -29,44 +39,68 @@ Please also take a look at the [coding guidelines](coding-guidelines.md) for thi
 
 If you're using VSCode (recommended) we suggest you use the following extensions:
 
-* EditorConfig for VS Code(Important)
-* TSLint
+* EditorConfig for VS Code (Important)
+* ESLint
 * Debugger for Chrome
 
 ## Detailed commands
 
 **Magic command** (starts the server and electron in dev mode)
 
-```shell
+```bash
 npm run dev
 ```
 
 Build project
 
-```shell
+```bash
 npm run build
 ```
 
 Run app
 
-```shell
+```bash
 npm run electron
 ```
 
 Run watch (this will build files on save)
 
-```shell
+```bash
 npm run watch
 ```
 
 Run dev server (this will handle the refresh of files and later should have live reload)
 
-```shell
+```bash
 npm run dev-server
 ```
 
 Run dev electron (to use the dev server you need to run this)
 
-```shell
+```bash
 npm run dev-electron
 ```
+
+## Working on the Shared Libraries and the Portal Extension
+
+The Portal Extension relies on packages provided by the shared libraries. The portal installs the necessary dependencies that are published on a remote registry. If you want to work on the portal in tandem with local, unpublished versions of the shared libraries, you will need to run the following commands:
+
+1. Install the Batch Utility CLI:
+
+    ```bash
+    npm run dev-setup
+    ```
+
+2. Configure paths to relevant repos:
+
+    ```bash
+    butil configure
+    ```
+
+3. Run the link command:
+
+    ```bash
+    butil link
+    ```
+
+The Portal Extension will now use the shared libraries directly from this repo.
---
+++ util/build-util/util.ts
@@ -0,0 +1,279 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+import color from "cli-color";
+import * as editorconfig from "editorconfig";
+import * as fs from "fs";
+import inquirer from "inquirer";
+import * as os from "os";
+import * as path from "path";
+import * as shell from "shelljs";
+
+export const batchExplorerHome = path.resolve("../../");
+export const configFile = path.resolve(
+    os.homedir(),
+    ".config/batch/butil.json"
+);
+
+const portalReactPath = "src/src/BatchExtension/Client/ReactViews";
+
+export interface Configuration {
+    paths: {
+        batchExplorer?: string;
+        batchPortalExtension?: string;
+    };
+}
+
+export const info = (...args: string[]) => console.log(color.blue(...args));
+export const warn = (...args: string[]) => console.warn(color.yellow(...args));
+export const error = (...args: string[]) => console.error(color.red(...args));
+
+export function readJsonOrDefault(filename: string, defaultJson = {}) {
+    if (fs.existsSync(filename)) {
+        return require(filename);
+    }
+    return defaultJson;
+}
+
+export async function saveJson(filename: string, json: unknown) {
+    const indent = await exports.getEditorConfig("indent_size", filename);
+    const confPath = path.dirname(filename);
+    if (!fs.existsSync(confPath)) {
+        mkdirp(confPath);
+    }
+    return new Promise((resolve) =>
+        fs.writeFile(filename, JSON.stringify(json, null, indent), resolve)
+    );
+}
+
+export function validateDirectory(path: string) {
+    if (shell.test("-d", path)) {
+        return true;
+    }
+    return "Not a valid directory";
+}
+
+export function resolvePath(...paths: string[]) {
+    if (paths.length > 0 && paths[0]) {
+        paths[0] = paths[0].replace(/^~/, os.homedir());
+    }
+    return path.resolve(...paths);
+}
+
+export async function loadConfiguration(): Promise<Configuration> {
+    return readJsonOrDefault(configFile);
+}
+
+/**
+ * Gets option from the project's editor config.
+ * @param {string} option The config option to return
+ * @param {string} filePattern The file pattern to get the option for (defaults to global)
+ * @returns a promise that resolves to the option
+ */
+export async function getEditorConfig(
+    option: keyof editorconfig.KnownProps,
+    filePattern?: string
+) {
+    let configPath = `${batchExplorerHome}/.editorconfig`;
+    if (filePattern) {
+        configPath += "/" + filePattern;
+    }
+    const config: editorconfig.KnownProps = await editorconfig.parse(
+        configPath
+    );
+    return config[option];
+}
+
+export function copyFiles(sourcePath: string, destPath: string) {
+    if (!sourcePath) {
+        error("Failed to copy: No source path specified");
+        return;
+    }
+    if (!destPath) {
+        error("Failed to copy: No dest path specified");
+        return;
+    }
+
+    shell.cp(sourcePath, destPath);
+}
+
+export function mkdirp(targetPath: string) {
+    if (!targetPath) {
+        error("Failed to make directory: No path specified");
+        return;
+    }
+    targetPath = path.resolve(targetPath);
+
+    fs.mkdirSync(targetPath, { recursive: true });
+}
+
+export function moveFiles(sourcePath: string, destPath: string) {
+    if (!sourcePath) {
+        error("Failed to move: No source path specified");
+        return;
+    }
+    if (!destPath) {
+        error("Failed to move: No dest path specified");
+        return;
+    }
+
+    shell.mv(sourcePath, destPath);
+}
+
+export function rmrf(targetPath: string) {
+    if (!targetPath) {
+        error("Failed to delete: No path specified");
+        return;
+    }
+    fs.rmSync(path.resolve(targetPath), {
+        recursive: true,
+        force: true,
+    });
+}
+
+// integrations have one place to look for things like coverage reports
+export function gatherBuildResults(basePath: string) {
+    const baseBuildDir = path.join(basePath, "build");
+
+    const doCopy = (src: string, dst: string) => {
+        info(`Copying build/test results from ${src}/* to ${dst}`);
+        if (!fs.existsSync(src)) {
+            warn(`${src} does not exist - skipping`);
+            return;
+        }
+        mkdirp(dst);
+        shell.cp("-r", src + "/*", dst);
+    };
+
+    // packages
+    doCopy(
+        path.join(basePath, "packages", "common", "build"),
+        path.join(baseBuildDir, "common")
+    );
+    doCopy(
+        path.join(basePath, "packages", "react", "build"),
+        path.join(baseBuildDir, "react")
+    );
+
+    // web
+    doCopy(path.join(basePath, "web", "build"), path.join(baseBuildDir, "web"));
+
+    // desktop
+    doCopy(
+        path.join(basePath, "desktop", "coverage"),
+        path.join(baseBuildDir, "desktop", "coverage")
+    );
+}
+
+export async function linkLocalProjects() {
+    runLinkageTask((opts: LinkOptions) => {
+        info(`Linking ${opts.versionedPackageName}`);
+        shell.cd(opts.packagePath);
+        shell.exec(`npm -s link`);
+        shell.cd(opts.targetPath);
+        shell.exec(`npm link ${opts.packageName}`);
+    });
+}
+
+export async function unlinkLocalProjects() {
+    runLinkageTask((opts: LinkOptions) => {
+        info(`Unlinking ${opts.versionedPackageName}`);
+        shell.cd(opts.targetPath);
+        shell.rm(`node_modules/${opts.packageName}`);
+        shell.exec(`npm install ${opts.versionedPackageName}`);
+    });
+}
+
+/**
+ * Configure CLI properties
+ */
+export async function configure() {
+    const config = readJsonOrDefault(configFile);
+    config.paths ||= {};
+    const answers = await inquirer.prompt([
+        {
+            name: "batchPortalExtension",
+            message: "Path to Batch Portal Extension",
+            default: config.paths.batchPortalExtension,
+            validate: validateDirectory,
+        },
+        {
+            name: "batchExplorer",
+            message: "Path to Batch Explorer",
+            default: config.paths.batchExplorer || batchExplorerHome,
+            validate: validateDirectory,
+        },
+    ]);
+
+    for (const [key, value] of Object.entries(answers)) {
+        config.paths[key] = resolvePath(value as string);
+    }
+    await saveJson(configFile, config);
+    info(`Configuration saved to ${configFile}`);
+}
+
+interface LinkOptions {
+    versionedPackageName?: string;
+    packageName?: string;
+    packagePath?: string;
+    targetPath?: string;
+}
+
+/**
+ * Links or unlinks local NPM projects as dependencies
+ */
+async function runLinkageTask(callback: (opts: LinkOptions) => void) {
+    const config = await loadConfiguration();
+    if (!config.paths || !config.paths.batchPortalExtension) {
+        error(
+            `Configuration does not contain portal paths. ` +
+                `Run the 'configure' command to fix.`
+        );
+        return;
+    }
+    const targetPath = path.join(
+        config.paths.batchPortalExtension,
+        portalReactPath
+    );
+    const targetPackageJson = path.join(targetPath, "package.json");
+    if (!fs.existsSync(targetPackageJson)) {
+        error(`No package.json in target directory ${targetPath}`);
+        return;
+    }
+
+    const targetConf = readJsonOrDefault(targetPackageJson);
+
+    const packageRoot = path.join(
+        config.paths.batchExplorer || batchExplorerHome,
+        "packages"
+    );
+
+    // Iterate over each source package to link/unlink
+    shell.ls(`${packageRoot}/*/package.json`).forEach((packageJson) => {
+        const packagePath = packageJson.replace("/package.json", "");
+        const json = readJsonOrDefault(packageJson);
+        const packageName = json.name;
+        const sourceVersion = json.version;
+
+        let targetVersion = targetConf.dependencies?.[packageName];
+        if (!targetVersion) {
+            targetVersion = targetConf.devDependencies?.[packageName];
+        }
+        if (targetVersion) {
+            if (targetVersion !== sourceVersion) {
+                warn(
+                    `Target version ${targetVersion} different ` +
+                        `than source version ${sourceVersion}`
+                );
+            }
+            callback({
+                packageName,
+                versionedPackageName: `${packageName}@${sourceVersion}`,
+                packagePath,
+                targetPath,
+            });
+        } else {
+            info(`Package ${packageName} not in dependencies - skipping`);
+        }
+    });
+}
