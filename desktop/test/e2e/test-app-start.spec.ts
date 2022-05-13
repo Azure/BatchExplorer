854c5c99a97cbda0d300a0c070415bc9b67ef8be
Shiran Pasternak <shiran.pasternak@microsoft.com>
2022-05-13 14:52:47 -0400

    Onboarding Axe task for E2E testing.

--- desktop/test/e2e/test-app-start.spec.ts
+++ desktop/test/e2e/test-app-start.spec.ts
@@ -1,6 +1,12 @@
 import { ElectronApplication, Page, _electron as electron } from "playwright";
 import { test, expect } from "@playwright/test";
 import { waitForWindows, WindowType, getWindow } from "./utils";
+import AxeBuilder from "@axe-core/playwright";
+import * as Axe from "axe-core";
+import { convertAxeToSarif } from "axe-sarif-converter";
+import path from "path";
+import fs from "fs";
+import { promisify } from "util";
 
 let app: ElectronApplication;
 
@@ -39,7 +45,53 @@ test.describe("Bundled application is starting correctly", () => {
             "bl-account-list *[aria-selected=true] .quick-list-row-state")
         ).toBe("westus");
     });
+
+    describe("Accessibility Tests", () => {
+        beforeEach(async () => {
+            await waitForWindows(app, 4);
+        });
+
+        // This test case shows the most basic example: run a scan, fail the test if there are any failures.
+        // This is the way to go if you have no known/pre-existing violations you need to temporarily baseline.
+        test('accessibility of h1 element', async ({ browserName, page }) => {
+            const accessibilityScanResults = await new AxeBuilder({ page })
+                // This withTags directive restricts Axe to only run tests that detect known violations of
+                // WCAG 2.1 A and AA rules (similar to what Accessibility Insights reports). If you omit
+                // this, Axe will additionally run several "best practice" rules, which are good ideas to
+                // check for periodically but may report false positives in certain edge cases.
+                .withTags(['wcag2a', 'wcag2aa', 'wcag21aa'])
+                .analyze();
+
+            await exportAxeAsSarifTestResult('index-h1-element.sarif', accessibilityScanResults, browserName);
+
+            expect(accessibilityScanResults.violations).toStrictEqual([]);
+        });
+
+    })
 });
+
+// SARIF is a general-purpose log format for code analysis tools.
+//
+// Exporting axe results as .sarif files lets our Azure Pipelines build results page show a nice visualization
+// of any accessibility failures we find using the Sarif Results Viewer Tab extension
+// (https://marketplace.visualstudio.com/items?itemName=sariftools.sarif-viewer-build-tab)
+async function exportAxeAsSarifTestResult(sarifFileName: string, axeResults: Axe.AxeResults, browserName: string): Promise<void> {
+    // We use the axe-sarif-converter package for the conversion step, then write the results
+    // to a file that we'll be publishing from a CI build step in azure-pipelines.yml
+    const sarifResults = convertAxeToSarif(axeResults);
+
+    // This directory should be .gitignore'd and should be published as a build artifact in azure-pipelines.yml
+    const testResultsDirectory = path.join(__dirname, '..', 'test-results', browserName);
+    await promisify(fs.mkdir)(testResultsDirectory, { recursive: true });
+
+    const sarifResultFile = path.join(testResultsDirectory, sarifFileName);
+    await promisify(fs.writeFile)(
+        sarifResultFile,
+        JSON.stringify(sarifResults, null, 2));
+
+    // eslint-disable-next-line no-console
+    console.log(`Exported axe results to ${sarifResultFile}`);
+}
 
 /**
  * Sign in using AAD interactive auth
