a87ed99bc3630b42e33c436cdc23b863ef185f5e
David Watrous <509299+dpwatrous@users.noreply.github.com>
2022-12-08 16:05:34 -0500

    Fix pipeline errors on vnext branch

--- desktop/scripts/azpipelines/update-build-name.ps1
+++ desktop/scripts/azpipelines/update-build-name.ps1
@@ -1,4 +1,5 @@
-$version = npm run ts "scripts/package/get-version.ts"
+Set-Location "desktop"
+$version = npm run -s ts "scripts/package/get-version.ts"
 
 Write-Host "Updating build number to $version"
 Write-Host "##vso[build.updatebuildnumber]$version"
