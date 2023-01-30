ed3d0ecaa698424e419e48d9c56acff5ff7c735e
Shiran Pasternak <shiran.pasternak@microsoft.com>
2023-01-30 14:18:16 -0500

    Gets BatchExplorer version directly through NPM

--- scripts/azpipelines/update-build-name.ps1
+++ scripts/azpipelines/update-build-name.ps1
@@ -1,4 +1,4 @@
-$version = npm run -s ts "scripts/package/get-version.ts"
+$version = $(npm pkg get version) -replace '"', ""
 
 Write-Host "Updating build number to $version"
 Write-Host "##vso[build.updatebuildnumber]$version"
