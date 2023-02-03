1d7ac1aea3d5a7e62e0179a9271b16a1ab527f71
Shiran Pasternak <shiran.pasternak@microsoft.com>
2023-02-03 13:15:57 -0500

    Restores build version string for release packaging

--- scripts/azpipelines/build-type.ps1
+++ scripts/azpipelines/build-type.ps1
@@ -3,21 +3,21 @@ param (
     $buildNumber
 )
 # This compute the build type for VSTS build
-$buildType="dev"
+$buildType = "dev"
 
 Write-Host "Branch is $branch"
 
 If ($branch -like "refs/heads/master") {
-    $buildType="insider"
+    $buildType = "insider"
 }
 
 If ($branch -like "refs/heads/stable") {
-    $buildType="stable"
+    $buildType = "stable"
 }
 
 # Change to curent branch for testing
 If ($branch -like "refs/heads/feature/signing-vsts") {
-    $buildType="testing"
+    $buildType = "testing"
 }
 
 Write-Host "Build type is $buildType"
@@ -26,6 +26,6 @@ Write-Host "##vso[task.setvariable variable=BUILD_TYPE]$buildType"
 
 # Hyphens in the version causes RPM packaging to fail
 # See https://azurebatch.visualstudio.com/BatchExplorer/_workitems/edit/379
-$newBuildNumber=$buildNumber -replace '-', '_'
+$newBuildNumber = $buildNumber -replace '-', '_'
 Write-Host "Setting build number to $newBuildNumber"
 Write-Host "##vso[build.updatebuildnumber]$newBuildNumber"
