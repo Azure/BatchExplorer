f9fdc03e0f3c1bb4c064a0ebb0ef776ce437e39d
David Watrous <509299+dpwatrous@users.noreply.github.com>
2023-02-20 18:01:42 -0500

    Add equalsIgnoreCase utility

--- packages/common/src/ui-common/util/__tests__/string.spec.ts
+++ packages/common/src/ui-common/util/__tests__/string.spec.ts
@@ -1,4 +1,4 @@
-import { capitalizeFirst } from "../../util";
+import { capitalizeFirst, equalsIgnoreCase } from "../../util";
 
 describe("String utility functions", () => {
     test("capitalizeFirst() function", () => {
@@ -7,5 +7,23 @@ describe("String utility functions", () => {
         expect(capitalizeFirst("A")).toBe("A");
         expect(capitalizeFirst("")).toBe("");
         expect(capitalizeFirst("1")).toBe("1");
+    });
+
+    test("equalsIgnoreCase() function", () => {
+        expect(equalsIgnoreCase("Abc", "abC")).toBe(true);
+        expect(equalsIgnoreCase("abc", " abc")).toBe(false);
+        expect(equalsIgnoreCase("", "")).toBe(true);
+        expect(equalsIgnoreCase(undefined, undefined)).toBe(true);
+        expect(equalsIgnoreCase(undefined, "")).toBe(true);
+        expect(equalsIgnoreCase(undefined, " ")).toBe(false);
+        expect(equalsIgnoreCase(null as unknown as undefined, undefined)).toBe(
+            true
+        );
+        expect(
+            equalsIgnoreCase(
+                null as unknown as undefined,
+                null as unknown as undefined
+            )
+        ).toBe(true);
     });
 });
--- packages/common/src/ui-common/util/string.ts
+++ packages/common/src/ui-common/util/string.ts
@@ -9,3 +9,22 @@ export function capitalizeFirst(str: string): string {
     }
     return str.substring(0, 1).toUpperCase() + str.substring(1);
 }
+
+/**
+ * Case insensitive string equality. Note that if both strings are either null,
+ * undefined or empty string they will be considered equal.
+ *
+ * @param str1 The first string to compare
+ * @param str2 The second string to compare
+ * @returns True if the strings are equal, or if both values are null or undefined.
+ *          False otherwise.
+ */
+export function equalsIgnoreCase(str1?: string, str2?: string): boolean {
+    if (str1 == null) {
+        str1 = "";
+    }
+    if (str2 == null) {
+        str2 = "";
+    }
+    return str1.toLowerCase() === str2.toLowerCase();
+}
