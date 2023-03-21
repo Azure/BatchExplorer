c5052f3349cf13cd685b1806ce678cee77540d92
David Watrous <509299+dpwatrous@users.noreply.github.com>
2023-03-21 11:47:41 -0400

    Remove react-router dependency from playground
    Added some basic hash routing, but routing should mainly be the concern of the application importing the playground, not the playground itself.

--- packages/playground/src/ui-playground/layout/demo-main-content.tsx
+++ packages/playground/src/ui-playground/layout/demo-main-content.tsx
@@ -1,45 +1,11 @@
 import * as React from "react";
-import { Route, Switch } from "react-router";
-import { ButtonDemo } from "../demo/form/button/button-demo";
-import { ComboBoxDemo } from "../demo/form/combobox/combobox-demo";
-import { SearchBoxDemo } from "../demo/form/searchbox/searchbox-demo";
-import { TextFieldDemo } from "../demo/form/textfield/textfield-demo";
-import { CertificateDisplayDemo } from "../demo/display/certificate/certificate-display-demo";
-import { ActionFormDemo } from "../demo/form/action-form-demo";
-import { DropdownDemo } from "../demo/form/dropdown-demo";
-import { RadioButtonDemo } from "../demo/form/radiobutton/radiobutton-demo";
-import { CheckboxDemo } from "../demo/form/checkbox/checkbox-demo";
+import { DemoName, DEMO_MAP } from "../demo-routes";
 
-export const DemoMainContent: React.FC = () => {
-    return (
-        <Switch>
-            <Route path="/playground/actionform">
-                <ActionFormDemo />
-            </Route>
-            <Route path="/playground/button">
-                <ButtonDemo />
-            </Route>
-            <Route path="/playground/checkbox">
-                <CheckboxDemo />
-            </Route>
-            <Route path="/playground/radiobutton">
-                <RadioButtonDemo />
-            </Route>
-            <Route path="/playground/combobox">
-                <ComboBoxDemo />
-            </Route>
-            <Route path="/playground/dropdown">
-                <DropdownDemo />
-            </Route>
-            <Route path="/playground/searchbox">
-                <SearchBoxDemo />
-            </Route>
-            <Route path="/playground/textfield">
-                <TextFieldDemo />
-            </Route>
-            <Route path="/playground/displays/certificate">
-                <CertificateDisplayDemo />
-            </Route>
-        </Switch>
-    );
+export interface DemoMainContentProps {
+    demoName?: DemoName;
+}
+
+export const DemoMainContent: React.FC<DemoMainContentProps> = (props) => {
+    const demoName = props.demoName ?? "default";
+    return DEMO_MAP[demoName]();
 };
