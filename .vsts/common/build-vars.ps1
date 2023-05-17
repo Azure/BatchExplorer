d8edd98e25dbd31e5a88101e4729b736380d8646
hoppe <hoppewang@microsoft.com>
2023-05-17 15:22:38 +0800

    remove checkbox inconsistent props & modifie package.json

--- .vsts/common/build-vars.ps1
+++ .vsts/common/build-vars.ps1
@@ -20,25 +20,6 @@ If ($branch -like "refs/heads/stable") {
     $distTag = "stable"
 }
 
-If ($branch -like "refs/heads/vnext") {
-    $buildType = "vnext"
-    $distTag = "next"
-}
-
-# TEMPORARY (2023-02-09): This is to support the transition from vnext to main.
-# Eventually, the distribution pipeline on the main branch will create:
-#
-# - A BatchExplorer insider build (buildType = "insider")
-# - NPM packages with the latest tag (distTag = "latest")
-#
-# In the next phase, the `vnext` branch will be removed and the above condition
-# can be removed as well. In a subsequent phase, the `master` branch will be
-# deprecated, at which point the `master` condition above will also be removed.
-If ($branch -like "refs/heads/main") {
-    $buildType = "vnext"
-    $distTag = "next"
-}
-
 If ($branch -like "refs/heads/hoppe/update-v1") {
     $buildType = "hoppe-update-v1"
     $distTag = "hoppe-update-v1"
