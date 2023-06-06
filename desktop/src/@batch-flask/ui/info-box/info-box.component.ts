8c28aad8e9d81ba1cde5f8d3f0a9315cf4df41f5
Waseem Dahman <mhdahman@microsoft.com>
2023-06-06 10:51:07 -0700

    Fixes AB#598: Alt not defined on image

--- desktop/src/@batch-flask/ui/info-box/info-box.component.ts
+++ desktop/src/@batch-flask/ui/info-box/info-box.component.ts
@@ -2,11 +2,7 @@ import { Component, Input } from "@angular/core";
 
 @Component({
     selector: "bl-info-box",
-    template: `
-        <div class="infobox">
-            <div class="infobox-image"><i class="fa fa-info-circle fa-3" aria-hidden="true"></i></div>
-            <div class="infobox-text">{{message}}</div>
-        </div>`,
+    templateUrl: "info-box.html",
 })
 export class InfoBoxComponent {
     @Input()
--- desktop/src/@batch-flask/ui/info-box/info-box.module.ts
+++ desktop/src/@batch-flask/ui/info-box/info-box.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { InfoBoxComponent } from "./info-box.component";
+import { I18nUIModule } from "../i18n";
 
 const components = [
     InfoBoxComponent,
@@ -10,7 +11,7 @@ const components = [
     declarations: components,
     entryComponents: [],
     exports: [...components],
-    imports: [RouterModule],
+    imports: [RouterModule, I18nUIModule],
 })
 
 export class InfoBoxModule {
