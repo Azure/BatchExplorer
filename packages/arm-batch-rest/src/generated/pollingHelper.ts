f80f45d432a410a3ff2627a9a58eebf3898bc7f9
NickKouds <nickouds@hotmail.com>
2023-06-26 21:45:51 -0700

    Add RLC generated src files to pretty ignore and regenerated SDK

--- packages/arm-batch-rest/src/generated/pollingHelper.ts
+++ packages/arm-batch-rest/src/generated/pollingHelper.ts
@@ -3,12 +3,12 @@
 
 import { Client, HttpResponse } from "@azure-rest/core-client";
 import {
-    LongRunningOperation,
-    LroEngine,
-    LroEngineOptions,
-    LroResponse,
-    PollerLike,
-    PollOperationState,
+  LongRunningOperation,
+  LroEngine,
+  LroEngineOptions,
+  LroResponse,
+  PollerLike,
+  PollOperationState
 } from "@azure/core-lro";
 
 /**
@@ -19,35 +19,35 @@ import {
  * @returns - A poller object to poll for operation state updates and eventually get the final response.
  */
 export function getLongRunningPoller<TResult extends HttpResponse>(
-    client: Client,
-    initialResponse: TResult,
-    options: LroEngineOptions<TResult, PollOperationState<TResult>> = {}
+  client: Client,
+  initialResponse: TResult,
+  options: LroEngineOptions<TResult, PollOperationState<TResult>> = {}
 ): PollerLike<PollOperationState<TResult>, TResult> {
-    const poller: LongRunningOperation<TResult> = {
-        requestMethod: initialResponse.request.method,
-        requestPath: initialResponse.request.url,
-        sendInitialRequest: async () => {
-            // In the case of Rest Clients we are building the LRO poller object from a response that's the reason
-            // we are not triggering the initial request here, just extracting the information from the
-            // response we were provided.
-            return getLroResponse(initialResponse);
-        },
-        sendPollRequest: async (path) => {
-            // This is the callback that is going to be called to poll the service
-            // to get the latest status. We use the client provided and the polling path
-            // which is an opaque URL provided by caller, the service sends this in one of the following headers: operation-location, azure-asyncoperation or location
-            // depending on the lro pattern that the service implements. If non is provided we default to the initial path.
-            const response = await client
-                .pathUnchecked(path ?? initialResponse.request.url)
-                .get();
-            const lroResponse = getLroResponse(response as TResult);
-            lroResponse.rawResponse.headers["x-ms-original-url"] =
-                initialResponse.request.url;
-            return lroResponse;
-        },
-    };
+  const poller: LongRunningOperation<TResult> = {
+    requestMethod: initialResponse.request.method,
+    requestPath: initialResponse.request.url,
+    sendInitialRequest: async () => {
+      // In the case of Rest Clients we are building the LRO poller object from a response that's the reason
+      // we are not triggering the initial request here, just extracting the information from the
+      // response we were provided.
+      return getLroResponse(initialResponse);
+    },
+    sendPollRequest: async (path) => {
+      // This is the callback that is going to be called to poll the service
+      // to get the latest status. We use the client provided and the polling path
+      // which is an opaque URL provided by caller, the service sends this in one of the following headers: operation-location, azure-asyncoperation or location
+      // depending on the lro pattern that the service implements. If non is provided we default to the initial path.
+      const response = await client
+        .pathUnchecked(path ?? initialResponse.request.url)
+        .get();
+      const lroResponse = getLroResponse(response as TResult);
+      lroResponse.rawResponse.headers["x-ms-original-url"] =
+        initialResponse.request.url;
+      return lroResponse;
+    }
+  };
 
-    return new LroEngine(poller, options);
+  return new LroEngine(poller, options);
 }
 
 /**
@@ -56,20 +56,20 @@ export function getLongRunningPoller<TResult extends HttpResponse>(
  * @returns - An LRO response that the LRO engine can work with
  */
 function getLroResponse<TResult extends HttpResponse>(
-    response: TResult
+  response: TResult
 ): LroResponse<TResult> {
-    if (Number.isNaN(response.status)) {
-        throw new TypeError(
-            `Status code of the response is not a number. Value: ${response.status}`
-        );
-    }
+  if (Number.isNaN(response.status)) {
+    throw new TypeError(
+      `Status code of the response is not a number. Value: ${response.status}`
+    );
+  }
 
-    return {
-        flatResponse: response,
-        rawResponse: {
-            ...response,
-            statusCode: Number.parseInt(response.status),
-            body: response.body,
-        },
-    };
+  return {
+    flatResponse: response,
+    rawResponse: {
+      ...response,
+      statusCode: Number.parseInt(response.status),
+      body: response.body
+    }
+  };
 }
