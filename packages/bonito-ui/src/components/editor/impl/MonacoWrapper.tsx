61d8e670d102b9d94ed3594bc62efb7702f2a9e2
David Watrous <509299+dpwatrous@users.noreply.github.com>
2023-09-05 12:14:32 -0400

    Switch monaco wrapper to use import instead of require.ensure

--- packages/bonito-ui/src/components/editor/impl/MonacoWrapper.tsx
+++ packages/bonito-ui/src/components/editor/impl/MonacoWrapper.tsx
@@ -5,21 +5,9 @@ import { Spinner, SpinnerSize } from "@fluentui/react/lib/Spinner";
 import type * as MonacoEditorImpl from "./MonacoEditorImpl";
 
 /**
- * Import monaco dynamically.
- *
- * This avoids using import() to kludge around a problem with Typescript when it
- * is not using module = "esnext" (which prevents code splitting).
- *
- * See: https://github.com/webpack/webpack/issues/5703#issuecomment-357512412
+ * Import monaco dynamically to allow for code splitting
  */
-const MonacoEditorLazy = React.lazy(async () => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const req = require as any;
-    return new Promise<typeof MonacoEditorImpl>((resolve) => {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        return req.ensure([], (req: any) => resolve(req("./MonacoEditorImpl")));
-    });
-});
+const MonacoEditorLazy = React.lazy(() => import("./MonacoEditorImpl"));
 
 const LoadingSpinner: React.FC = () => {
     return (
