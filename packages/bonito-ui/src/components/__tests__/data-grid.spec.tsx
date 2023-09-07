066ee6ffd921fe2569de26a36ac3c3e3e2f37364
David Watrous <509299+dpwatrous@users.noreply.github.com>
2023-09-06 20:31:17 -0400

    Update jest-axe to remove jsdom warnings
    See: https://github.com/NickColley/jest-axe/issues/147

--- packages/bonito-ui/src/components/__tests__/data-grid.spec.tsx
+++ packages/bonito-ui/src/components/__tests__/data-grid.spec.tsx
@@ -9,6 +9,12 @@ const ignoredA11yRules = {
     rules: {
         // TODO: Re-enable this when DetailsList fixes this issue
         "aria-toggle-field-name": { enabled: false },
+        // See https://github.com/microsoft/fluentui/issues/28706
+        "aria-required-children": { enabled: false },
+        // TODO: Figure out if this is a real issue. Started happening
+        //       after upgrading to jest-axe 7.x. May be this issue:
+        //       https://github.com/microsoft/fluentui/issues/18474
+        "empty-table-header": { enabled: false },
     },
 };
 
