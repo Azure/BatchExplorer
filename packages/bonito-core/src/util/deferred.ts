e5e31caa17a498ecbaa2451ce13003c0b2230fe3
David Watrous <509299+dpwatrous@users.noreply.github.com>
2023-09-09 11:11:12 -0400

    Add Deferred tests

--- packages/bonito-core/src/util/deferred.ts
+++ packages/bonito-core/src/util/deferred.ts
@@ -3,19 +3,29 @@
  * at a later point in time with a function call.
  */
 export class Deferred<T = void> {
-    public promise: Promise<T>;
-    public done = false;
-    public resolve!: (result: T) => void;
-    public reject!: (error: unknown) => void;
+    promise: Promise<T>;
+
+    done = false;
+    resolved = false;
+    rejected = false;
+    resolvedTo?: T;
+    rejectedTo?: unknown;
+
+    resolve!: (result: T) => void;
+    reject!: (error: unknown) => void;
 
     constructor() {
         this.promise = new Promise<T>((resolve, reject) => {
             this.reject = (e) => {
                 this.done = true;
+                this.rejected = true;
+                this.rejectedTo = e;
                 reject(e);
             };
             this.resolve = (r: T) => {
                 this.done = true;
+                this.resolved = true;
+                this.resolvedTo = r;
                 resolve(r);
             };
         });
