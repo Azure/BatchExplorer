1b4b4e608229853cce643676fe73d4507d140ff6
David Watrous <509299+dpwatrous@users.noreply.github.com>
2023-11-02 16:13:47 -0400

    Disable code coverage for most of playground/web (#2829)
    These are non-production and their code coverage scores
    shouldn't be aggregated with production packages.

--- packages/playground/jest.config.js
+++ packages/playground/jest.config.js
@@ -6,5 +6,7 @@ module.exports = require("@batch/common-config/jest-common").createConfig(
     {
         testEnvironment: "jsdom",
         setupFilesAfterEnv: ["<rootDir>/src/__tests__/setup-tests.tsx"],
+        // Ignore most code coverage as this isn't used in production
+        coveragePathIgnorePatterns: ["^(?!.*(playground-example.tsx))"],
     }
 );
