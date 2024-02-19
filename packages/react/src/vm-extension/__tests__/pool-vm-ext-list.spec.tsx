68f9b2b9351f33367f3060bd937fc99e5b2570fe
hoppe <hoppewang@microsoft.com>
2024-02-19 14:21:00 +0800

    Add accessibility tests for node and pool vm extension lists

--- packages/react/src/vm-extension/__tests__/pool-vm-ext-list.spec.tsx
+++ packages/react/src/vm-extension/__tests__/pool-vm-ext-list.spec.tsx
@@ -4,6 +4,8 @@ import { render, waitFor } from "@testing-library/react";
 import React from "react";
 import { translate } from "@azure/bonito-core";
 import { initMockBatchBrowserEnvironment } from "../../environment";
+import { dataGridIgnoredA11yRules } from "@azure/bonito-ui/lib/components/data-grid/test-ignore-a11y-rules";
+import { runAxe } from "@azure/bonito-ui/lib/test-util/a11y";
 
 const poolArmIdWithExt =
     "/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/supercomputing/providers/microsoft.batch/batchaccounts/hobo/pools/hobopool1";
@@ -14,11 +16,14 @@ describe("PoolVMExtList", () => {
     });
 
     it("should render vm extension lists", async () => {
-        const { getByText } = render(
+        const { getByText, container } = render(
             <PoolVMExtList poolArmId={poolArmIdWithExt} />
         );
         getByText(translate("lib.react.vmExtension.name"));
         await waitFor(() => getByText("batchextension1"));
+        expect(
+            await runAxe(container, dataGridIgnoredA11yRules)
+        ).toHaveNoViolations();
     });
 
     it("should render vm extension lists with no result", async () => {
