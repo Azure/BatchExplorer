f433574614ccb04bc1a5cdad50185a6e6760d035
Brenda Coronel <t-brecoronel@microsoft.com>
2024-07-03 14:39:42 -0700

    Infinite scroll added to demo

--- packages/service/src/index.ts
+++ packages/service/src/index.ts
@@ -3,4 +3,5 @@ export * from "./account";
 export * from "./certificate";
 export * from "./pool";
 export * from "./node";
+export * from "./task";
 export * from "./constants";
---
+++ packages/service/src/task/index.ts
@@ -0,0 +1,2 @@
+export * from "./task-service";
+export * from "./live-task-service";
