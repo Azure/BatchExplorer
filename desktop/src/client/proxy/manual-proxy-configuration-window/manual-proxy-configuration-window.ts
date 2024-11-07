d63220029a39ee3694ad34d0aba0acef9678f9f0
Shiran Pasternak <shiran.pasternak@microsoft.com>
2024-11-07 10:04:10 -0500

    Fixes broken proxy config window (#2938)
    * Fixes broken proxy config window
    
    * Refactors proxy window class and adds unit tests

--- desktop/src/client/proxy/manual-proxy-configuration-window/manual-proxy-configuration-window.ts
+++ desktop/src/client/proxy/manual-proxy-configuration-window/manual-proxy-configuration-window.ts
@@ -1,25 +1,62 @@
 import { autobind } from "@batch-flask/core";
 import { Constants } from "client/client-constants";
-import { BatchExplorerApplication, ClosedWindowError, GenericWindow, enableRemoteForWindow } from "client/core";
-import { Deferred } from "common";
+import { BatchExplorerApplication, GenericWindow } from "client/core";
 import { BrowserWindow, app, ipcMain, nativeImage } from "electron";
 import { ProxySetting, ProxySettings } from "get-proxy-settings";
 
 const urls = Constants.urls.manualProxyConfiguration;
 const url = process.env.HOT ? urls.dev : urls.prod;
 
+export interface ProxyConfiguration {
+    url: string;
+    port: string;
+    username: string;
+    password: string;
+}
+
 export class ManualProxyConfigurationWindow extends GenericWindow {
     public settings: Promise<ProxySettings | null>;
-    private _deferred: Deferred<ProxySettings | null>;
+    private resolveSettings!: (value: ProxySettings | null) => void;
+    private rejectSettings!: (reason?: any) => void;
+    private proxyConfiguration: ProxyConfiguration | null = null;
+    private settingsSaved = false;
 
     constructor(batchExplorerApplication: BatchExplorerApplication, private currentSettings?: ProxySettings | null) {
         super(batchExplorerApplication);
-        this._deferred = new Deferred();
-        this.settings = this._deferred.promise;
+        this.settings = new Promise((resolve, reject) => {
+            this.resolveSettings = resolve;
+            this.rejectSettings = reject;
+        });
     }
 
-    protected createWindow() {
-        const window = new BrowserWindow({
+    public createWindow() {
+        const window = this._initializeWindow();
+
+        if (this.currentSettings) {
+            const setting = this.currentSettings.https || this.currentSettings.http;
+            if (setting) {
+                this.proxyConfiguration = {
+                    url: `${setting.protocol}://${setting.host}`,
+                    port: setting.port,
+                    username: setting.credentials?.username,
+                    password: setting.credentials?.password,
+                };
+            }
+        }
+
+        window.loadURL(url).catch((error) => {
+            console.error("Failed to load URL:", error);
+            this.rejectSettings(error);
+        });
+
+        window.once("ready-to-show", () => this.show());
+
+        this._setupEvents(window);
+        return window;
+    }
+
+    private _initializeWindow() {
+        return new BrowserWindow({
             title: app.name,
             height: 400,
             width: 500,
@@ -33,50 +70,48 @@ export class ManualProxyConfigurationWindow extends GenericWindow {
                 contextIsolation: false,
             },
         });
-        enableRemoteForWindow(window);
-
-        if (this.currentSettings) {
-            const setting = this.currentSettings.https || this.currentSettings.http;
-            if (setting) {
-                (window as any).currentSettings = {
-                    url: `${setting.protocol}://${setting.host}`,
-                    port: setting.port,
-                    username: setting.credentials && setting.credentials.username,
-                    password: setting.credentials && setting.credentials.password,
-                };
-            }
-        }
-
-        window.loadURL(url);
-        window.once("ready-to-show", () => {
-            this.show();
-        });
-        this._setupEvents(window);
-        return window;
     }
 
     private _setupEvents(window: BrowserWindow) {
-        ipcMain.once("proxy-configuration-submitted", this._processNewSettings);
+        this._subscribeIpcHandlers();
         window.on("close", () => {
-            ipcMain.removeListener("proxy-configuration-submitted", this._processNewSettings);
-            if (!this._deferred.hasCompleted) {
-                this._deferred.reject(new ClosedWindowError("Window was closed"));
+            this._unsubscribeIpcHandlers();
+            if (!this.settingsSaved) {
+                // inform caller that the window was closed
+                this.rejectSettings("Window was closed");
             }
         });
     }
 
+    private _subscribeIpcHandlers() {
+        ipcMain.on("proxy-configuration-submitted", this._processNewSettings);
+        ipcMain.handle("proxy-configuration-requested", this._loadProxySettings);
+    }
+
+    private _unsubscribeIpcHandlers() {
+        ipcMain.removeListener("proxy-configuration-submitted", this._processNewSettings);
+        ipcMain.removeHandler("proxy-configuration-requested");
+    }
+
     @autobind()
-    private _processNewSettings(event, { url, port, username, password }) {
+    private _processNewSettings(_, config: ProxyConfiguration) {
+        const { url, port, username, password } = config;
         this.hide();
         if (url && port) {
             const setting = new ProxySetting(`${url}:${port}`);
             if (username) {
                 setting.credentials = { username, password };
             }
-            this._deferred.resolve({ http: setting, https: setting });
+            this.resolveSettings({ http: setting, https: setting });
         } else {
-            this._deferred.resolve(null);
+            this.resolveSettings(null);
         }
+        this.settingsSaved = true;
         this.close();
+    }
+
+    @autobind()
+    private _loadProxySettings(): ProxyConfiguration | null {
+        return this.proxyConfiguration;
     }
 }
